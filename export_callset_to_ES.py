82e300e7b07bab9bc4cea1513fc3aa703ab544f7
bw2 <weisburd@broadinstitute.org>
2017-07-24 13:17:14 -0400

    run VEP before loading, disable indexing and doc_values for vep annotationsn field

--- export_callset_to_ES.py
+++ export_callset_to_ES.py
@@ -12,6 +12,7 @@ from utils.computed_fields_utils import get_expr_for_variant_id, \
     get_expr_for_worst_transcript_consequence_annotations_struct, get_expr_for_end_pos, \
     get_expr_for_xpos, get_expr_for_contig, get_expr_for_start_pos, get_expr_for_alt_allele, \
     get_expr_for_ref_allele
+from utils.gcloud_utils import inputs_older_than_outputs
 from utils.vds_schema_string_utils import convert_vds_schema_string_to_annotate_variants_expr
 from utils.add_1kg_phase3 import add_1kg_phase3_data_struct
 from utils.add_clinvar import add_clinvar_data_struct
@@ -20,7 +21,9 @@ from utils.add_mpc import add_mpc_data_struct
 from utils.elasticsearch_utils import export_vds_to_elasticsearch
 
 p = argparse.ArgumentParser()
-p.add_argument("-g", "--genome_version", help="Genome build: 37 or 38", choices=["37", "38"], required=True )
+p.add_argument("-g", "--genome-version", help="Genome build: 37 or 38", choices=["37", "38"], required=True )
+p.add_argument("-f", "--force-vep", help="Re-run VEP even the input file is already annotated. "
+    "Otherwise, VEP will be skipped if the input VDS already has a va.vep field.")
 p.add_argument("-H", "--host", help="Elasticsearch host or IP", default="10.48.0.105")
 p.add_argument("-p", "--port", help="Elasticsearch port", default=30001, type=int)  # 9200
 p.add_argument("-i", "--index", help="Elasticsearch index name", default="variant_callset")
@@ -31,7 +34,7 @@ p.add_argument("dataset_path", help="input VCF or VDS")
 # parse args
 args = p.parse_args()
 
-hc = hail.HailContext(log="./hail.log")
+hc = hail.HailContext(log="/hail.log")
 
 if args.dataset_path.endswith(".vds"):
     vds = hc.read(args.dataset_path)
@@ -41,6 +44,14 @@ else:
 vds = vds.annotate_variants_expr("va.originalAltAlleles=%s" % get_expr_for_orig_alt_alleles_set())
 #vds = vds.annotate_variants_expr("va.vep=va.info.CSQ")
 vds = vds.split_multi()
+
+if not any(field.name == "vep" for field in vds.variant_schema.fields):
+    vep_output_path = args.dataset_path.replace(".vds", "").replace(".vcf.gz", "").replace(".vcf.bgz", "") + ".vep.vds"
+    if not inputs_older_than_outputs([args.dataset_path], [vep_output_path]):
+        vds = vds.vep(config="/vep/vep-gcloud.properties", root='va.vep', block_size=1000)  #, csq=True)
+        vds.write(vep_output_path, overwrite=True)
+    else:
+        vds = vds.read(vep_output_path)
 
 #pprint(vds.variant_schema)
 #pprint(vds.sample_ids)
@@ -53,7 +64,7 @@ vds_computed_annotations_exprs = [
     "va.transcriptIds = %s" % get_expr_for_vep_transcript_ids_set(vep_root="va.vep"),
     "va.transcriptConsequenceTerms = %s" % get_expr_for_vep_consequence_terms_set(vep_root="va.vep"),
     "va.sortedTranscriptConsequences = %s" % get_expr_for_vep_sorted_transcript_consequences_array(vep_root="va.vep"),
-    "va.mainTranscriptAnnotations = %s" % get_expr_for_worst_transcript_consequence_annotations_struct("va.sortedTranscriptConsequences"),
+    "va.mainTranscript = %s" % get_expr_for_worst_transcript_consequence_annotations_struct("va.sortedTranscriptConsequences"),
     "va.sortedTranscriptConsequences = json(va.sortedTranscriptConsequences)",
 
     "va.variantId = %s" % get_expr_for_variant_id(),
@@ -97,7 +108,7 @@ INPUT_SCHEMA = {
         geneIds: Set[String],
         transcriptIds: Set[String],
         transcriptConsequenceTerms: Set[String],
-        mainTranscriptAnnotations: Struct,
+        mainTranscript: Struct,
         sortedTranscriptConsequences: String,
     """,
     "info_fields": """
@@ -106,7 +117,6 @@ INPUT_SCHEMA = {
          AN: Int,
          BaseQRankSum: Double,
          ClippingRankSum: Double,
-         DB: Boolean,
          DP: Int,
          DS: Boolean,
          END: Int,
@@ -122,7 +132,8 @@ INPUT_SCHEMA = {
     """
 }
 
-DISABLE_INDEX_FOR_FIELDS=("sortedTranscriptConsequences", )
+DISABLE_INDEX_FOR_FIELDS = ("sortedTranscriptConsequences", )
+DISABLE_DOC_VALUES_FOR_FIELDS = ("sortedTranscriptConsequences", )
 
 vds = vds.annotate_variants_expr(
     convert_vds_schema_string_to_annotate_variants_expr(root="va.clean", **INPUT_SCHEMA)
@@ -134,10 +145,11 @@ vds = add_1kg_phase3_data_struct(hc, vds, args.genome_version, root="va.g1k")
 vds = add_clinvar_data_struct(hc, vds, args.genome_version, root="va.clinvar")
 vds = add_mpc_data_struct(hc, vds, args.genome_version, root="va.mpc")
 
+# see https://hail.is/hail/annotationdb.html#query-builder
 #vds = vds.annotate_variants_db([
 #    'va.cadd.PHRED',
 #    'va.cadd.RawScore',
-#    'va.dann.score'
+#    'va.dann.score',
 #])
 
 pprint(vds.variant_schema)
