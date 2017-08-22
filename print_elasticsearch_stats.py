6e276ff8b664f2f9fab042d26e8db8f7fd7e1d16
bw2 <weisburd@broadinstitute.org>
2017-08-22 07:31:25 -0400

    by default, connect to elasticsearch via kubectl proxy

--- print_elasticsearch_stats.py
+++ print_elasticsearch_stats.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 import pip
 
 from utils.elasticsearch_utils import print_elasticsearch_stats
@@ -8,14 +7,17 @@ pip.main(['install', 'elasticsearch'])
 import elasticsearch
 
 p = argparse.ArgumentParser()
-p.add_argument("-H", "--host", default="10.48.0.105")
-p.add_argument("-p", "--port", default="30001")
+p.add_argument("-H", "--host", help="elasticsearch client host. The default address works if "
+    "`kubectl proxy` is running in the background.",
+    default="http://localhost:8001/api/v1/namespaces/default/services/elasticsearch:9200/proxy")
+p.add_argument("-p", "--port", help="elasticsearch client port.", default="30001")
 
 args = p.parse_args()
 
 # to get the ip address, run  `kubectl describe pod elasticsearch-1019229749-vhghc`
-ELASTICSEARCH_HOST = args.host #os.environ.get('ELASTICSEARCH_SERVICE_HOST', p.host)
-ELASTICSEARCH_PORT = args.port #os.environ.get('ELASTICSEARCH_SERVICE_PORT', p.port) #"9200"
+ELASTICSEARCH_HOST = args.host
+ELASTICSEARCH_PORT = args.port
 es = elasticsearch.Elasticsearch(ELASTICSEARCH_HOST, port=ELASTICSEARCH_PORT)
 
 print_elasticsearch_stats(es)
+
