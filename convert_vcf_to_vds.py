b878d39fb9b2a81f94e70f929d071ff2ccc5195a
bw2 <weisburd@broadinstitute.org>
2017-09-03 23:46:06 -0400

    option to convert multiple vcfs

--- convert_vcf_to_vds.py
+++ convert_vcf_to_vds.py
@@ -7,24 +7,34 @@ from pprint import pprint
 from utils.computed_fields_utils import get_expr_for_orig_alt_alleles_set
 
 p = ap.ArgumentParser()
-p.add_argument("vcf_path")
+p.add_argument("vcf_path", nargs="+")
 args = p.parse_args()
 
-print(args.vcf_path)
+print(", ".join(args.vcf_path))
 
 hc = hail.HailContext(log="/hail.log")
 
-output_path = args.vcf_path.replace(".vcf", "").replace(".gz", "") + ".vds"
+for vcf_path in args.vcf_path:
+    print("\n")
+    print("==> import_vcf: %s" % vcf_path)
 
-print("\n==> import_vcf: %s" % args.vcf_path)
-vds = hc.import_vcf(args.vcf_path, force_bgz=True, min_partitions=10000)
+    output_path = vcf_path.replace(".vcf", "").replace(".gz", "") + ".vds"
+    print("==> output: %s" % output_path)
 
-print("\n==> split_multi")
-vds = vds.annotate_variants_expr("va.originalAltAlleles=%s" % get_expr_for_orig_alt_alleles_set())
-vds = vds.split_multi()
-print("")
-pprint(vds.variant_schema)
-print("\n==> output: %s" % output_path)
-vds.write(output_path, overwrite=True)
+    vds = hc.import_vcf(vcf_path, force_bgz=True, min_partitions=10000)
 
 
+    #vds = hc.read("gs://seqr-reference-data/GRCh38/CADD/whole_genome_SNVs.liftover.GRCh38.vds")
+    #output_path = "gs://seqr-reference-data/GRCh38/CADD/whole_genome_SNVs.liftover.GRCh38.fixed.vds"
+
+    #vds = hc.read("gs://seqr-reference-data/GRCh38/CADD/InDels.liftover.GRCh38.vds")
+    #output_path = "gs://seqr-reference-data/GRCh38/CADD/InDels.liftover.GRCh38.fixed.vds"
+
+    print("\n==> split_multi")
+    vds = vds.annotate_variants_expr("va.originalAltAlleles=%s" % get_expr_for_orig_alt_alleles_set())
+    vds = vds.split_multi()
+    print("")
+    pprint(vds.variant_schema)
+    
+
+    vds.write(output_path, overwrite=True)
