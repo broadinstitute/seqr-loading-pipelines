3c29467313eba82c02c2a6018fa3c8e848255781
bw2 <weisburd@broadinstitute.org>
2017-09-17 14:40:06 -0400

    connect to ipython notebook - copied from https://github.com/Nealelab/cloudtools/blob/master/cloudtools/connect.py

---
+++ connect_to_cluster.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# This script is mostly copied from:
+#  https://github.com/Nealelab/cloudtools/blob/master/cloudtools/connect.py
+
+import argparse
+import os
+from subprocess import Popen, check_call
+
+parser = argparse.ArgumentParser()
+parser.add_argument('name', type=str, help='Cluster name.')
+parser.add_argument('--service', type=str,
+                    choices=['notebook', 'nb', 'spark-ui', 'ui', 'spark-ui1', 'ui1',
+                             'spark-ui2', 'ui2', 'spark-history', 'hist'],
+                    help='Web service to launch.', default="notebook")
+parser.add_argument('--port', '-p', default='10000', type=str,
+                    help='Local port to use for SSH tunnel to master node (default: %(default)s).')
+parser.add_argument('--zone', '-z', default='us-central1-b', type=str,
+                    help='Compute zone for Dataproc cluster (default: %(default)s).')
+args = parser.parse_args()
+
+print("Connecting to cluster '{}'...".format(args.name))
+
+# shortcut mapping
+shortcut = {
+    'ui': 'spark-ui',
+    'ui1': 'spark-ui1',
+    'ui2': 'spark-ui2',
+    'hist': 'history',
+    'nb': 'notebook'
+}
+
+service = args.service
+if service in shortcut:
+    service = shortcut[service]
+
+# Dataproc port mapping
+dataproc_ports = {
+    'spark-ui': 4040,
+    'spark-ui1': 4041,
+    'spark-ui2': 4042,
+    'spark-history': 18080,
+    'notebook': 8123
+}
+connect_port = dataproc_ports[service]
+
+# open SSH tunnel to master node
+cmd = [
+    'gcloud',
+    'compute',
+    'ssh',
+    '{}-m'.format(args.name),
+    '--zone={}'.format(args.zone),
+    '--ssh-flag=-D {}'.format(args.port),
+    '--ssh-flag=-N',
+    '--ssh-flag=-f',
+    '--ssh-flag=-n'
+]
+with open(os.devnull, 'w') as f:
+    check_call(cmd, stdout=f, stderr=f)
+
+# open Chrome with SOCKS proxy configuration
+cmd = [
+    r'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    'http://localhost:{}'.format(connect_port),
+    '--proxy-server=socks5://localhost:{}'.format(args.port),
+    '--host-resolver-rules=MAP * 0.0.0.0 , EXCLUDE localhost',
+    '--user-data-dir=/tmp/'
+]
+with open(os.devnull, 'w') as f:
+    Popen(cmd, stdout=f, stderr=f)
