25b5946e9dc92afbf86527de421a3e7f61a541fa
bw2 <weisburd@broadinstitute.org>
2017-09-20 09:01:29 -0400

    finished implementing

--- create_snapshot.py
+++ create_snapshot.py
@@ -14,11 +14,10 @@ logger.setLevel(logging.INFO)
 
 p = argparse.ArgumentParser()
 p.add_argument("-H", "--host", help="Elasticsearch node host or IP. To look this up, run: `kubectl describe nodes | grep Addresses`", required=True)
-p.add_argument("-p", "--port", help="Elasticsearch port", default=30001, type=int)  # 9200
+p.add_argument("-p", "--port", help="Elasticsearch port", default=9200, type=int)  # 9200
 p.add_argument("-b", "--bucket", help="Google bucket name", default="seqr-database-backups")
-
 p.add_argument("-r", "--repo", help="Repository name", default="elasticsearch-prod")
-
+p.add_argument("-i", "--index", help="Index name(s). One or more comma-separated index names to include in the snapshot", required=True)
 
 # parse args
 args = p.parse_args()
@@ -38,11 +37,11 @@ body = {
 es.snapshot.create_repository(repository=args.repo, body=body)
 
 # see https://www.elastic.co/guide/en/elasticsearch/reference/current/modules-snapshots.html
-snapshot_name = "snapshot_"+time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+snapshot_name = "snapshot_%s__%s" % (args.index, time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()))
 
-#es.snapshot.create(repository=args.repo, snapshot=snapshot_name, body={
-    #"indices": "index_1,index_2"
-#})
+es.snapshot.create(repository=args.repo, snapshot=snapshot_name, body={
+        "indices": args.index
+})
 
 print("Getting snapshot status for: " + snapshot_name)
 print(es.snapshot.status(repository=args.repo))
