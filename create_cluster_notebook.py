f6de67549dcb770a15da83aebc59d0ef6575f258
bw2 <weisburd@broadinstitute.org>
2017-10-23 08:59:54 -0400

    switch to default network

--- create_cluster_notebook.py
+++ create_cluster_notebook.py
@@ -3,14 +3,13 @@
 import argparse
 import os
 
-from machine_types import MACHINE_TYPES, get_cost
+from machine_types import get_cost, MACHINE_TYPES
 
 p = argparse.ArgumentParser()
 p.add_argument("-z", "--zone", default="us-central1-b")
-p.add_argument("-m", "--machine-type", default="n1-highmem-4", choices=MACHINE_TYPES)
+p.add_argument("-m", "--machine-type", default="n1-highmem-8", choices=MACHINE_TYPES)
 p.add_argument("-p", "--project", default="seqr-project")
-p.add_argument("--max-idle", help="The duration before cluster is auto-deleted after last job completes, such as 30m, 2h or 1d", default="10m")
-p.add_argument("cluster", nargs="?", default="vep-grch37")
+p.add_argument("cluster", nargs="?", default="notebook")
 p.add_argument("num_workers", nargs="?", help="num worker nodes", default=2, type=int)
 p.add_argument("num_preemptible_workers", nargs="?", help="num preemptible worker nodes", default=0, type=int)
 args = p.parse_args()
@@ -20,23 +19,25 @@ cost2 = get_cost(machine_type=args.machine_type, hours=1, is_preemptible=True) *
 print("$$$ cost: $%0.2f/h + $%0.2f preemptible/h = $%0.2f / hour total" % (cost1, cost2, cost1+cost2))
 
 # create cluster
-command = """gcloud beta dataproc clusters create %(cluster)s \
-    --max-idle %(max_idle)s \
-    --zone %(zone)s  \
-    --master-machine-type %(machine_type)s  \
+command = """gcloud dataproc clusters create %(cluster)s \
+    --zone %(zone)s \
+    --master-machine-type %(machine_type)s \
     --master-boot-disk-size 100  \
-    --num-workers %(num_workers)s  \
+    --num-workers %(num_workers)s \
     --num-preemptible-workers %(num_preemptible_workers)s \
     --project %(project)s \
     --worker-machine-type %(machine_type)s  \
     --worker-boot-disk-size 75 \
     --num-worker-local-ssds 1 \
     --image-version 1.1 \
-    --properties "spark:spark.driver.extraJavaOptions=-Xss4M,spark:spark.executor.extraJavaOptions=-Xss4M,spark:spark.driver.memory=45g,spark:spark.driver.maxResultSize=30g,spark:spark.task.maxFailures=20,spark:spark.yarn.executor.memoryOverhead=15g,spark:spark.kryoserializer.buffer.max=1g,hdfs:dfs.replication=1"  \
-    --initialization-actions gs://hail-common/hail-init.sh,gs://hail-common/vep/vep/GRCh37/vep85-GRCh37-init.sh
+    --network %(project)s-auto-vpc \
+    --properties "spark:spark.driver.extraJavaOptions=-Xss4M,spark:spark.executor.extraJavaOptions=-Xss4M,spark:spark.driver.memory=15g,spark:spark.driver.maxResultSize=30g,spark:spark.task.maxFailures=20,spark:spark.yarn.executor.memoryOverhead=15g,spark:spark.kryoserializer.buffer.max=1g,hdfs:dfs.replication=1"  \
+    --initialization-actions gs://hail-common/init_notebook.py
 """ % args.__dict__
-#    --network %(project)s-auto-vpc \
 
 print(command)
 os.system(command)
 
+command = "python connect_to_cluster.py %(cluster)s" % args.__dict__
+print(command)
+os.system(command)
