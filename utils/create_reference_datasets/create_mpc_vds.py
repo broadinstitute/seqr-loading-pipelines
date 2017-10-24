76c1040913b25dc71512c479687c5980641f3d7c
bw2 <weisburd@broadinstitute.org>
2017-10-24 18:54:02 -0400

    script for creating the mpc vds

---
+++ utils/create_reference_datasets/create_mpc_vds.py
@@ -0,0 +1,5 @@
+input_path = "gs://seqr-reference-data/GRCh37/MPC/fordist_constraint_official_mpc_values.vcf.gz"
+output_path = "gs://seqr-reference-data/GRCh37/MPC/fordist_constraint_official_mpc_values.vds"
+
+input_path = "gs://seqr-reference-data/GRCh38/MPC/fordist_constraint_official_mpc_values.liftover.GRCh38.vcf.gz"
+output_path = "gs://seqr-reference-data/GRCh38/MPC/fordist_constraint_official_mpc_values.liftover.GRCh38.vds"
