33985057f83e5771ae451b52861a1b960a961085
bw2 <weisburd@broadinstitute.org>
2017-10-30 00:52:02 -0400

    updated clinvar paths and schema

--- utils/add_clinvar.py
+++ utils/add_clinvar.py
@@ -2,58 +2,58 @@ from utils.vds_schema_string_utils import convert_vds_schema_string_to_annotate_
 
 
 CLINVAR_FIELDS = """
-    MEASURESET_TYPE: String,
-    MEASURESET_ID: String,
-    RCV: String,
-    ALLELE_ID: String,
-    CLINICAL_SIGNIFICANCE: String,
-    PATHOGENIC: String,
-    BENIGN: String,
-    CONFLICTED: String,
-    REVIEW_STATUS: String,
-    GOLD_STARS: String,
-    ALL_SUBMITTERS: String,
-    ALL_TRAITS: String,
-    ALL_PMIDS: String,
-    INHERITANCE_MODES: String,
-    AGE_OF_ONSET: String,
-    PREVALENCE: String,
-    DISEASE_MECHANISM: String,
-    ORIGIN: String,
-    XREFS: String
+    variation_type: String,
+    variation_id: String,
+    --- rcv: String,
+    --- scv: String,
+    allele_id: Int,
+    clinical_significance: String,
+    pathogenic: Int,
+    likely_pathogenic: Int,
+    uncertain_significance: Int,
+    likely_benign: Int,
+    benign: Int,
+    conflicted: Int
+    gold_stars: String,
+    review_status: String,
+    all_submitters: String,
+    all_traits: String,
+    all_pmids: String,
+    inheritance_modes: String,
+    age_of_onset: String,
+    prevalence: String,
+    disease_mechanism: String,
+    origin: String,
+    xrefs: String,
 """
-
 
 def add_clinvar_to_vds(hail_context, vds, genome_version, root="va.clinvar", info_fields=CLINVAR_FIELDS, subset=None, verbose=True):
     """Add clinvar annotations to the vds"""
 
     if genome_version == "37":
-        clinvar_single_vds = 'gs://seqr-reference-data/GRCh37/clinvar/clinvar_alleles.single.b37.vds'
-        clinvar_multi_vds = 'gs://seqr-reference-data/GRCh37/clinvar/clinvar_alleles.multi.b37.vds'
+        clinvar_vds_path = 'gs://seqr-reference-data/GRCh38/clinvar/clinvar_alleles.b37.vds'
     elif genome_version == "38":
-        clinvar_single_vds = 'gs://seqr-reference-data/GRCh38/clinvar/clinvar_alleles.single.b38.vds'
-        clinvar_multi_vds = 'gs://seqr-reference-data/GRCh38/clinvar/clinvar_alleles.multi.b38.vds'
+        clinvar_vds_path = 'gs://seqr-reference-data/GRCh38/clinvar/clinvar_alleles.b38.vds'
     else:
         raise ValueError("Invalid genome_version: " + str(genome_version))
 
-    for clinvar_vds_path in [clinvar_single_vds, clinvar_multi_vds]:
-        clinvar_vds = hail_context.read(clinvar_vds_path)
+    clinvar_vds = hail_context.read(clinvar_vds_path)
 
-        if subset:
-            import hail
-            clinvar_vds = clinvar_vds.filter_intervals(hail.Interval.parse(subset))
+    if subset:
+        import hail
+        clinvar_vds = clinvar_vds.filter_intervals(hail.Interval.parse(subset))
 
-        expr = convert_vds_schema_string_to_annotate_variants_expr(
-            root=root,
-            other_source_fields=info_fields,
-            other_source_root="vds.info",
-        )
+    expr = convert_vds_schema_string_to_annotate_variants_expr(
+        root=root,
+        other_source_fields=info_fields,
+        other_source_root="vds",
+    )
 
-        if verbose:
-            print(expr)
-            #print("\n==> clinvar vds summary: ")
-            #print("\n" + str(clinvar_vds.summarize()))
+    if verbose:
+        print(expr)
+        #print("\n==> clinvar vds summary: ")
+        #print("\n" + str(clinvar_vds.summarize()))
 
-        vds = vds.annotate_variants_vds(clinvar_vds, expr=expr)
+    vds = vds.annotate_variants_vds(clinvar_vds, expr=expr)
 
     return vds
