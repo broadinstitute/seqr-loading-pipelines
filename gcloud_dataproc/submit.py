f1b64f0ec107877b177bc116e92344fb5d06fa8b
bw2 <weisburd@broadinstitute.org>
2018-08-12 09:36:11 -0400

    add --run-locally option

--- gcloud_dataproc/submit.py
+++ gcloud_dataproc/submit.py
@@ -8,18 +8,13 @@ import socket
 p = argparse.ArgumentParser()
 p.add_argument("-p", "--project", default="seqr-project")
 p.add_argument("-c", "--cluster", default="no-vep")
+p.add_argument("--run-locally", default=os.environ.get("HAIL_HOME"),
+    help="Run using a local hail install instead of submitting to dataproc. "
+         "The value must should be the local hail directory (default: $HAIL_HOME). "
+         "Also, this assumes 'spark-submit' is on $PATH")
 p.add_argument("script")
 
 args, unparsed_args = p.parse_known_args()
-
-#hail_zip="gs://gnomad-bw2/hail-jar/hail-python.test.zip"
-#hail_jar="gs://gnomad-bw2/hail-jar/hail-all-spark.test.jar"
-
-#hail_zip="gs://gnomad-bw2/hail-jar/hail-python.test2.zip"
-#hail_jar="gs://gnomad-bw2/hail-jar/hail-all-spark.test2.jar"
-
-#hail_zip="gs://gnomad-bw2/hail-jar/hail-0.1-with-strip-chr-prefix.zip"
-#hail_jar="gs://gnomad-bw2/hail-jar/hail-0.1-with-strip-chr-prefix.jar"
 
 hail_zip = "gs://gnomad-bw2/hail-jar/hail-0.1-es-6.2.4-with-strip-chr-prefix.zip"
 hail_jar = "gs://gnomad-bw2/hail-jar/hail-0.1-es-6.2.4-with-strip-chr-prefix.jar"
@@ -36,9 +31,6 @@ hail_jar = "gs://gnomad-bw2/hail-jar/hail-0.1-es-6.2.4-with-strip-chr-prefix.jar
 #hail_jar="gs://hail-common/0.1-vep-debug.jar"
 
 
-
-project = args.project
-cluster = args.cluster
 script = args.script
 script_args = " ".join(['"%s"' % arg for arg in unparsed_args])
 
@@ -47,19 +39,30 @@ if "load_dataset_to_es_pipeline" in script:
     directory = "%s:%s" % (socket.gethostname(), os.getcwd())
     script_args += " --username '%(username)s' --directory '%(directory)s'" % locals()
 
-hail_scripts_zip = "/tmp/hail_scripts.zip"
 
-os.chdir(os.path.join(os.path.dirname(__file__), ".."))
-os.system("zip -r %(hail_scripts_zip)s hail_scripts download_and_create_reference_datasets/hail_scripts" % locals())
+if args.run_locally:
+    command = """spark-submit \
+        --jars /hail/build/libs/hail-all-spark.jar 
+        --py-files /hail/build/distributions/hail-python.zip 
+        "%(script)s" %(script_args)s
+    """ % locals()
+else:
+    project = args.project
+    cluster = args.cluster
 
-command = """gcloud dataproc jobs submit pyspark \
-  --project=%(project)s \
-  --cluster=%(cluster)s \
-  --files=%(hail_jar)s \
-  --py-files=%(hail_zip)s,%(hail_scripts_zip)s \
-  --properties="spark.files=./$(basename %(hail_jar)s),spark.driver.extraClassPath=./$(basename %(hail_jar)s),spark.executor.extraClassPath=./$(basename %(hail_jar)s)" \
-  "%(script)s" -- %(script_args)s
-""" % locals()
+    hail_scripts_zip = "/tmp/hail_scripts.zip"
+
+    os.chdir(os.path.join(os.path.dirname(__file__), ".."))
+    os.system("zip -r %(hail_scripts_zip)s hail_scripts download_and_create_reference_datasets/hail_scripts" % locals())
+
+    command = """gcloud dataproc jobs submit pyspark \
+      --project=%(project)s \
+      --cluster=%(cluster)s \
+      --files=%(hail_jar)s \
+      --py-files=%(hail_zip)s,%(hail_scripts_zip)s \
+      --properties="spark.files=./$(basename %(hail_jar)s),spark.driver.extraClassPath=./$(basename %(hail_jar)s),spark.executor.extraClassPath=./$(basename %(hail_jar)s)" \
+      "%(script)s" -- %(script_args)s
+    """ % locals()
 
 print(command)
 os.system(command)
