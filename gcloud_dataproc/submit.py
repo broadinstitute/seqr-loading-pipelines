9664a8228d0dc02105b0a1eb5e3762a6b4ee5229
bw2 <weisburd@broadinstitute.org>
2018-08-22 12:50:57 -0400

    updated local spark-submit config to match dataproc spark config

--- gcloud_dataproc/submit.py
+++ gcloud_dataproc/submit.py
@@ -48,6 +48,13 @@ if args.run_locally:
     hail_home = args.hail_home
     spark_home = args.spark_home
     command = """%(spark_home)s/bin/spark-submit \
+        --driver-memory 7G  \
+        --executor-memory 7G \
+        --conf spark.driver.extraJavaOptions=-Xss4M \
+        --conf spark.executor.extraJavaOptions=-Xss4M \
+        --conf spark.yarn.executor.memoryOverhead=15g \
+        --conf spark.driver.maxResultSize=30g \
+        --conf spark.kryoserializer.buffer.max=1g \
         --jars %(hail_home)s/build/libs/hail-all-spark.jar \
         --py-files %(hail_home)s/build/distributions/hail-python.zip \
         "%(script)s" %(script_args)s
