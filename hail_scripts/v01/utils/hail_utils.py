3c4f1f7eb4031e8c46bc3f9660e6f8d639d60270
bw2 <weisburd@broadinstitute.org>
2018-08-23 08:51:42 -0400

    added log statement

--- hail_scripts/v01/utils/hail_utils.py
+++ hail_scripts/v01/utils/hail_utils.py
@@ -1,7 +1,13 @@
 import hail
+import logging
 import time
 
+logger = logging.getLogger()
 
-def create_hail_context():
+
+def create_hail_context(verbose=True):
+    if verbose:
+        logger.info("\n==> create HailContext")
+
     return hail.HailContext(log="/hail_{}.log".format(time.strftime("%y%m%d_%H%M%S")))
 
