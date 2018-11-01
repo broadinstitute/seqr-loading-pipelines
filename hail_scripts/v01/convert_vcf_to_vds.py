915c266f8f650a28f1e18e08c1dde88c5acf8972
bw2 <weisburd@broadinstitute.org>
2018-11-01 10:18:41 -0400

    better support for importing multiple VCFs

--- hail_scripts/v01/convert_vcf_to_vds.py
+++ hail_scripts/v01/convert_vcf_to_vds.py
@@ -8,6 +8,7 @@ from hail_scripts.v01.utils.vds_utils import write_vds
 p = ap.ArgumentParser()
 p.add_argument("--sites-only", help="Ignore all genotype info in the vcf.", action="store_true")
 p.add_argument("--version", help="(optional) version string to put in VDS globals.version")
+p.add_argument("--output-vds", help="(optional) output vds path")
 p.add_argument("vcf_path", nargs="+")
 args = p.parse_args()
 
@@ -15,30 +16,34 @@ print(", ".join(args.vcf_path))
 
 hc = create_hail_context()
 
-for vcf_path in args.vcf_path:
-    print("\n")
-    print("==> import_vcf: %s" % vcf_path)
+vcf_path = ",".join(args.vcf_path)
+print("\n")
+print("==> import_vcf: %s" % vcf_path)
 
-    output_path = vcf_path.replace(".vcf", "").replace(".gz", "").replace(".bgz", "") + ".vds"
-    print("==> output: %s" % output_path)
+if args.output:
+    output_path = args.output_vds
+else:
+    output_path = vcf_path.replace(".vcf", "").replace(".gz", "").replace(".bgz", "").replace(".*", "").replace("*", "")+".vds"
 
-    if args.sites_only:
-        vds = hc.import_vcf(vcf_path, force_bgz=True, min_partitions=10000, drop_samples=True)
-    else:
-        vds = hc.import_vcf(vcf_path, force_bgz=True, min_partitions=10000)
+print("==> output: %s" % output_path)
 
-    vds = vds.annotate_global_expr('global.sourceFilePath = "{}"'.format(vcf_path))
-    if args.version:
-        vds = vds.annotate_global_expr('global.version = "{}"'.format(args.version))
+if args.sites_only:
+    vds = hc.import_vcf(vcf_path, force_bgz=True, min_partitions=10000, drop_samples=True)
+else:
+    vds = hc.import_vcf(vcf_path, force_bgz=True, min_partitions=10000)
 
-    print("\n==> split_multi")
-    vds = vds.annotate_variants_expr("va.originalAltAlleles=%s" % get_expr_for_orig_alt_alleles_set())
+vds = vds.annotate_global_expr('global.sourceFilePath = "{}"'.format(vcf_path))
+if args.version:
+    vds = vds.annotate_global_expr('global.version = "{}"'.format(args.version))
 
-    # ensure that va.wasSplit and va.aIndex are defined before calling split_multi() since split_multi() doesn't define these if all variants are bi-allelic
-    vds = vds.annotate_variants_expr('va.wasSplit=false, va.aIndex=1')
-    vds = vds.split_multi()
+print("\n==> split_multi")
+vds = vds.annotate_variants_expr("va.originalAltAlleles=%s" % get_expr_for_orig_alt_alleles_set())
 
-    print("")
-    pprint(vds.variant_schema)
+# ensure that va.wasSplit and va.aIndex are defined before calling split_multi() since split_multi() doesn't define these if all variants are bi-allelic
+vds = vds.annotate_variants_expr('va.wasSplit=false, va.aIndex=1')
+vds = vds.split_multi()
 
-    write_vds(vds, output_path)
+print("")
+pprint(vds.variant_schema)
+
+write_vds(vds, output_path)
