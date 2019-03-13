62ecea50858875ca772283f8dfd0d97bbc4e0ac4
Kevin Nguyen <knguyen142@gmail.com>
2019-03-13 11:00:53 -0400

    combining with gnomad wip

--- download_and_create_reference_datasets/v02/hail_scripts/write_combined_reference_data_ht.py
+++ download_and_create_reference_datasets/v02/hail_scripts/write_combined_reference_data_ht.py
@@ -4,123 +4,138 @@ import hail as hl
 
 OUTPUT_DIR = 'gs://seqr-kev/combined-test'
 
+
 CONFIG =  {
     '1kg': {
         '37': {
             'path': 'gs://seqr-reference-data/GRCh37/1kg/1kg.wgs.phase3.20130502.GRCh37_sites.ht',
-            'selection': {'AC': 'info.AC', 'AF': 'info.AF', 'AN': 'info.AN', 'POPMAX_AF': 'POPMAX_AF'},
+            'select': {'AC': 'info.AC#', 'AF': 'info.AF#', 'AN': 'info.AN', 'POPMAX_AF': 'POPMAX_AF'},
             'field_name': 'g1k',
         },
         '38': {
             'path': 'gs://seqr-reference-data/GRCh38/1kg/1kg.wgs.phase3.20170504.GRCh38_sites.ht',
-            'selection': {'AC': 'info.AC', 'AF': 'info.AF', 'AN': 'info.AN', 'POPMAX_AF': 'POPMAX_AF'},
+            'select': {'AC': 'info.AC#', 'AF': 'info.AF#', 'AN': 'info.AN', 'POPMAX_AF': 'POPMAX_AF'},
             'field_name': 'g1k',
         },
     },
     'cadd': {
         '38': {
             'path': 'gs://seqr-reference-data/GRCh38/CADD/CADD_snvs_and_indels.v1.4.ht',
-            'selection': ['PHRED'],
+            'select': ['PHRED'],
         },
         '37': {
             'path': 'gs://seqr-reference-data/GRCh37/CADD/CADD_snvs_and_indels.v1.4.ht',
-            'selection': ['PHRED'],
+            'select': ['PHRED'],
         },
     },
     'dbnsfp': {
         '37': {
             'path': 'gs://seqr-reference-data/GRCh37/dbNSFP/v2.9.3/dbNSFP2.9.3_variant.ht',
-            'selection': ['SIFT_pred', 'Polyphen2_HVAR_pred', 'MutationTaster_pred', 'FATHMM_pred', 'MetaSVM_pred', 'REVEL_score',
+            'select': ['SIFT_pred', 'Polyphen2_HVAR_pred', 'MutationTaster_pred', 'FATHMM_pred', 'MetaSVM_pred', 'REVEL_score',
                           'GERP_RS', 'phastCons100way_vertebrate'],
         },
         '38': {
             'path': 'gs://seqr-reference-data/GRCh38/dbNSFP/v3.5/dbNSFP3.5a_variant.ht',
-            'selection': ['SIFT_pred', 'Polyphen2_HVAR_pred', 'MutationTaster_pred', 'FATHMM_pred', 'MetaSVM_pred', 'REVEL_score',
+            'select': ['SIFT_pred', 'Polyphen2_HVAR_pred', 'MutationTaster_pred', 'FATHMM_pred', 'MetaSVM_pred', 'REVEL_score',
                           'GERP_RS', 'phastCons100way_vertebrate'],
         },
     },
     'eigen': {
         '37': {
             'path': 'gs://seqr-reference-data/GRCh37/eigen/EIGEN_coding_noncoding.grch37.ht',
-            'selection': {'Eigen_phred': 'info.Eigen-phred'},
+            'select': {'Eigen_phred': 'info.Eigen-phred'},
         },
         '38': {
             'path': 'gs://seqr-reference-data/GRCh38/eigen/EIGEN_coding_noncoding.liftover_grch38.ht',
-            'selection': {'Eigen_phred': 'info.Eigen-phred'},
+            'select': {'Eigen_phred': 'info.Eigen-phred'},
         },
     },
     'mpc': {
         '37': {
             'path': 'gs://seqr-reference-data/GRCh37/MPC/fordist_constraint_official_mpc_values.ht',
-            'selection': {'MPC': 'info.MPC'},
+            'select': {'MPC': 'info.MPC'},
         },
         '38': {
             'path': 'gs://seqr-reference-data/GRCh38/MPC/fordist_constraint_official_mpc_values.liftover.GRCh38.ht',
-            'selection': {'MPC': 'info.MPC'},
+            'select': {'MPC': 'info.MPC'},
         },
     },
     'primate_ai': {
         '37': {
             'path': 'gs://seqr-reference-data/GRCh37/primate_ai/PrimateAI_scores_v0.2.ht',
-            'selection': {'score': 'info.score'},
+            'select': {'score': 'info.score'},
         },
         '38': {
             'path': 'gs://seqr-reference-data/GRCh38/primate_ai/PrimateAI_scores_v0.2.liftover_grch38.ht',
-            'selection': {'score': 'info.score'},
+            'select': {'score': 'info.score'},
         },
     },
     'splice_ai': {
         '37': {
             'path': 'gs://seqr-reference-data/GRCh37/spliceai/spliceai_scores.ht',
-            'selection': {'delta_score': 'info.max_DS'},
+            'select': {'delta_score': 'info.max_DS'},
         },
         '38': {
             'path': 'gs://seqr-reference-data/GRCh38/spliceai/spliceai_scores.ht',
-            'selection': {'delta_score': 'info.max_DS'},
+            'select': {'delta_score': 'info.max_DS'},
         },
     },
     'topmed': {
         '37': {
             'path': 'gs://seqr-reference-data/GRCh37/TopMed/bravo-dbsnp-all.removed_chr_prefix.liftunder_GRCh37.ht',
-            'selection': {'AC': 'info.AC'},
+            'select': {'AC': 'info.AC#'},
         },
         '38': {
             'path': 'gs://seqr-reference-data/GRCh38/TopMed/bravo-dbsnp-all.ht',
-            'selection': {'AC': 'info.AC'},
+            'select': {'AC': 'info.AC#'},
         },
     },
     'gnomad_exome_coverage': {
         '37': {
             'path': 'gs://gnomad-public/release/2.1/coverage/exomes/gnomad.exomes.r2.1.coverage.ht',
-            'selection': {'AC': 'info.AC'},
         },
     },
     'gnomad_genome_coverage': {
         '37': {
             'path': 'gs://gnomad-public/release/2.1/coverage/genomes/gnomad.genomes.r2.1.coverage.ht',
-            'selection': {'AC': 'info.AC'},
+            'select': {'AC': 'info.AC'},
         },
     },
     'gnomad_exomes': {
         '37': {
             'path': 'gs://gnomad-public/release/2.1.1/ht/exomes/gnomad.exomes.r2.1.1.sites.ht',
-            'selection': {'AC': 'info.AC'},
+            'select': {},
+            'custom_select': 'custom_gnomad_select'
         },
     },
     'gnomad_genome': {
         '37': {
             'path': 'gs://gnomad-public/release/2.1.1/ht/genomes/gnomad.genomes.r2.1.1.sites.ht',
-            'selection': {'AC': 'info.AC'},
+            'select': {},
+            'custom_select': 'custom_gnomad_select'
         },
     },
     'exac': {
         '37': {
             'path': 'gs://seqr-reference-data/GRCh37/TopMed/bravo-dbsnp-all.removed_chr_prefix.liftunder_GRCh37.ht',
-            'selection': {'AC': 'info.AC'},
+            'select': {'AC': 'info.AC'},
         },
     },
 
 }
+
+def custom_gnomad_select(ht):
+    selects = {}
+    global_idx = hl.eval(ht.globals.freq_index_dict['gnomad'])
+    selects['AF'] = ht.freq[global_idx].AF
+    selects['AN'] = ht.freq[global_idx].AN
+    selects['AC'] = ht.freq[global_idx].AC
+    selects['hom'] = ht.freq[global_idx].homozygote_count
+
+    selects['POPMAX_AF'] = ht.popmax[ht.globals.popmax_index_dict['gnomad']].AF
+    selects['hemi'] = hl.cond(ht.locus.in_autosome_or_par(),
+                              0, ht.freq[ht.globals.freq_index_dict['gnomad_male']].AC)
+    return selects
 
 def get_mt(dataset, reference_genome):
     config = CONFIG[dataset][reference_genome]
@@ -128,15 +143,23 @@ def get_mt(dataset, reference_genome):
     base_ht = hl.read_table(config['path'])
 
     field_name = config.get('field_name') or dataset
-    if isinstance(config['selection'], list):
-        select_fields = {selection: base_ht[selection] for selection in config['selection'] }
-    elif isinstance(config['selection'], dict):
+    if isinstance(config['select'], list):
+        select_fields = {selection: base_ht[selection] for selection in config['select'] }
+    elif isinstance(config['select'], dict):
         select_fields = {}
-        for key, val in config['selection'].items():
+        for key, val in config['select'].items():
             ht = base_ht
             for attr in val.split('.'):
-                ht = ht[attr]
+                if attr.endswith('#'):
+                    attr = attr[:-1]
+                    ht = ht[attr][base_ht.a_index-1]
+                else:
+                    ht = ht[attr]
             select_fields[key] = ht
+
+    if 'custom_select' in config:
+        custom_select_fn_str = config['custom_select']
+        select_fields = {**select_fields, **globals()[custom_select_fn_str](base_ht)}
 
     print(select_fields)
     select_query = {
@@ -156,12 +179,13 @@ def join_mts(datasets, reference_genome):
             joined_mt = joined_mt.join(dataset_mt)
     joined_mt.describe()
     print(os.path.join(OUTPUT_DIR, 'combined-%s.ht' % '-'.join(datasets)))
-    # joined_mt.write(os.path.join(OUTPUT_DIR, 'combined%s.ht' % combined_str))
+    joined_mt.write(os.path.join(OUTPUT_DIR, 'combined%s.ht' % '-'.join(datasets)))
 
 def run():
-    join_mts(['1kg', 'mpc', 'cadd', 'eigen', 'dbnsfp', 'topmed', 'primate_ai', 'splice_ai'],
-             '37')
-    join_mts(['1kg', 'mpc', 'cadd', 'eigen', 'dbnsfp', 'topmed', 'primate_ai', 'splice_ai'],
-             '38')
+    # join_mts(['1kg'], '37')
+    join_mts(['1kg', 'mpc', 'cadd', 'eigen', 'dbnsfp', 'topmed', 'primate_ai', 'splice_ai', 'gnomad_genome', 'gnomad_exomes'],
+              '37')
+    # join_mts(['1kg', 'mpc', 'cadd', 'eigen', 'dbnsfp', 'topmed', 'primate_ai', 'splice_ai'],
+    #          '38')
 
 run()
