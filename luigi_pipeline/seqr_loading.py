c3f8d751235c198ce41b67848712affe370d257d
Mike Wilson <mwilson@broadinstitute.org>
2019-04-04 12:23:39 -0400

    Added remap and subsetting to seqr_loading.py

--- luigi_pipeline/seqr_loading.py
+++ luigi_pipeline/seqr_loading.py
@@ -3,7 +3,7 @@ import logging
 import luigi
 import hail as hl
 
-from lib.hail_tasks import HailMatrixTableTask, HailElasticSearchTask, GCSorLocalTarget
+from lib.hail_tasks import HailMatrixTableTask, HailElasticSearchTask, GCSorLocalTarget, MatrixTableSampleSetError
 from hail_scripts.v02.utils.computed_fields import variant_id
 from hail_scripts.v02.utils.computed_fields import vep
 
@@ -21,12 +21,17 @@ class SeqrVCFToMTTask(HailMatrixTableTask):
     sample_type = luigi.ChoiceParameter(choices=['WGS', 'WES'], description='Sample type, WGS or WES', var_type=str)
     validate = luigi.BoolParameter(default=True, description='Perform validation on the dataset.')
     dataset_type = luigi.ChoiceParameter(choices=['VARIANTS', 'SV'], default='VARIANTS', description='VARIANTS or SV.')
+    remap_path = luigi.OptionalParameter(default=None, description="Path to a file with two columns: s and seqr_id.")
+    subset_path = luigi.OptionalParameter(default=None, description="Path to a file with sample IDs, s, for subsetting MT.")
 
     def run(self):
         mt = self.import_vcf()
         if self.validate:
             self.validate_mt(mt, self.genome_version, self.sample_type)
-
+        if self.remap_path:
+            mt = self.remap_sample_ids(mt, self.remap_path)
+        if self.subset_path:
+            mt = self.subset_samples_and_variants(mt, self.subset_path)
         mt = hl.split_multi(mt)
         mt = HailMatrixTableTask.run_vep(mt, self.genome_version, self.vep_runner)
         mt = self.derive_fields(mt, self.dataset_type)
