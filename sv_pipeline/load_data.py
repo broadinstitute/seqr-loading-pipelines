e94cb8d851df465761cba17bee175ba6e941ceb0
Hana Snow <hsnow@broadinstitute.org>
2020-03-16 15:09:17 -0400

    add gene annotations

--- sv_pipeline/load_data.py
+++ sv_pipeline/load_data.py
@@ -4,6 +4,13 @@ import argparse
 import re
 from tqdm import tqdm
 
+
+GENCODE_CHR_COL_IDX = 0
+GENCODE_TYPE_COL_IDX = 2
+GENCODE_START_COL_IDX = 3
+GENCODE_END_COL_IDX = 4
+GENCODE_INFO_COL_IDX = 8
+
 CHR_COL = 'chr'
 START_COL = 'start'
 END_COL = 'end'
@@ -17,13 +24,16 @@ SC_COL = 'sc'
 SF_COL = 'sf'
 VAR_NAME_COL = 'var_name'
 
+CHROM_FIELD = 'contig'
 SAMPLE_ID_FIELD = 'sample_id'
 GENOTYPES_FIELD = 'genotypes'
+GENES_FIELD = 'geneIds'
+TRANSCRIPTS_FIELD = 'sortedTranscriptConsequences'
 
 BOOL_MAP = {'TRUE': True, 'FALSE': False}
 
 COL_CONFIGS = {
-    CHR_COL: {'field_name': 'contig', 'format': lambda val: val.lstrip('chr')},
+    CHR_COL: {'field_name': CHROM_FIELD, 'format': lambda val: val.lstrip('chr')},
     SC_COL: {'format': int},
     SF_COL: {'format': float},
     VAR_NAME_COL: {'field_name': 'variantId', 'format': lambda val, call='any': '{}_{}'.format(val, call)},
@@ -115,7 +125,73 @@ def subset_and_group_svs(input_dataset, sample_subset, ignore_missing_samples):
     return parsed_svs_by_name.values()
 
 
-def load_data(input_dataset, sample_subset_file, ignore_missing_samples=False):
+def add_transcripts(svs, gencode_file_path):
+    svs.sort(key=lambda sv: (
+        int(sv[CHROM_FIELD]) if sv[CHROM_FIELD].isdigit() else sv[CHROM_FIELD], sv[END_COL], sv[START_COL]
+    ))
+    for sv in svs:
+        sv[TRANSCRIPTS_FIELD] = []
+    current_sv_idx = 0
+    new_chrom = None
+
+    with open(gencode_file_path) as gencode_file:
+        for i, line in enumerate(tqdm(gencode_file, unit=' gencode records')):
+            line = line.rstrip('\r\n')
+            if not line or line.startswith('#'):
+                continue
+            fields = line.split('\t')
+
+            if fields[GENCODE_TYPE_COL_IDX] != 'transcript':
+                continue
+
+            chrom = fields[GENCODE_CHR_COL_IDX].lstrip('chr')
+            start = int(fields[GENCODE_START_COL_IDX])
+            end = int(fields[GENCODE_END_COL_IDX])
+            transcript_info = None
+
+            if new_chrom:
+                if chrom != new_chrom:
+                    continue
+                else:
+                    new_chrom = None
+
+            while chrom != svs[current_sv_idx][CHROM_FIELD]:
+                current_sv_idx += 1
+                if current_sv_idx == len(svs):
+                    # all SVs have been annotated
+                    return
+
+            while svs[current_sv_idx][END_COL] < start:
+                current_sv_idx += 1
+                if current_sv_idx == len(svs):
+                    # all SVs have been annotated
+                    return
+                if svs[current_sv_idx][CHROM_FIELD] != chrom:
+                    new_chrom = svs[current_sv_idx][CHROM_FIELD]
+                    break
+
+            if new_chrom:
+                continue
+
+            for sv in svs[current_sv_idx:]:
+                if sv[CHROM_FIELD] != chrom or sv[START_COL] > end:
+                    break
+                if not transcript_info:
+                    # Info field in the format "gene_id "ENSG00000223972.5"; transcript_id "ENST00000450305.2"; gene_type ..."
+                    info_fields = {
+                        x.strip().split()[0]: x.strip().split()[1].strip('"')
+                        for x in fields[GENCODE_INFO_COL_IDX].split(';') if x != ''
+                    }
+                    transcript_info = {
+                        'gene_id': info_fields['gene_id'].split('.')[0],
+                        'transcript_id': info_fields['transcript_id'].split('.')[0],
+                        'biotype': info_fields['gene_type'],
+                    }
+
+                sv[TRANSCRIPTS_FIELD].append(transcript_info)
+
+
+def load_data(input_dataset, sample_subset_file, gencode_file_path, ignore_missing_samples=False):
     sample_subset = get_sample_subset(sample_subset_file)
     print('Subsetting to {} samples'.format(len(sample_subset)))
     # TODO remap sample ids
@@ -123,6 +199,11 @@ def load_data(input_dataset, sample_subset_file, ignore_missing_samples=False):
     print('Parsing BED file')
     parsed_svs = subset_and_group_svs(input_dataset, sample_subset, ignore_missing_samples=ignore_missing_samples)
     print('Found {} SVs'.format(len(parsed_svs)))
+
+    print('Adding gene annotations')
+    add_transcripts(parsed_svs, gencode_file_path)
+    for sv in parsed_svs:
+        sv[GENES_FIELD] = list({transcript['gene_id'] for transcript in sv[TRANSCRIPTS_FIELD]})
 
     import json
     for sv in parsed_svs[:5]:
@@ -133,9 +214,10 @@ if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('input_dataset', help='input VCF or VDS')
     p.add_argument('--sample-subset')
+    p.add_argument('--gencode')
     p.add_argument('--ignore-missing-samples', action='store_true')
 
     args = p.parse_args()
 
-    load_data(args.input_dataset, args.sample_subset, ignore_missing_samples=args.ignore_missing_samples)
+    load_data(args.input_dataset, args.sample_subset, args.gencode, ignore_missing_samples=args.ignore_missing_samples)
 
