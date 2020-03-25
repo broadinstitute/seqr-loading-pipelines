f57700013cf178c6265c4a6578b3dffb91ce740b
Hana Snow <hsnow@broadinstitute.org>
2020-03-25 16:06:09 -0400

    fix xpos

--- sv_pipeline/load_data.py
+++ sv_pipeline/load_data.py
@@ -70,7 +70,7 @@ CHROMOSOMES = [
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21',
     '22', 'X', 'Y', 'M',
 ]
-CHROM_TO_XPOS_OFFSET = {chrom: 1 + i*int(1e9) for i, chrom in enumerate(CHROMOSOMES)}
+CHROM_TO_XPOS_OFFSET = {chrom: (1 + i)*int(1e9) for i, chrom in enumerate(CHROMOSOMES)}
 
 ES_TYPE_MAP = {
     int: 'integer',
