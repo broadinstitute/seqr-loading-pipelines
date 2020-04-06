55546281840a0538052e4b4a202f0e6900794e2e
bw2 <weisburd@broadinstitute.org>
2020-04-06 04:02:21 -0400

    Add timestamp

--- download_and_create_reference_datasets/v02/hail_scripts/write_clinvar_ht.py
+++ download_and_create_reference_datasets/v02/hail_scripts/write_clinvar_ht.py
@@ -7,9 +7,11 @@ for genome_version in ["37", "38"]:
 
     mt = download_and_import_latest_clinvar_vcf(genome_version)
 
+    timestamp = hl.eval(mt.version)
+
     ht = mt.rows()
     ht = ht.annotate(gold_stars=CLINVAR_GOLD_STARS_LOOKUP.get(hl.delimit(ht.info.CLNREVSTAT)))
 
     ht.describe()
 
-    write_ht(ht, CLINVAR_HT_PATH.format(genome_version=genome_version))
+    write_ht(ht, CLINVAR_HT_PATH.format(genome_version=genome_version).replace(".ht", ".") + timestamp + ".ht")
