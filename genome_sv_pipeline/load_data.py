57006e28892c76e29c2f00c514343f1c40ccbfba
Shifa Zhang <zhangshifa07504@gmail.com>
2021-03-22 11:51:00 -0400

    Added geneIds and other fields.

--- genome_sv_pipeline/load_data.py
+++ genome_sv_pipeline/load_data.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from tqdm import tqdm
 
 from sv_pipeline.load_data import get_sample_subset, get_sample_remap,\
-    CHROM_FIELD, SC_FIELD, SF_FIELD, VARIANT_ID_FIELD, CALL_FIELD, START_COL, \
+    CHROM_FIELD, SC_FIELD, SF_FIELD, GENES_FIELD, VARIANT_ID_FIELD, CALL_FIELD, START_COL, \
     END_COL, QS_FIELD, CN_FIELD, SAMPLE_ID_FIELD, GENOTYPES_FIELD
 
 CHR_ATTR = 'CHROM'
@@ -19,11 +19,30 @@ VAR_NAME_ATTR = 'ID'
 CALL_ATTR = 'ALT'
 START_ATTR = 'POS'
 END_ATTR = 'END'
-QS_ATTR = 'RD_GQ'
-CN_ATTR = 'RD_CN'
+GQ_ATTR = 'GQ'
+RD_GQ_ATTR = 'RD_GQ'
+RD_CN_ATTR = 'RD_CN'
 INFO_ATTR = 'INFO'
+FILTER_ATTR = 'FILTER'
+N_HET_ATTR = 'N_HET'
+N_HOMALT_ATTR = 'N_HOMALT'
+GNOMAND_SVS_ID_ATTR = 'gnomAD_V2_SVID'
+GNOMAND_SVS_AF_ATTR = 'gnomAD_V2_AF'
+CTX_TYPE_ATTR = 'CTX_TYPE'
+CHR2_ATTR = 'CHR2'
+END2_ATTR = 'END2'
+CN_ATTR = 'CN'
 
 SN_FIELD = 'sn'
+FILTER_FIELD = 'filters'
+N_HET_FIELD = 'sv_callset_Hemi'
+N_HOMALT_FIELD = 'sv_callset_Hom'
+GNOMAD_SVS_ID_FIELD = 'gnomad_svs_ID'
+GNOMAD_SVS_AF_FIELD = 'gnomad_svs_AF'
+SV_DETAIL_FIELD = 'svDetail'
+CHR2_FIELD = 'chr2'
+END2_FIELD = 'end2'
+RD_QS_FIELD = 'rd_qs'
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -34,15 +53,32 @@ COL_CONFIGS = {
     AF_ATTR: {'root_attr': INFO_ATTR, 'field_name': SF_FIELD},
     AN_ATTR: {'root_attr': INFO_ATTR, 'field_name': SN_FIELD},
     VAR_NAME_ATTR: {'field_name': VARIANT_ID_FIELD},
-    CALL_ATTR: {'field_name': CALL_FIELD, 'format': lambda val: val[0]},
-    START_ATTR: {'field_name': START_COL, 'format': int},
-    END_ATTR: {'root_attr': INFO_ATTR, 'field_name': END_COL, 'format': int},
-    QS_ATTR: {'root_attr': 'data', 'field_name': QS_FIELD, 'format': int, 'allow_missing': True},
-    CN_ATTR: {'root_attr': 'data', 'field_name': CN_FIELD, 'format': int, 'allow_missing': True},
+    CALL_ATTR: {'field_name': CALL_FIELD, 'format': lambda val: val[0].type},
+    CTX_TYPE_ATTR: {'root_attr': INFO_ATTR, 'field_name': SV_DETAIL_FIELD, 'allow_missing': True},
+    START_ATTR: {'field_name': START_COL},
+    END_ATTR: {'root_attr': INFO_ATTR, 'field_name': END_COL},
+    FILTER_ATTR: {'field_name': FILTER_FIELD, 'allow_missing': True, 'format': lambda val: val.remove('PASS') if 'PASS' in val else val},
+    N_HET_ATTR: {'root_attr': INFO_ATTR, 'field_name': N_HET_FIELD},
+    N_HOMALT_ATTR: {'root_attr': INFO_ATTR, 'field_name': N_HOMALT_FIELD},
+    GNOMAND_SVS_ID_ATTR: {'root_attr': INFO_ATTR, 'field_name': GNOMAD_SVS_ID_FIELD, 'allow_missing': True},
+    GNOMAND_SVS_AF_ATTR: {'root_attr': INFO_ATTR, 'field_name': GNOMAD_SVS_AF_FIELD, 'allow_missing': True},
+    CHR2_ATTR:  {'root_attr': INFO_ATTR, 'field_name': CHR2_FIELD, 'allow_missing': True},
+    END2_ATTR: {'root_attr': INFO_ATTR, 'field_name': END2_FIELD, 'allow_missing': True},
+    GQ_ATTR: {'root_attr': 'data', 'field_name': QS_FIELD, 'allow_missing': True},
+    RD_GQ_ATTR: {'root_attr': 'data', 'field_name': RD_QS_FIELD, 'allow_missing': True},
+    RD_CN_ATTR: {'root_attr': 'data', 'field_name': CN_FIELD, 'allow_missing': True},
+    CN_ATTR: {'root_attr': 'data', 'field_name': CN_FIELD, 'allow_missing': True},
 }
 
-CORE_COLUMNS = [CHR_ATTR, AC_ATTR, AF_ATTR, AN_ATTR, VAR_NAME_ATTR, CALL_ATTR, START_ATTR, END_ATTR]
-SAMPLE_COLUMNS = [QS_ATTR, CN_ATTR]
+GENES_COLUMNS = ['PROTEIN_CODING__LOF', 'LINCRNA__LOF', 'PROTEIN_CODING__DUP_LOF', 'LINCRNA__DUP_LOF',
+                 'PROTEIN_CODING__COPY_GAIN', 'LINCRNA__COPY_GAIN', 'PROTEIN_CODING__DUP_PARTIAL', 'LINCRNA__DUP_PARTIAL',
+                 'PROTEIN_CODING__MSV_EXON_OVR', 'LINCRNA__MSV_EXON_OVR', 'PROTEIN_CODING__INTRONIC', 'LINCRNA__INTRONIC',
+                 'PROTEIN_CODING__INV_SPAN', 'LINCRNA__INV_SPAN', 'PROTEIN_CODING__UTR', 'LINCRNA__UTR',
+                 'PROTEIN_CODING__PROMOTER']
+
+CORE_COLUMNS = [CHR_ATTR, AC_ATTR, AF_ATTR, AN_ATTR, VAR_NAME_ATTR, CALL_ATTR, CTX_TYPE_ATTR, START_ATTR, END_ATTR,
+                FILTER_ATTR, N_HET_ATTR, N_HOMALT_ATTR, GNOMAND_SVS_ID_ATTR, GNOMAND_SVS_AF_ATTR, CHR2_ATTR, END2_ATTR]
+SAMPLE_COLUMNS = [GQ_ATTR, RD_GQ_ATTR, CN_ATTR, RD_CN_ATTR]
 
 
 def get_field_val(row, col, format_kwargs=None):
@@ -54,15 +90,11 @@ def get_field_val(row, col, format_kwargs=None):
     :param format_kwargs: optional arguments to pass to the value formatter
     :return: parsed value
     """
-    root_attr = COL_CONFIGS[col].get('root_attr', col)
-    val = getattr(row, root_attr)
-    if val is None:
-        if COL_CONFIGS[col].get('allow_missing'):
-            return None
-        raise IndexError('Column "{}" is missing from row {}'.format(col, row))
-
-    if COL_CONFIGS[col].get('root_attr'):
-        val = val.get(col) if isinstance(val, dict) else getattr(val, col)
+    root_attr = getattr(row, COL_CONFIGS[col].get('root_attr', ''), None)
+    if root_attr:
+        val = root_attr.get(col) if isinstance(root_attr, dict) else getattr(root_attr, col, None)
+    else:
+        val = getattr(row, col, None)
     if val is None:
         if COL_CONFIGS[col].get('allow_missing'):
             return None
@@ -85,6 +117,15 @@ def get_parsed_column_values(row, columns):
     return {COL_CONFIGS[col].get('field_name', col): get_field_val(row, col) for col in columns}
 
 
+def parse_gene_ids(info):
+    gene_ids = []
+    keys = info.keys()
+    genes_cols = [col for col in GENES_COLUMNS if col in keys]
+    for col in genes_cols:
+        gene_ids += info.get(col)
+    return gene_ids
+
+
 def parse_sv_row(row, parsed_svs_by_id):
     """
     Parse the given row into the desired SV output format and add it to the dictionary of parsed SVs
@@ -94,17 +135,26 @@ def parse_sv_row(row, parsed_svs_by_id):
     :param sample_id: the sample id for the row
     :return: none
     """
-    parsed_row = get_parsed_column_values(row, CORE_COLUMNS)
-    variant_id = parsed_row.get(VARIANT_ID_FIELD)
-    parsed_svs_by_id[variant_id] = parsed_row
-
     samples = []
     for sample in row.samples:
+        if not sample.is_variant:
+            continue
         parsed_sample = get_parsed_column_values(sample, SAMPLE_COLUMNS)
         parsed_sample[SAMPLE_ID_FIELD] = sample.sample
         samples.append(parsed_sample)
 
+    if not samples:  # no sample has this variant
+        return False
+
+    parsed_row = get_parsed_column_values(row, CORE_COLUMNS)
+    variant_id = parsed_row.get(VARIANT_ID_FIELD)
+    parsed_svs_by_id[variant_id] = parsed_row
+
+    parsed_svs_by_id[variant_id][GENES_FIELD] = parse_gene_ids(row.INFO)
+
     parsed_svs_by_id[variant_id][GENOTYPES_FIELD] = samples
+
+    return True
 
 
 def load_file(file_path, parse_row, sample_subset, out_file_path=None):
@@ -157,12 +207,10 @@ def subset_and_group_svs(input_dataset, sample_subset, sample_remap, sample_type
     def _parse_row(row):
         samples = [sample for sample in row.samples if sample.sample in sample_subset]
         row.samples = samples
-        for sample in row.samples:
-            if sample.gt_alleles[0] != '0' or sample.gt_alleles[1] != '0':
-                parse_sv_row(row, parsed_svs_by_name)
-                return row
-        skipped_svs.add(row.ID)
-        return None
+        if not parse_sv_row(row, parsed_svs_by_name):
+            skipped_svs.add(row.ID)
+            return False
+        return True
 
     found_samples = load_file(input_dataset, _parse_row, sample_subset, out_file_path=out_file_path)
 
@@ -180,6 +228,7 @@ def subset_and_group_svs(input_dataset, sample_subset, sample_remap, sample_type
                 raise Exception(missing_sample_error)
 
     return parsed_svs_by_name
+
 
 def test_data_parsing(guid, input_dataset, sample_type='WGS'):
     sample_subset = get_sample_subset(guid, sample_type)
@@ -268,11 +317,11 @@ def get_all_sample_rows(guid):
 test_data_parsing('R0332_cmg_estonia_wgs', 'vcf/sv.vcf.gz')
 # Outputs:
 # INFO:__main__:Subsetting to 167 samples
-# 145568 rows [33:30, 72.39 rows/s]
+# 145568 rows [14:52, 163.09 rows/s]
 # INFO:__main__:Found 106 sample ids
 # INFO:__main__:Missing the following 61 samples:
 # E00859946, HK015_0036, HK015_0038_D2, HK017-0044, HK017-0045, HK017-0046, HK032_0081, HK032_0081_2_D2, HK035_0089, HK060-0154_1, HK060-0155_1, HK060-0156_1, HK061-0157_D1, HK061-0158_D1, HK061-0159_D1, HK079-001_D2, HK079-002_D2, HK079-003_D2, HK080-001_D2, HK080-002_D2, HK080-003_D2, HK081-001_D2, HK081-002_D2, HK081-003_D2, HK085-001_D2, HK085-002_D2, HK085-004_D2, HK085-006_D2, HK100-001_D1, HK100-002_D1, HK100-003_D1, HK100-004_D1, HK104-001_D2, HK104-002_D2, HK108-001_1, HK108-002_1, HK108-003_1, HK112-001_1, HK112-002_1, HK112-003_1, HK115-001_1, HK115-002_1, HK115-003_1, HK117-001_1, HK117-002_1, HK117-003_1, HK119-001_1, HK119-002_1, HK119-003_1, OUN_HK124_001_D1, OUN_HK124_002_D1, OUN_HK124_003_D1, OUN_HK126_001_D1, OUN_HK126_002_D1, OUN_HK126_003_D1, OUN_HK131_001_D1, OUN_HK131_002_D1, OUN_HK131_003_D1, OUN_HK132_001_D1, OUN_HK132_002_D1, OUN_HK132_003_D1
-# INFO:__main__:Found 67889 SVs
+# INFO:__main__:Found 67275 SVs
 
 #stat_sv_type()
 # Outputs:
