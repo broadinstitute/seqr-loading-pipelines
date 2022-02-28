bfcab508a49783ecf533d04ebde2e62a6ecc3078
Shifa Zhang <zhangshifa07504@gmail.com>
2022-02-28 16:56:10 -0500

    Use inheritance for Luigi tasks.

--- luigi_pipeline/seqr_loading_optimized.py
+++ luigi_pipeline/seqr_loading_optimized.py
@@ -1,13 +1,11 @@
 import logging
-import os
 import sys
-import re
 
 import luigi
 import hail as hl
 
-from lib.hail_tasks import HailMatrixTableTask, HailElasticSearchTask, GCSorLocalTarget, MatrixTableSampleSetError
-from lib.model.seqr_mt_schema import SeqrSchema, SeqrVariantSchema, SeqrGenotypesSchema, SeqrVariantsAndGenotypesSchema
+from lib.hail_tasks import HailMatrixTableTask, HailElasticSearchTask
+from lib.model.seqr_mt_schema import SeqrVariantSchema, SeqrGenotypesSchema, SeqrVariantsAndGenotypesSchema
 import seqr_loading
 
 logger = logging.getLogger(__name__)
@@ -23,14 +21,14 @@ class SeqrVCFToVariantMTTask(seqr_loading.SeqrVCFToMTTask):
         self.read_vcf_write_mt(schema_cls=SeqrVariantSchema)
 
 
-class SeqrVCFToGenotypesMTTask(HailMatrixTableTask):
+class BaseVCFToGenotypesMTTask(HailMatrixTableTask):
     remap_path = luigi.OptionalParameter(default=None,
                                          description="Path to a tsv file with two columns: s and seqr_id.")
     subset_path = luigi.OptionalParameter(default=None,
                                           description="Path to a tsv file with one column of sample IDs: s.")
 
     def requires(self):
-        return [SeqrVCFToVariantMTTask()]
+        return [self.VariantTask()]
 
     def run(self):
         mt = hl.read_matrix_table(self.input()[0].path)
@@ -40,31 +38,43 @@ class SeqrVCFToGenotypesMTTask(HailMatrixTableTask):
         if self.subset_path:
             mt = self.subset_samples_and_variants(mt, self.subset_path)
 
-        mt = SeqrGenotypesSchema(mt).annotate_all(overwrite=True).select_annotated_mt()
+        mt = self.GenotypesSchema(mt).annotate_all(overwrite=True).select_annotated_mt()
 
         mt.describe()
         mt.write(self.output().path, stage_locally=True, overwrite=True)
 
 
-class SeqrMTToESOptimizedTask(HailElasticSearchTask):
+class SeqrVCFToGenotypesMTTask(BaseVCFToGenotypesMTTask):
+    VariantTask = SeqrVCFToVariantMTTask
+    GenotypesSchema = SeqrGenotypesSchema
+
+
+class BaseMTToESOptimizedTask(HailElasticSearchTask):
 
     def __init__(self, *args, **kwargs):
         # TODO: instead of hardcoded index, generate from project_guid, etc.
         super().__init__(*args, **kwargs)
 
     def requires(self):
-        return [SeqrVCFToVariantMTTask(), SeqrVCFToGenotypesMTTask()]
+        return [self.VariantTask(), self.GenotypesTask()]
 
     def run(self):
         variants_mt = hl.read_matrix_table(self.input()[0].path)
         genotypes_mt = hl.read_matrix_table(self.input()[1].path)
+        genotypes_mt = genotypes_mt.drop(*[k for k in genotypes_mt.globals.keys()])
         row_ht = genotypes_mt.rows().join(variants_mt.rows())
 
-        row_ht = SeqrVariantsAndGenotypesSchema.elasticsearch_row(row_ht)
+        row_ht = self.VariantsAndGenotypesSchema.elasticsearch_row(row_ht)
         es_shards = self._mt_num_shards(genotypes_mt)
         self.export_table_to_elasticsearch(row_ht, es_shards)
         
         self.cleanup(es_shards)
+
+
+class SeqrMTToESOptimizedTask(BaseMTToESOptimizedTask):
+    VariantTask = SeqrVCFToVariantMTTask
+    GenotypesTask = SeqrVCFToGenotypesMTTask
+    VariantsAndGenotypesSchema = SeqrVariantsAndGenotypesSchema
 
 
 if __name__ == '__main__':
--- luigi_pipeline/seqr_mito_loading.py
+++ luigi_pipeline/seqr_mito_loading.py
@@ -6,32 +6,33 @@ import pprint
 import luigi
 import hail as hl
 
-from lib.hail_tasks import HailElasticSearchTask
-from lib.model.mito_mt_schema import SeqrMitoVariantsAndGenotypesSchema
-from sv_pipeline.utils.common import get_es_index_name
+from lib.model.mito_mt_schema import SeqrMitoVariantsAndGenotypesSchema, SeqrMitoVariantSchema, SeqrMitoGenotypesSchema
+from luigi_pipeline.seqr_loading_optimized import BaseVCFToGenotypesMTTask, BaseMTToESOptimizedTask
 import seqr_loading
 
 logger = logging.getLogger(__name__)
 
 
-class SeqrMitoMTTask(seqr_loading.SeqrVCFToMTTask):
+class SeqrMitoVariantMTTask(seqr_loading.SeqrVCFToMTTask):
+    """
+    Loads all annotations for the variants of a VCF (MT) into a Hail Table (parent class of MT is a misnomer).
+    """
     high_constraint_interval_path = luigi.Parameter(description='Path to the tsv file storing the high constraint intervals.')
 
-    def read_annotate_write_mt(self, schema_cls=SeqrMitoVariantsAndGenotypesSchema):
+    def read_mt_write_mt(self, schema_cls=SeqrMitoVariantsAndGenotypesSchema):
         logger.info("Args:")
         pprint.pprint(self.__dict__)
 
         mt = hl.read_matrix_table(self.source_paths[0])
-
-        #Drop unwanted global fields
-        mt = mt.drop('dbsnp_version',
+        mt = mt.drop('dbsnp_version', 'vep_version',
             'dp_hist_all_variants_n_larger', 'mq_hist_all_variants_bin_edges', 'dp_hist_all_variants_n_larger',
             'age_hist_all_samples_n_smaller', 'age_hist_all_samples_bin_freq', 'age_hist_all_samples_n_larger',
             'tlod_hist_all_variants_bin_freq', 'dp_hist_all_variants_bin_freq', 'mq_hist_all_variants_bin_freq',
             'col_annotation_descriptions', 'dp_hist_all_variants_bin_edges', 'mq_hist_all_variants_n_larger',
             'age_hist_all_samples_bin_edges', 'tlod_hist_all_variants_bin_edges', 'pop_order', 'hap_order',
             'global_annotation_descriptions', 'row_annotation_descriptions', 'tlod_hist_all_variants_n_larger')
-
+        if not self.dont_validate:
+            self.validate_mt(mt, self.genome_version, self.sample_type)
         if self.remap_path:
             mt = self.remap_sample_ids(mt, self.remap_path)
         if self.subset_path:
@@ -53,25 +54,19 @@ class SeqrMitoMTTask(seqr_loading.SeqrVCFToMTTask):
         mt.write(self.output().path, stage_locally=True, overwrite=True)
 
     def run(self):
-        self.read_annotate_write_mt()
+        # We only want to use the Variant Schema.
+        self.read_mt_write_mt(schema_cls=SeqrMitoVariantSchema)
 
 
-class SeqrMitoMTToESTask(HailElasticSearchTask):
-    project_guid = luigi.Parameter(description='Project GUID.', default=None)
+class SeqrMitoGenotypesMTTask(BaseVCFToGenotypesMTTask):
+    VariantTask = SeqrMitoVariantMTTask
+    GenotypesSchema = SeqrMitoGenotypesSchema
 
-    def requires(self):
-        return [SeqrMitoMTTask()]
 
-    def run(self):
-        mt = hl.read_matrix_table(self.input()[0].path)
-
-        row_ht = SeqrMitoVariantsAndGenotypesSchema.elasticsearch_row(mt.rows())
-        if self.project_guid:
-            self.es_index = get_es_index_name(f'{self.project_guid}__mito', mt.globals_table().take(1)[0])
-        es_shards = self._mt_num_shards(mt)
-        self.export_table_to_elasticsearch(row_ht, es_shards)
-
-        self.cleanup(es_shards)
+class SeqrMitoMTToESTask(BaseMTToESOptimizedTask):
+    VariantTask = SeqrMitoVariantMTTask
+    GenotypesTask = SeqrMitoGenotypesMTTask
+    VariantsAndGenotypesSchema = SeqrMitoVariantsAndGenotypesSchema
 
 
 if __name__ == '__main__':
