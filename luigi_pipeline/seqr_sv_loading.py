7635da08749f6d234e613b49519e461a07bce4f0
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-02-21 14:54:48 -0500

    some typos

--- luigi_pipeline/seqr_sv_loading.py
+++ luigi_pipeline/seqr_sv_loading.py
@@ -4,7 +4,7 @@ import sys
 import luigi
 import hail as hl
 
-from lib.model.mito_mt_schema import SeqrSVVariantSchema, SeqrSVGenotypesSchema, 
+from lib.model.sv_mt_schema import SeqrSVVariantSchema, SeqrSVGenotypesSchema, SeqrSVVariantsAndGenotypesSchema
 from luigi_pipeline.seqr_loading_optimized import SeqrVCFToVariantMTTask
 from sv_pipeline.genome.utils.mapping_gene_ids import load_gencode
 
