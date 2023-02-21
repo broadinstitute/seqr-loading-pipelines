a9c9d7720514de61990993946396d7f81b354a51
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-02-21 12:15:55 -0500

    ws

--- luigi_pipeline/seqr_sv_loading.py
+++ luigi_pipeline/seqr_sv_loading.py
@@ -44,8 +44,6 @@ class SeqrSVMTToESTask(BaseMTToESOptimizedTask):
     VariantsAndGenotypesSchema = SeqrSVVariantsAndGenotypesSchema
 
 
-
-
 if __name__ == '__main__':
     # If run does not succeed, exit with 1 status code.
     luigi.run() or sys.exit(1)
