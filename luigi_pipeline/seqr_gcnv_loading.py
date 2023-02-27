d31737f7f23436d4293a9c1ae72ac1eb79b26720
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-02-27 15:09:07 -0500

    just use a custom run function

--- luigi_pipeline/seqr_gcnv_loading.py
+++ luigi_pipeline/seqr_gcnv_loading.py
@@ -26,9 +26,21 @@ class SeqrGCNVVariantMTTask(SeqrVCFToVariantMTTask):
 
     is_new_joint_call = luigi.BoolParameter(default=False, description='Is this a fully joint-called callset.')
 
-    def import_dataset(self):
+    def run(self):
         ht = hl.import_table(self.source_paths[0], impute=True)
-        return ht.to_matrix_table(row_key=['variant_name', 'svtype'], col_key=['sample_fix'])
+        ht.to_matrix_table(row_key=['variant_name', 'svtype'], col_key=['sample_fix'])
+
+        if self.remap_path:
+            mt = self.remap_sample_ids(mt, self.remap_path)
+        if self.subset_path:
+            mt = self.subset_samples_and_variants(mt, self.subset_path)
+        
+        kwargs = self.get_schema_class_kwargs()
+        mt = self.SCHEMA_CLASS(mt, **kwargs).annotate_all(overwrite=True).select_annotated_mt()
+        mt = self.annotate_globals(mt)
+        
+        mt.describe()
+        mt.write(self.output().path, stage_locally=True, overwrite=True)       
 
     def get_schema_class_kwargs(self):
         return {
