7cdcc90cbefa809e6ad680c7f549c25577358819
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-03-13 12:29:02 -0400

    update comment

--- luigi_pipeline/lib/model/seqr_mt_schema.py
+++ luigi_pipeline/lib/model/seqr_mt_schema.py
@@ -348,6 +348,6 @@ class SeqrVariantsAndGenotypesSchema(SeqrVariantSchema, SeqrGenotypesSchema):
         table = ds.drop('vep').flatten()
         # When flattening, the table is unkeyed, which causes problems because our row keys should not
         # be normal fields. We can also re-key, but I believe this is computational?
-        # PS: locus and alleles used to be the only possible row keys... however we're extending to drop any row key!
+        # PS: row key is often locus and allele, but does not have to be
         row_key = table.row_key
         return table.drop(*row_key)
