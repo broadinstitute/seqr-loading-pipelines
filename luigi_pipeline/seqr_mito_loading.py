86ab54fc207c8b34ef0ae74ac03693e0e2e014a8
Benjamin Blankenmeister <b.p.blankenmeister@gmail.com>
2023-03-31 09:25:58 -0400

    Update seqr_mito_loading.py

--- luigi_pipeline/seqr_mito_loading.py
+++ luigi_pipeline/seqr_mito_loading.py
@@ -23,7 +23,7 @@ class SeqrMitoVariantMTTask(SeqrVCFToVariantMTTask):
     """
     Loads all annotations for the variants of a Matrix Table into a Hail Table.
     """
-    dataset_type = "MITO"
+    dataset_type = 'MITO'
     high_constraint_interval_path = luigi.Parameter(description='Path to the tsv file storing the high constraint intervals.')
     RUN_VEP = False
     SCHEMA_CLASS = SeqrMitoVariantSchema
