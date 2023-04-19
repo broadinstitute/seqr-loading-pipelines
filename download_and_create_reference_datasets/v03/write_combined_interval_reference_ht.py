d14815ec7649d3bb9985e2b0e88b69cb53d08428
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-04-19 11:02:35 -0400

    only a single script :facepalm:

--- download_and_create_reference_datasets/v03/write_combined_interval_reference_ht.py
+++ download_and_create_reference_datasets/v03/write_combined_interval_reference_ht.py
@@ -4,7 +4,7 @@ import os
 
 import hail as hl
 
-from hail_scripts.reference_data.combine import join_hts
+from hail_scripts.reference_data.combine import join_hts, update_joined_ht_globals
 from hail_scripts.reference_data.config import GCS_PREFIXES, SCREEN_REGION_TYPE_LOOKUP
 from hail_scripts.utils.hail_utils import write_ht
 
@@ -12,20 +12,30 @@ DATASETS = ['gnomad_non_coding_constraint', 'screen']
 INTERVAL_REFERENCE_HT_PATH = 'combined_interval_reference/combined_interval_reference.GRCh{genome_version}.ht'
 VERSION = '1.0.0'
 
-def run(environment: str):
-    genome_version = '38'
-    ht = join_hts(
+def update_existing(destination_path: str, dataset: str, genome_version: str):
+    destination_ht = import_table(destination_path)
+    destination_ht = destination_ht.transmute(**{dataset: dataset_ht[destination_ht.key][dataset]})
+    return update_joined_ht_globals(destination_ht, DATASETS, VERSION, [], genome_version) 
+
+def create_new(genome_version: str):
+    return join_hts(
         DATASETS,
         VERSION,
         reference_genome=genome_version,
     )
+
+def run(environment: str):
+    genome_version = '38'
     destination_path = os.path.join(GCS_PREFIXES[environment], INTERVAL_REFERENCE_HT_PATH).format(
         environment=environment,
         genome_version=genome_version,
     )
+    if hl.hadoop_exists(os.path.join(destination_path, "_SUCCESS")):
+        ht = update_existing(destination_path, dataset, genome_version)
+    else:
+        ht = create_new(genome_version)
     print(f'Uploading ht to {destination_path}')
     write_ht(ht, destination_path)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -34,5 +44,10 @@ if __name__ == "__main__":
         default='dev',
         choices=['dev', 'prod']
     )
+    parser.add_argument(
+        '--dataset',
+        choices=DATASETS,
+        required=True,
+    )
     args, _ = parser.parse_known_args()
-    run(args.environment)
+    run(args.environment, args.dataset)
