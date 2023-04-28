11f4d8f379f41183e9d5ae64a6a3dfcbee95a802
Benjamin Blankenmeister <b.p.blankenmeister@gmail.com>
2023-04-28 11:58:01 -0400

    Add linting over new code (#452)
    * move clinvar to reference data
    
    * formatting
    
    * forgot a dir!

--- hail_scripts/reference_data/clinvar_test.py
+++ hail_scripts/reference_data/clinvar_test.py
@@ -12,11 +12,25 @@ class ClinvarTest(unittest.TestCase):
     def test_parsed_clnsig(self):
         ht = hl.Table.parallelize(
             [
-               {'info': hl.Struct(CLNSIG=['Pathogenic|Affects'])},
-               {'info': hl.Struct(CLNSIG=['Pathogenic/Likely_pathogenic/Pathogenic', '_low_penetrance'])},
-               {'info': hl.Struct(CLNSIG=['Likely_pathogenic/Pathogenic', '_low_penetrance|association|protective'])},
-               {'info': hl.Struct(CLNSIG=['Likely_pathogenic', '_low_penetrance'])},
-               {'info': hl.Struct(CLNSIG=['association|protective'])},
+                {'info': hl.Struct(CLNSIG=['Pathogenic|Affects'])},
+                {
+                    'info': hl.Struct(
+                        CLNSIG=[
+                            'Pathogenic/Likely_pathogenic/Pathogenic',
+                            '_low_penetrance',
+                        ],
+                    ),
+                },
+                {
+                    'info': hl.Struct(
+                        CLNSIG=[
+                            'Likely_pathogenic/Pathogenic',
+                            '_low_penetrance|association|protective',
+                        ],
+                    ),
+                },
+                {'info': hl.Struct(CLNSIG=['Likely_pathogenic', '_low_penetrance'])},
+                {'info': hl.Struct(CLNSIG=['association|protective'])},
             ],
             hl.tstruct(info=hl.tstruct(CLNSIG=hl.tarray(hl.tstr))),
         )
@@ -34,8 +48,15 @@ class ClinvarTest(unittest.TestCase):
     def test_parsed_clnsigconf(self):
         ht = hl.Table.parallelize(
             [
-               {'info': hl.Struct(CLNSIGCONF=hl.missing(hl.tarray(hl.tstr)))},
-               {'info': hl.Struct(CLNSIGCONF=['Pathogenic(8)|Likely_pathogenic(2)|Pathogenic', '_low_penetrance(1)|Uncertain_significance(1)'])},
+                {'info': hl.Struct(CLNSIGCONF=hl.missing(hl.tarray(hl.tstr)))},
+                {
+                    'info': hl.Struct(
+                        CLNSIGCONF=[
+                            'Pathogenic(8)|Likely_pathogenic(2)|Pathogenic',
+                            '_low_penetrance(1)|Uncertain_significance(1)',
+                        ],
+                    ),
+                },
             ],
             hl.tstruct(info=hl.tstruct(CLNSIGCONF=hl.tarray(hl.tstr))),
         )
@@ -47,8 +68,6 @@ class ClinvarTest(unittest.TestCase):
                     hl.Struct(count=9, pathogenicity_id=0),
                     hl.Struct(count=2, pathogenicity_id=4),
                     hl.Struct(count=1, pathogenicity_id=11),
-                ]
+                ],
             ],
         )
-
-
