9f3535f69f0945d4523d3076c00fb87d910e9707
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-05-14 17:00:36 -0400

    Fresh eyes cleanup

--- v03_pipeline/lib/misc/pedigree.py
+++ v03_pipeline/lib/misc/pedigree.py
@@ -14,27 +14,27 @@ def import_pedigree(pedigree_path: str) -> hl.Table:
         },
     )
     ht = ht.select(
-        s=ht.Individual_ID,
         family_id=ht.Family_ID,
+        s=ht.Individual_ID,
     )
-    return ht.key_by(ht.s)
+    return ht.key_by(ht.family_id)
 
 def families_missing_samples(pedigree_ht: hl.Table, samples_ht: hl.Table) -> hl.Table:
-    ht = pedigree_ht.anti_join(samples_ht)
-    ht = ht.join(pedigree_ht)
+    ht = pedigree_ht.key_by(pedigree_ht.s).anti_join(samples_ht)
     ht = ht.key_by(ht.family_id)
     ht = ht.distinct()
     return ht.select()
 
 def families_to_include(pedigree_ht: hl.Table, samples_ht: hl.Table) -> hl.Table:
-    pt = pedigree_ht.key_by(pedigree_ht.family_id)
-    ht = families_missing_samples(pedigree_ht, samples_ht)
-    ht = pt.anti_join(ht)
+    ht = pedigree_ht.anti_join(
+        families_missing_samples(pedigree_ht, samples_ht)
+    )
     ht = ht.distinct()
     return ht.select()
 
 def samples_to_include(pedigree_ht: hl.Table, samples_ht: hl.Table) -> hl.Table:
-    pt = pedigree_ht.key_by(pedigree_ht.family_id)
-    ht = families_to_include(pedigree_ht, samples_ht)
-    ht = pt.join(ht)
-    return ht.select(ht.s)
+    ht = pedigree_ht.join(
+        families_to_include(pedigree_ht, samples_ht)
+    )
+    ht = ht.key_by(ht.s)
+    return ht.select()
