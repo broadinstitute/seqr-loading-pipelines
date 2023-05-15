08577797a9c99e7e81b351d62392aceb4248e3ac
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-05-15 10:45:18 -0400

    lint

--- v03_pipeline/lib/misc/pedigree.py
+++ v03_pipeline/lib/misc/pedigree.py
@@ -19,22 +19,21 @@ def import_pedigree(pedigree_path: str) -> hl.Table:
     )
     return ht.key_by(ht.family_id)
 
+
 def families_to_exclude(pedigree_ht: hl.Table, samples_ht: hl.Table) -> hl.Table:
     ht = pedigree_ht.key_by(pedigree_ht.s).anti_join(samples_ht)
     ht = ht.key_by(ht.family_id)
     ht = ht.distinct()
     return ht.select()
 
+
 def families_to_include(pedigree_ht: hl.Table, samples_ht: hl.Table) -> hl.Table:
-    ht = pedigree_ht.anti_join(
-        families_to_exclude(pedigree_ht, samples_ht)
-    )
+    ht = pedigree_ht.anti_join(families_to_exclude(pedigree_ht, samples_ht))
     ht = ht.distinct()
     return ht.select()
 
+
 def samples_to_include(pedigree_ht: hl.Table, samples_ht: hl.Table) -> hl.Table:
-    ht = pedigree_ht.join(
-        families_to_include(pedigree_ht, samples_ht)
-    )
+    ht = pedigree_ht.join(families_to_include(pedigree_ht, samples_ht))
     ht = ht.key_by(ht.s)
     return ht.select()
--- v03_pipeline/lib/misc/pedigree_test.py
+++ v03_pipeline/lib/misc/pedigree_test.py
@@ -14,10 +14,9 @@ TEST_PEDIGREE_2 = 'v03_pipeline/var/test/test_pedigree_2.tsv'
 
 
 class DownloadUtilsTest(unittest.TestCase):
-
     def test_empty_pedigree(self) -> None:
         with self.assertRaises(ValueError):
-            pedigree_ht = import_pedigree(TEST_PEDIGREE_1)
+            _ = import_pedigree(TEST_PEDIGREE_1)
 
     def test_parse_project(self) -> None:
         pedigree_ht = import_pedigree(TEST_PEDIGREE_2)
@@ -41,11 +40,17 @@ class DownloadUtilsTest(unittest.TestCase):
         )
 
         self.assertCountEqual(
-            [f.family_id for f in families_to_exclude(pedigree_ht, samples_ht).collect()],
+            [
+                f.family_id
+                for f in families_to_exclude(pedigree_ht, samples_ht).collect()
+            ],
             ['BBL_HT-007-5195'],
-        ) 
+        )
         self.assertCountEqual(
-            [f.family_id for f in families_to_include(pedigree_ht, samples_ht).collect()],
+            [
+                f.family_id
+                for f in families_to_include(pedigree_ht, samples_ht).collect()
+            ],
             [
                 'BBL_SDS1-000178',
                 'BBL_BC1-000345',
@@ -59,5 +64,4 @@ class DownloadUtilsTest(unittest.TestCase):
                 'BBL_BC1-000345_02_D1',
                 'BBL_BC1-000345_03_D1',
             ],
-        )         
-
+        )
