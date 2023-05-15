f9da85334ac1491ad891c7c2692b7983612f5033
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-05-15 13:35:48 -0400

    checkpoint

--- rename_ref_data.py
+++ rename_ref_data.py
@@ -2,20 +2,22 @@ import hail as hl
 
 ht = hl.read_table('gs://seqr-reference-data/GRCh37/all_reference_data/combined_reference_data_grch37.ht')
 ht = ht.drop('g1k')
-ht.transmute(
+ht = ht.transmute(
     gnomad_genome_coverage=hl.Struct(x10=ht.gnomad_genome_coverage),
     gnomad_exome_coverage=hl.Struct(x10=ht.gnomad_exome_coverage),
 )
-ht.annotate_globals(datasets=ht.datasets.remove('1kg'))
+ht = ht.annotate_globals(datasets=ht.datasets.remove('1kg'))
+ht = ht.checkpoint('/tmp/37.ht')
 ht.write('gs://seqr-reference-data/GRCh37/all_reference_data/combined_reference_data_grch37.ht', overwrite=True)
 
 
 ht = hl.read_table('gs://seqr-reference-data/GRCh38/all_reference_data/combined_reference_data_grch38.ht')
 ht = ht.drop('g1k')
-ht.transmute(
+ht = ht.transmute(
     gnomad_genome_coverage=hl.Struct(x10=ht.gnomad_genome_coverage),
     gnomad_exome_coverage=hl.Struct(x10=ht.gnomad_exome_coverage),
 )
-ht.annotate_globals(datasets={k: v for k, v in ht.datasets.collect()[0].items() if k!='1kg'})
-ht.annotate_globals(version='2.0.5')
+ht = ht.annotate_globals(datasets={k: v for k, v in ht.datasets.collect()[0].items() if k!='1kg'})
+ht = ht.annotate_globals(version='2.0.5')
+ht = ht.checkpoint('/tmp/38.ht')
 ht.write('gs://seqr-reference-data/GRCh38/all_reference_data/combined_reference_data_grch38.ht', overwrite=True)
