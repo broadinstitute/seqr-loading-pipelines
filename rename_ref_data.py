ee96d5e99d1e37166773ba09be7296ce90e28ad2
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-05-15 14:40:30 -0400

    write to scratch temp

--- rename_ref_data.py
+++ rename_ref_data.py
@@ -7,7 +7,7 @@ ht = ht.transmute(
     gnomad_exome_coverage=hl.Struct(x10=ht.gnomad_exome_coverage),
 )
 ht = ht.annotate_globals(datasets=ht.datasets.remove('1kg'))
-ht = ht.checkpoint('/tmp/37.ht')
+ht = ht.checkpoint('gs://seqr-scratch-temp/37.ht')
 ht.write('gs://seqr-reference-data/GRCh37/all_reference_data/combined_reference_data_grch37.ht', overwrite=True)
 
 
@@ -19,5 +19,5 @@ ht = ht.transmute(
 )
 ht = ht.annotate_globals(datasets={k: v for k, v in ht.datasets.collect()[0].items() if k!='1kg'})
 ht = ht.annotate_globals(version='2.0.5')
-ht = ht.checkpoint('/tmp/38.ht')
+ht = ht.checkpoint('gs://seqr-scratch-temp/38.ht')
 ht.write('gs://seqr-reference-data/GRCh38/all_reference_data/combined_reference_data_grch38.ht', overwrite=True)
