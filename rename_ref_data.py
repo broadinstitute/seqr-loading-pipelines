0a653787fe16278d881424d19b5cddf8f11eb9ca
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-05-15 12:48:23 -0400

    temp script to rename reference data

---
+++ rename_ref_data.py
@@ -0,0 +1,20 @@
+import hail as hl
+
+ht = hl.read_table('gs://seqr-reference-data/GRCh37/all_reference_data/combined_reference_data_grch37.ht')
+ht = ht.drop('g1k')
+ht.transmute(
+    gnomad_genome_coverage=hl.Struct(x10=ht.gnomad_genome_coverage),
+    gnomad_exome_coverage=hl.Struct(x10=ht.gnomad_exome_coverage),
+)
+ht.annotate_globals(datasets=ht.datasets.remove('1kg'))
+ht.write('gs://seqr-reference-data/GRCh37/all_reference_data/combined_reference_data_grch37.ht', overwrite=True)
+
+
+ht = hl.read_table('gs://seqr-reference-data/GRCh38/all_reference_data/combined_reference_data_grch38.ht')
+ht = ht.drop('g1k')
+ht.transmute(
+    gnomad_genome_coverage=hl.Struct(x10=ht.gnomad_genome_coverage),
+    gnomad_exome_coverage=hl.Struct(x10=ht.gnomad_exome_coverage),
+)
+ht.annotate_globals(datasets={k: v for k, v in ht.datasets.collect()[0].items() if k!='1kg'})
+ht.write('gs://seqr-reference-data/GRCh38/all_reference_data/combined_reference_data_grch38.ht', overwrite=True)
