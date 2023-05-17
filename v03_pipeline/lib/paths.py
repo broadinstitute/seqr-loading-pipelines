e59e10bf7cfbcc9b7fb4e96581ba74e0289ad3c7
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-05-17 18:42:33 -0400

    support checkpoints

--- v03_pipeline/lib/paths.py
+++ v03_pipeline/lib/paths.py
@@ -6,21 +6,25 @@ from v03_pipeline.lib.definitions import (
     Env,
     PipelineVersion,
     ReferenceGenome,
+    Storage,
 )
-
 
 def _v03_pipeline_prefix(
     env: Env,
+    storage: Storage,
     root: DataRoot,
     reference_genome: ReferenceGenome,
     dataset_type: DatasetType,
 ) -> str:
-    if env == Env.LOCAL:
+    if env == Env.TEST:
+        root = DataRoot.TEST_DATASETS
+    elif env == Env.LOCAL:
         root = DataRoot.LOCAL_DATASETS
-    if env == Env.DEV:
+    elif env == Env.DEV or storage == Storage.CHECKPOINT:
         root = DataRoot.SEQR_SCRATCH_TEMP
     return os.path.join(
         root.value,
+        'checkpoints' if storage == Storage.CHECKPOINT else '',
         reference_genome.value,
         PipelineVersion.V03.value,
         dataset_type.value,
@@ -36,6 +40,7 @@ def family_table_path(
     return os.path.join(
         _v03_pipeline_prefix(
             env,
+            Storage.PERMANENT,
             DataRoot.SEQR_LOADING_TEMP,
             reference_genome,
             dataset_type,
@@ -48,6 +53,7 @@ def family_table_path(
 
 def project_table_path(
     env: Env,
+    storage: Storage,
     reference_genome: ReferenceGenome,
     dataset_type: DatasetType,
     project_guid: str,
@@ -55,6 +61,7 @@ def project_table_path(
     return os.path.join(
         _v03_pipeline_prefix(
             env,
+            storage,
             DataRoot.SEQR_DATASETS,
             reference_genome,
             dataset_type,
@@ -67,12 +74,14 @@ def project_table_path(
 
 def variant_annotations_table_path(
     env: Env,
+    storage: Storage,
     reference_genome: ReferenceGenome,
     dataset_type: DatasetType,
 ) -> str:
     return os.path.join(
         _v03_pipeline_prefix(
             env,
+            storage,
             DataRoot.SEQR_DATASETS,
             reference_genome,
             dataset_type,
@@ -83,12 +92,14 @@ def variant_annotations_table_path(
 
 def variant_lookup_table_path(
     env: Env,
+    storage: Storage,
     reference_genome: ReferenceGenome,
     dataset_type: DatasetType,
 ) -> str:
     return os.path.join(
         _v03_pipeline_prefix(
             env,
+            storage,
             DataRoot.SEQR_DATASETS,
             reference_genome,
             dataset_type,
--- v03_pipeline/lib/paths_test.py
+++ v03_pipeline/lib/paths_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from v03_pipeline.lib.definitions import DatasetType, Env, ReferenceGenome
+from v03_pipeline.lib.definitions import DatasetType, Env, ReferenceGenome, Storage
 from v03_pipeline.lib.paths import (
     family_table_path,
     project_table_path,
@@ -12,6 +12,7 @@ from v03_pipeline.lib.paths import (
 class TestPaths(unittest.TestCase):
     def test_family_table_path(self) -> None:
         for env, expected_path in [
+            (Env.TEST, 'test-datasets/GRCh37/v03/SNV/families/franklin/samples.ht'),
             (Env.LOCAL, 'seqr-datasets/GRCh37/v03/SNV/families/franklin/samples.ht'),
             (
                 Env.DEV,
@@ -36,29 +37,60 @@ class TestPaths(unittest.TestCase):
         self.assertEqual(
             project_table_path(
                 Env.PROD,
+                Storage.PERMANENT,
                 ReferenceGenome.GRCh38,
                 DatasetType.MITO,
                 'R0652_pipeline_test',
             ),
             'gs://seqr-datasets/GRCh38/v03/MITO/projects/R0652_pipeline_test/samples.ht',
         )
+        self.assertEqual(
+            project_table_path(
+                Env.PROD,
+                Storage.CHECKPOINT,
+                ReferenceGenome.GRCh38,
+                DatasetType.MITO,
+                'R0652_pipeline_test',
+            ),
+            'gs://seqr-scratch-temp/checkpoints/GRCh38/v03/MITO/projects/R0652_pipeline_test/samples.ht',
+        )
 
     def test_variant_annotations_table_path(self) -> None:
         self.assertEqual(
             variant_annotations_table_path(
                 Env.DEV,
+                Storage.PERMANENT,
                 ReferenceGenome.GRCh38,
                 DatasetType.GCNV,
             ),
             'gs://seqr-scratch-temp/GRCh38/v03/GCNV/annotations.ht',
+        )
+        self.assertEqual(
+            variant_annotations_table_path(
+                Env.DEV,
+                Storage.CHECKPOINT,
+                ReferenceGenome.GRCh38,
+                DatasetType.GCNV,
+            ),
+            'gs://seqr-scratch-temp/checkpoints/GRCh38/v03/GCNV/annotations.ht',
         )
 
     def test_variant_lookup_table_path(self) -> None:
         self.assertEqual(
             variant_lookup_table_path(
                 Env.LOCAL,
+                Storage.PERMANENT,
                 ReferenceGenome.GRCh37,
                 DatasetType.SV,
             ),
             'seqr-datasets/GRCh37/v03/SV/lookup.ht',
+        )
+        self.assertEqual(
+            variant_lookup_table_path(
+                Env.LOCAL,
+                Storage.CHECKPOINT,
+                ReferenceGenome.GRCh37,
+                DatasetType.SV,
+            ),
+            'seqr-datasets/checkpoints/GRCh37/v03/SV/lookup.ht',
         )
