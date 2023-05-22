142a3024f97acddcf91727b6a1813fd0232cac65
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-05-22 12:17:29 -0400

    updates from upstream

--- v03_pipeline/lib/tasks/base/base_pipeline_task.py
+++ v03_pipeline/lib/tasks/base/base_pipeline_task.py
@@ -5,18 +5,6 @@ from v03_pipeline.lib.definitions import DatasetType, Env, ReferenceGenome
 from v03_pipeline.lib.misc.io import write_ht
 
 
-def empty_table(
-    dataset_type: DatasetType,
-    reference_genome: ReferenceGenome,
-) -> hl.Table:
-    key_type = dataset_type.table_key_type(reference_genome)
-    return hl.Table.parallelize(
-        [],
-        key_type,
-        key=key_type.fields,
-    )
-
-
 class BasePipelineTask(luigi.Task):
     env = luigi.EnumParameter(enum=Env, default=Env.LOCAL)
     reference_genome = luigi.EnumParameter(
@@ -35,16 +23,29 @@ class BasePipelineTask(luigi.Task):
     def run(self) -> None:
         if self.hail_temp_dir:
             # Need to use the GCP bucket as temp storage for very large callset joins
-            hl.init(tmp_dir=self.hail_temp_dir)
+            # `idempotent` makes this a no-op if already called.
+            hl.init(tmp_dir=self.hail_temp_dir, idempotent=True)
 
         # Interval ref data join causes shuffle death, this prevents it
         hl._set_flags(use_new_shuffle='1')  # noqa: SLF001
 
         if not self.output().exists():
-            ht = empty_table(
+            ht = self.empty_table(
                 self.dataset_type,
                 self.reference_genome,
             )
+        else:
+            ht = hl.read_table(self.output().path)
 
         ht = self.update(ht)
         write_ht(self.env, ht, self.output().path)
+
+    def empty_table(
+        self,
+        dataset_type: DatasetType,
+        reference_genome: ReferenceGenome,
+    ) -> hl.Table:
+        raise NotImplementedError
+
+    def update(self, mt: hl.Table) -> hl.Table:
+        raise NotImplementedError
