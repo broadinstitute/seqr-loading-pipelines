b3e7f7e04a7c2ccaf2efd78f75a9f6932be98d79
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-06-01 17:45:15 -0400

    Add task executor

---
+++ v03_pipeline/lib/tasks/__init__.py
@@ -0,0 +1,11 @@
+from v03_pipeline.lib.tasks.update_project_table import UpdateProjectTableTask
+from v03_pipeline.lib.tasks.update_sample_lookup_table import UpdateSampleLookupTableTask
+from v03_pipeline.lib.tasks.update_variant_annotations_table_with_new_samples import UpdateVariantAnnotationsTableWithNewSamplesTask
+from v03_pipeline.lib.tasks.write_family_table import WriteFamilyTableTask
+
+__all__ = [
+	'UpdateProjectTableTask',
+	'UpdateSampleLookupTableTask',
+	'UpdateVariantAnnotationsTableWithNewSamplesTask',
+	'WriteFamilyTableTask',
+]
