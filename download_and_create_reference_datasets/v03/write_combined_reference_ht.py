0052f654549dccd23338e0f7201ab4b88d50c490
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-06-12 14:22:36 -0400

    cleanup to use v03 enums

--- download_and_create_reference_datasets/v03/write_combined_reference_ht.py
+++ download_and_create_reference_datasets/v03/write_combined_reference_ht.py
@@ -11,9 +11,9 @@ from hail_scripts.reference_data.combine import join_hts, update_existing_joined
 from hail_scripts.reference_data.config import GCS_PREFIXES, AccessControl
 from hail_scripts.utils.hail_utils import write_ht
 
-from v03_pipeline.lib.model import DatasetType, Env, ReferenceGenome
+from v03_pipeline.lib.annotations.fields import get_fields
+from v03_pipeline.lib.model import AnnotationType, DatasetType, Env, ReferenceGenome
 from v03_pipeline.lib.vep import run_vep
-
 
 COMBINED_REFERENCE_HT_PATH = 'reference_datasets/combined.ht'
 DATASETS = [
@@ -36,9 +36,10 @@ def run(
     reference_genome: ReferenceGenome,
     dataset: str | None,
     vep_config_json_path: str | None,
+    liftover_ref_path: str,
 ):
     destination_path = os.path.join(
-        GCS_PREFIXES[(env, AccessControl.PUBLIC)],
+        GCS_PREFIXES[(env.value, AccessControl.PUBLIC)],
         COMBINED_REFERENCE_HT_PATH,
     ).format(
         genome_version=reference_genome.v02_value,
@@ -51,7 +52,6 @@ def run(
             destination_path,
             dataset,
             DATASETS,
-            VERSION,
             reference_genome.v02_value,
         )
         unannotated_rows_ht = ht.filter(~hl.is_defined(ht.vep))
@@ -62,15 +62,33 @@ def run(
             DatasetType.SNV,
             vep_config_json_path,
         )
+        unannotated_rows_ht = unannotated_rows_ht.annotate(
+            **get_fields(
+                unannotated_rows_ht,
+                AnnotationType.FORMATTING,
+                env,
+                reference_genome,
+                liftover_ref_path,
+            ),
+        )
         ht = ht.union(unannotated_rows_ht)
     else:
-        ht = join_hts(DATASETS, VERSION, reference_genome=reference_genome.v02_value)
+        ht = join_hts(DATASETS, reference_genome=reference_genome.v02_value)
         run_vep(
             ht,
             env,
             reference_genome,
             DatasetType.SNV,
             vep_config_json_path,
+        )
+        ht = ht.annotate(
+            **get_fields(
+                ht,
+                AnnotationType.FORMATTING,
+                env,
+                reference_genome,
+                liftover_ref_path,
+            ),
         )
 
     ht.describe()
@@ -104,10 +122,15 @@ if __name__ == '__main__':
         '--vep-config-json-path',
         default=None,
     )
+    parser.add_argument(
+        '--liftover-ref-path',
+        default='gs://hail-common/references/grch38_to_grch37.over.chain.gz',
+    )
     args, _ = parser.parse_known_args()
     run(
         args.env,
         args.reference_genome,
         args.dataset,
         args.vep_config_json_path,
+        args.liftover_ref_path,
     )
--- download_and_create_reference_datasets/v03/write_hgmd_ht.py
+++ download_and_create_reference_datasets/v03/write_hgmd_ht.py
@@ -6,19 +6,21 @@ from hail_scripts.reference_data.combine import get_ht
 from hail_scripts.reference_data.config import GCS_PREFIXES, AccessControl
 from hail_scripts.utils.hail_utils import write_ht
 
+from v03_pipeline.lib.model import Env, ReferenceGenome
+
 HGMD_HT_PATH = 'reference_datasets/hgmd.ht'
 PARTITIONS = 100
 
 
-def run(environment: str, genome_version: str):
+def run(env: Env, reference_genome: ReferenceGenome):
     dataset = 'hgmd'
     destination_path = os.path.join(
-        GCS_PREFIXES[(environment, AccessControl.PRIVATE)],
+        GCS_PREFIXES[(env.value, AccessControl.PRIVATE)],
         HGMD_HT_PATH,
     ).format(
-        genome_version=genome_version,
+        genome_version=reference_genome.v02_value,
     )
-    ht = get_ht(dataset, genome_version)
+    ht = get_ht(dataset, reference_genome.v02_value)
     print(f'Uploading ht to {destination_path}')
     write_ht(ht, destination_path)
 
@@ -26,14 +28,16 @@ def run(environment: str, genome_version: str):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--environment',
-        default='dev',
-        choices=['dev', 'prod'],
+        '--env',
+        type=Env,
+        choices=list(Env),
+        default=Env.DEV,
     )
     parser.add_argument(
-        '--genome-version',
-        default='38',
-        choices=['37', '38'],
+        '--reference-genome',
+        type=ReferenceGenome,
+        choices=list(ReferenceGenome),
+        default=ReferenceGenome.GRCh38,
     )
     args, _ = parser.parse_known_args()
-    run(args.environment, args.genome_version)
+    run(args.env, args.reference_genome)
