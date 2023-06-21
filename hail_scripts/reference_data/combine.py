9e31f5a9a458ee72a2fd9d9ba882eabdcec85764
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-06-21 16:31:01 -0400

    so many mistakes

--- hail_scripts/reference_data/combine.py
+++ hail_scripts/reference_data/combine.py
@@ -104,7 +104,7 @@ def get_ht(dataset: str, reference_genome: str):
     ht = ht.select_globals(
         **{
             f'{dataset}_globals': hl.struct(
-                path=config.get('path', config['source_path']),
+                path=config['source_path'] if 'custom_import' in config else config['path'],
                 version=parse_version(ht, dataset, config),
                 enums=config.get(
                     'enum_select',
