9ee440bd92f3313a7e53ad84f0ed9c5f5a1aae08
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-07-06 19:12:53 -0400

    add import

--- v03_pipeline/lib/tasks/__init__.py
+++ v03_pipeline/lib/tasks/__init__.py
@@ -5,11 +5,13 @@ from v03_pipeline.lib.tasks.update_sample_lookup_table import (
 from v03_pipeline.lib.tasks.update_variant_annotations_table_with_new_samples import (
     UpdateVariantAnnotationsTableWithNewSamplesTask,
 )
+from v03_pipeline.lib.tasks.write_sample_ids_for_run import WriteSampleIdsForRunTask
 from v03_pipeline.lib.tasks.write_family_table import WriteFamilyTableTask
 
 __all__ = [
     'UpdateProjectTableTask',
     'UpdateSampleLookupTableTask',
     'UpdateVariantAnnotationsTableWithNewSamplesTask',
+    'WriteSampleIdsForRunTask',
     'WriteFamilyTableTask',
 ]
