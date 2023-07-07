ed17d1fd1e0391a1c63f826b4d1b3329dcd3ec32
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-07-07 15:48:36 -0400

    missing import

--- v03_pipeline/lib/tasks/__init__.py
+++ v03_pipeline/lib/tasks/__init__.py
@@ -6,10 +6,12 @@ from v03_pipeline.lib.tasks.update_variant_annotations_table_with_new_samples im
     UpdateVariantAnnotationsTableWithNewSamplesTask,
 )
 from v03_pipeline.lib.tasks.write_family_table import WriteFamilyTableTask
+from v03_pipeline.lib.tasks.write_sample_ids_for_run import WriteSampleIdsForRunTask
 
 __all__ = [
     'UpdateProjectTableTask',
     'UpdateSampleLookupTableTask',
     'UpdateVariantAnnotationsTableWithNewSamplesTask',
+    'WriteSampleIdsForRunTask',
     'WriteFamilyTableTask',
 ]
