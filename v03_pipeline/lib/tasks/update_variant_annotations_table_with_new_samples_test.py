cb89bbdea7c9c6510f10c0f615a2cc181b6b57e7
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-07-25 12:00:22 -0400

    Getting test up to speed

--- v03_pipeline/lib/tasks/update_variant_annotations_table_with_new_samples_test.py
+++ v03_pipeline/lib/tasks/update_variant_annotations_table_with_new_samples_test.py
@@ -12,19 +12,28 @@ from hail_scripts.reference_data.clinvar import (
     CLINVAR_PATHOGENICITIES,
 )
 
-from v03_pipeline.lib.annotations.enums import BIOTYPES, CONSEQUENCE_TERMS, LOF_FILTERS
+from v03_pipeline.lib.annotations.enums import (
+    BIOTYPES,
+    CONSEQUENCE_TERMS,
+    LOF_FILTERS,
+    MITOTIP_PATHOGENICITIES,
+)
 from v03_pipeline.lib.model import DatasetType, Env, ReferenceGenome
 from v03_pipeline.lib.tasks.update_variant_annotations_table_with_new_samples import (
     UpdateVariantAnnotationsTableWithNewSamplesTask,
 )
 
-TEST_VCF = 'v03_pipeline/var/test/callsets/1kg_30variants.vcf.bgz'
+TEST_SNV_VCF = 'v03_pipeline/var/test/callsets/1kg_30variants.vcf.bgz'
+TEST_MITO_MT = 'v03_pipeline/var/test/callsets/mito_1.mt'
 TEST_REMAP = 'v03_pipeline/var/test/remaps/test_remap_1.tsv'
 TEST_PEDIGREE_3 = 'v03_pipeline/var/test/pedigrees/test_pedigree_3.tsv'
 TEST_PEDIGREE_4 = 'v03_pipeline/var/test/pedigrees/test_pedigree_4.tsv'
+TEST_PEDIGREE_5 = 'v03_pipeline/var/test/pedigrees/test_pedigree_5.tsv'
 TEST_COMBINED_1 = 'v03_pipeline/var/test/reference_data/test_combined_1.ht'
+TEST_COMBINED_MITO_1 = 'v03_pipeline/var/test/reference_data/test_combined_mito_1.ht'
 TEST_HGMD_1 = 'v03_pipeline/var/test/reference_data/test_hgmd_1.ht'
 TEST_INTERVAL_1 = 'v03_pipeline/var/test/reference_data/test_interval_1.ht'
+TEST_INTERVAL_MITO_1 = 'v03_pipeline/var/test/reference_data/test_interval_mito_1.ht'
 
 
 @patch('v03_pipeline.lib.paths.DataRoot')
@@ -42,6 +51,14 @@ class UpdateVariantAnnotationsTableWithNewSamplesTaskTest(unittest.TestCase):
             TEST_HGMD_1,
             f'{self._temp_local_reference_data}/v03/GRCh38/reference_datasets/hgmd.ht',
         )
+        shutil.copytree(
+            TEST_COMBINED_MITO_1,
+            f'{self._temp_local_reference_data}/v03/GRCh38/reference_datasets/combined_mito.ht',
+        )
+        shutil.copytree(
+            TEST_INTERVAL_MITO_1,
+            f'{self._temp_local_reference_data}/v03/GRCh38/reference_datasets/interval_mito.ht',
+        )
 
     def tearDown(self) -> None:
         if os.path.isdir(self._temp_local_datasets):
@@ -57,7 +74,7 @@ class UpdateVariantAnnotationsTableWithNewSamplesTaskTest(unittest.TestCase):
             env=Env.TEST,
             reference_genome=ReferenceGenome.GRCh38,
             dataset_type=DatasetType.SNV,
-            callset_path=TEST_VCF,
+            callset_path=TEST_SNV_VCF,
             project_guids=['R0113_test_project'],
             project_remap_paths=[TEST_REMAP],
             project_pedigree_paths=['bad_pedigree'],
@@ -74,7 +91,7 @@ class UpdateVariantAnnotationsTableWithNewSamplesTaskTest(unittest.TestCase):
             env=Env.TEST,
             reference_genome=ReferenceGenome.GRCh38,
             dataset_type=DatasetType.SNV,
-            callset_path=TEST_VCF,
+            callset_path=TEST_SNV_VCF,
             project_guids=['R0113_test_project'],
             project_remap_paths=[TEST_REMAP],
             project_pedigree_paths=[TEST_PEDIGREE_3],
@@ -97,7 +114,7 @@ class UpdateVariantAnnotationsTableWithNewSamplesTaskTest(unittest.TestCase):
             env=Env.TEST,
             reference_genome=ReferenceGenome.GRCh38,
             dataset_type=DatasetType.SNV,
-            callset_path=TEST_VCF,
+            callset_path=TEST_SNV_VCF,
             project_guids=['R0113_test_project'],
             project_remap_paths=[TEST_REMAP],
             project_pedigree_paths=[TEST_PEDIGREE_3],
@@ -130,7 +147,7 @@ class UpdateVariantAnnotationsTableWithNewSamplesTaskTest(unittest.TestCase):
             [
                 {
                     hl.Struct(
-                        callset=TEST_VCF,
+                        callset=TEST_SNV_VCF,
                         project_guid='R0113_test_project',
                     ),
                 },
@@ -142,7 +159,7 @@ class UpdateVariantAnnotationsTableWithNewSamplesTaskTest(unittest.TestCase):
             env=Env.TEST,
             reference_genome=ReferenceGenome.GRCh38,
             dataset_type=DatasetType.SNV,
-            callset_path=TEST_VCF,
+            callset_path=TEST_SNV_VCF,
             project_guids=['R0114_project4'],
             project_remap_paths=[TEST_REMAP],
             project_pedigree_paths=[TEST_PEDIGREE_4],
@@ -304,6 +321,89 @@ class UpdateVariantAnnotationsTableWithNewSamplesTaskTest(unittest.TestCase):
                             consequence_term=CONSEQUENCE_TERMS,
                             lof_filter=LOF_FILTERS,
                         ),
+                    ),
+                ),
+            ],
+        )
+
+    def test_mito_update_vat(self, mock_dataroot: Mock) -> None:
+        mock_dataroot.LOCAL_DATASETS.value = self._temp_local_datasets
+        mock_dataroot.LOCAL_REFERENCE_DATA.value = self._temp_local_reference_data
+        worker = luigi.worker.Worker()
+
+        update_variant_annotations_task = (
+            UpdateVariantAnnotationsTableWithNewSamplesTask(
+                env=Env.TEST,
+                reference_genome=ReferenceGenome.GRCh38,
+                dataset_type=DatasetType.MITO,
+                callset_path=TEST_MITO_MT,
+                project_guids=['R0115_test_project2'],
+                project_remap_paths=['not_a_real_file'],
+                project_pedigree_paths=[TEST_PEDIGREE_5],
+            )
+        )
+        worker.add(update_variant_annotations_task)
+        worker.run()
+        self.assertTrue(update_variant_annotations_task.complete())
+        ht = hl.read_table(update_variant_annotations_task.output().path)
+        self.assertEqual(ht.count(), 5)
+        self.assertCountEqual(
+            ht.globals.collect(),
+            [
+                hl.Struct(
+                    paths=hl.Struct(
+                        high_constraint_region_mito='gs://seqr-reference-data/GRCh38/mitochondrial/Helix high constraint intervals Feb-15-2022.tsv',
+                        clinvar='ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz',
+                        dbnsfp='gs://seqr-reference-data/GRCh38/dbNSFP/v4.2/dbNSFP4.2a_variant.ht',
+                        gnomad_mito='gs://gcp-public-data--gnomad/release/3.1/ht/genomes/gnomad.genomes.v3.1.sites.chrM.ht',
+                        helix_mito='gs://seqr-reference-data/GRCh38/mitochondrial/Helix/HelixMTdb_20200327.ht',
+                        hmtvar='gs://seqr-reference-data/GRCh38/mitochondrial/HmtVar/HmtVar%20Jan.%2010%202022.ht',
+                        mitomap='gs://seqr-reference-data/GRCh38/mitochondrial/MITOMAP/mitomap-confirmed-mutations-2022-02-04.ht',
+                        mitimpact='gs://seqr-reference-data/GRCh38/mitochondrial/MitImpact/MitImpact_db_3.0.7.ht',
+                        sorted_transcript_consequences=None,
+                        mitotip=None,
+                    ),
+                    versions=hl.Struct(
+                        high_constraint_region_mito='Feb-15-2022',
+                        clinvar='2023-07-22',
+                        dbnsfp='4.2',
+                        gnomad_mito='v3.1',
+                        helix_mito='20200327',
+                        hmtvar='Jan. 10 2022',
+                        mitomap='Feb. 04 2022',
+                        mitimpact='3.0.7',
+                        sorted_transcript_consequences=None,
+                        mitotip=None,
+                    ),
+                    enums=hl.Struct(
+                        high_constraint_region_mito=hl.Struct(),
+                        clinvar=hl.Struct(
+                            assertion=CLINVAR_ASSERTIONS,
+                            pathogenicity=CLINVAR_PATHOGENICITIES,
+                        ),
+                        dbnsfp=hl.Struct(
+                            SIFT_pred=['D', 'T'],
+                            Polyphen2_HVAR_pred=['D', 'P', 'B'],
+                            MutationTaster_pred=['D', 'A', 'N', 'P'],
+                            fathmm_MKL_coding_pred=['D', 'N'],
+                        ),
+                        gnomad_mito=hl.Struct(),
+                        helix_mito=hl.Struct(),
+                        hmtvar=hl.Struct(),
+                        mitomap=hl.Struct(),
+                        mitimpact=hl.Struct(),
+                        sorted_transcript_consequences=hl.Struct(
+                            biotype=BIOTYPES,
+                            consequence_term=CONSEQUENCE_TERMS,
+                            lof_filter=LOF_FILTERS,
+                        ),
+                        mitotip=hl.Struct(pathogenicities=MITOTIP_PATHOGENICITIES),
+                        updates={
+                            hl.Struct(
+                                callset='v03_pipeline/var/test/callsets/mito_1.mt',
+                                project_guid='R0115_test_project2',
+                            )
+                        },
                     ),
                 ),
             ],
