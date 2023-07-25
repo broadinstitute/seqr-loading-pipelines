bd8d69fb61e0f3d3d501c37d876f9a05cb1be159
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-07-24 20:40:13 -0400

    itertools

--- v03_pipeline/lib/tasks/update_variant_annotations_table_with_new_samples.py
+++ v03_pipeline/lib/tasks/update_variant_annotations_table_with_new_samples.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import functools
+import itertools
 
 import hail as hl
 import luigi
@@ -161,7 +162,7 @@ class UpdateVariantAnnotationsTableWithNewSamplesTask(BaseVariantAnnotationsTabl
             versions=hl.Struct(),
             enums=hl.Struct(),
         )
-        for rdc_ht in annotatable_rdc_hts.values() + joinable_rdc_hts.values():
+        for rdc_ht in itertools.chain(annotatable_rdc_hts.values(), joinable_rdc_hts.values()):
             rdc_globals = rdc_ht.index_globals()
             ht = ht.annotate_globals(
                 paths=hl.Struct(
