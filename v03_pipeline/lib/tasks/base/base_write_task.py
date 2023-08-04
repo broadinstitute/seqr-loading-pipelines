ae8e031e1772f951ddbb2e91d1a82f6dc0ca63c6
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-08-04 14:57:36 -0400

    Try this

--- v03_pipeline/lib/tasks/base/base_write_task.py
+++ v03_pipeline/lib/tasks/base/base_write_task.py
@@ -24,7 +24,7 @@ class BaseWriteTask(luigi.Task):
     def init_hail(self):
         if self.hail_temp_dir:
             # Need to use the GCP bucket as temp storage for very large callset joins
-            hl.init(tmp_dir=self.hail_temp_dir, idempotent=True)
+            #hl.init(tmp_dir=self.hail_temp_dir, idempotent=True)
 
         # Interval ref data join causes shuffle death, this prevents it
         hl._set_flags(use_new_shuffle='1')  # noqa: SLF001
