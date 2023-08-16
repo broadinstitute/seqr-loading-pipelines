74bf5d330b9799678b72df4cee93220e388fba04
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-08-16 17:19:52 -0400

    update test

--- v03_pipeline/lib/misc/validation_test.py
+++ v03_pipeline/lib/misc/validation_test.py
@@ -2,20 +2,70 @@ import unittest
 
 import hail as hl
 
-from v03_pipeline.lib.misc.validation import validate_contigs
+from v03_pipeline.lib.misc.validation import SeqrValidationError, validate_contigs
 from v03_pipeline.lib.model import ReferenceGenome
+
+
+def _mt_from_contigs(contigs):
+    return hl.MatrixTable.from_parts(
+        rows={
+            'locus': [
+                hl.Locus(
+                    contig=contig,
+                    position=1,
+                    reference_genome='GRCh38',
+                )
+                for contig in contigs
+            ],
+        },
+        cols={'s': ['sample_1']},
+        entries={'HL': [[0.0] for _ in range(len(contigs))]},
+    )
 
 
 class ValidationTest(unittest.TestCase):
     def test_validate_contigs(self) -> None:
-        mt = hl.MatrixTable.from_parts(
-            rows={'variants': [1, 2]},
-            cols={'s': ['sample_1', 'sample_2', 'sample_3', 'sample_4']},
-            entries={
-                'HL': [
-                    [0.0, hl.missing(hl.tfloat), 0.99, 0.01],
-                    [0.1, 0.2, 0.94, 0.99],
-                ],
-            },
+        mt = _mt_from_contigs(ReferenceGenome.GRCh38.standard_contigs)
+        self.assertIsNone(validate_contigs(mt, ReferenceGenome.GRCh38, 1))
+        self.assertRaisesRegex(
+            SeqrValidationError,
+            'Missing the following expected contigs',
+            validate_contigs,
+            mt,
+            ReferenceGenome.GRCh37,
+            1,
         )
-        validate_contigs(mt, ReferenceGenome.GRCh38, 1)
+        self.assertRaisesRegex(
+            SeqrValidationError,
+            'which is lower than expected minimum count',
+            validate_contigs,
+            mt,
+            ReferenceGenome.GRCh38,
+            2,
+        )
+
+        # Drop an optional contig
+        mt = _mt_from_contigs(ReferenceGenome.GRCh38.standard_contigs - {'chrY'})
+        self.assertIsNone(validate_contigs(mt, ReferenceGenome.GRCh38, 1))
+
+        # Drop a non-optional contig
+        mt = _mt_from_contigs(ReferenceGenome.GRCh38.standard_contigs - {'chr3'})
+        self.assertRaisesRegex(
+            SeqrValidationError,
+            'Missing the following expected contigs',
+            validate_contigs,
+            mt,
+            ReferenceGenome.GRCh38,
+            1,
+        )
+
+        # Add an unexpected contig
+        mt = _mt_from_contigs(ReferenceGenome.GRCh38.standard_contigs | {'chr503'})
+        self.assertRaisesRegex(
+            SeqrValidationError,
+            'Found the following unexpected contigs',
+            validate_contigs,
+            mt,
+            ReferenceGenome.GRCh38,
+            1,
+        )
