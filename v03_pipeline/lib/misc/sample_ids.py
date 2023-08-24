db45264f3f14a3962a20cb8a027b68086dcba894
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-08-24 16:01:35 -0400

    ignore_missing_samples_when_remapping

--- v03_pipeline/lib/misc/sample_ids.py
+++ v03_pipeline/lib/misc/sample_ids.py
@@ -14,7 +14,7 @@ def vcf_remap(mt: hl.MatrixTable) -> hl.MatrixTable:
     return mt
 
 
-def remap_sample_ids(mt: hl.MatrixTable, project_remap_ht: hl.Table) -> hl.MatrixTable:
+def remap_sample_ids(mt: hl.MatrixTable, project_remap_ht: hl.Table, ignore_missing_samples_when_remapping: bool) -> hl.MatrixTable:
     mt = vcf_remap(mt)
     collected_remap = project_remap_ht.collect()
     s_dups = [k for k, v in Counter([r.s for r in collected_remap]).items() if v > 1]
@@ -36,7 +36,10 @@ def remap_sample_ids(mt: hl.MatrixTable, project_remap_ht: hl.Table) -> hl.Matri
             f"IDs that aren't in the callset: {missing_samples}\n"
             f'All callset sample IDs:{mt.s.collect()}'
         )
-        raise MatrixTableSampleSetError(msg, missing_samples)
+        if ignore_missing_samples_when_remapping:
+            print(message)
+        else:
+            raise MatrixTableSampleSetError(msg, missing_samples)
 
     mt = mt.annotate_cols(**project_remap_ht[mt.s])
     remap_expr = hl.cond(hl.is_missing(mt.seqr_id), mt.s, mt.seqr_id)
@@ -49,7 +52,7 @@ def remap_sample_ids(mt: hl.MatrixTable, project_remap_ht: hl.Table) -> hl.Matri
 def subset_samples(
     mt: hl.MatrixTable,
     sample_subset_ht: hl.Table,
-    ignore_missing_samples: bool,
+    ignore_missing_samples_when_subsetting: bool,
 ) -> hl.MatrixTable:
     subset_count = sample_subset_ht.count()
     anti_join_ht = sample_subset_ht.anti_join(mt.cols())
