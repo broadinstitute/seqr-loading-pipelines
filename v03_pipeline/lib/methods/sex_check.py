3947ab5c58a1881df4cfbc02d5e7722d9f98bec7
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-09-05 23:19:14 -0400

    sex check

--- v03_pipeline/lib/methods/sex_check.py
+++ v03_pipeline/lib/methods/sex_check.py
@@ -1,4 +1,4 @@
-import logging
+import hail as hl
 
 from v03_pipeline.lib.model import ReferenceGenome
 
@@ -28,18 +28,18 @@ def get_contig_cov(
         msg = f'Contig: {contig} is invalid for this reference genome'
         raise ValueError(msg)
 
-    sex_mt = hl.filter_intervals(
+    mt = hl.filter_intervals(
         mt,
         [hl.parse_locus_interval(contig, reference_genome=reference_genome.value)],
     )
     if 'X' in contig:
-        sex_mt = sex_mt.filter_rows(sex_mt.locus.in_x_nonpar())
+        mt = mt.filter_rows(mt.locus.in_x_nonpar())
     if 'Y' in contig:
-        sex_mt = sex_mt.filter_rows(sex_mt.locus.in_y_nonpar())
+        mt = mt.filter_rows(mt.locus.in_y_nonpar())
 
     # Filter to common SNVs above defined callrate (should only have one index in the array because the MT only contains biallelic variants)
-    sex_mt = sex_mt.filter_rows(sex_mt[af_field] > af_threshold)
-    sex_mt = hl.variant_qc(sex_mt)
-    sex_mt = sex_mt.filter_rows(sex_mt.variant_qc.call_rate > call_rate_threshold)
-    sex_mt = sex_mt.annotate_cols(**{f"{contig}_mean_dp": hl.agg.mean(sex_mt.DP)})
-    return sex_mt.cols()
+    mt = mt.filter_rows(mt[af_field] > af_threshold)
+    mt = hl.variant_qc(mt)
+    mt = mt.filter_rows(mt.variant_qc.call_rate > call_rate_threshold)
+    mt = mt.select_cols(**{f'{contig}_mean_dp': hl.agg.mean(mt.DP)})
+    return mt.cols()
---
+++ v03_pipeline/lib/methods/sex_check_test.py
@@ -0,0 +1,65 @@
+import unittest
+
+import hail as hl
+
+from v03_pipeline.lib.methods.sex_check import get_contig_cov
+from v03_pipeline.lib.model import ReferenceGenome
+
+
+class SexCheckTest(unittest.TestCase):
+    def test_get_contig_cov(self):
+        mt = hl.MatrixTable.from_parts(
+            rows={
+                'locus': [
+                    hl.Locus(
+                        contig='chr1',
+                        position=1,
+                        reference_genome='GRCh38',
+                    ),
+                    hl.Locus(
+                        contig='chr1',
+                        position=2,
+                        reference_genome='GRCh38',
+                    ),
+                    hl.Locus(
+                        contig='chr1',
+                        position=3,
+                        reference_genome='GRCh38',
+                    ),
+                    hl.Locus(
+                        contig='chr2',
+                        position=4,
+                        reference_genome='GRCh38',
+                    ),
+                ],
+                'alleles': [
+                    ['A', 'C'],
+                    ['A', 'C'],
+                    ['A', 'C'],
+                    ['A', 'C'],
+                ],
+                'AF': [0.1, 0.02, 0.001, 0.1],
+            },
+            cols={'s': ['sample_1', 'sample_2']},
+            entries={
+                'DP': [
+                    [0.0, hl.missing(hl.tfloat)],
+                    [0.1, 0.3],
+                    [hl.missing(hl.tfloat), 0.5],
+                    [0.4, 0.6],
+                ],
+                'GT': [
+                    [hl.Call(alleles=[0, 0], phased=False),hl.Call(alleles=[0, 0], phased=False)],
+                    [hl.Call(alleles=[0, 0], phased=False),hl.Call(alleles=[0, 0], phased=False)],
+                    [hl.Call(alleles=[0, 0], phased=False),hl.Call(alleles=[0, 0], phased=False)],
+                    [hl.Call(alleles=[0, 0], phased=False),hl.Call(alleles=[0, 0], phased=False)],
+                ],
+            },
+        )
+        mt = mt.key_rows_by('locus', 'alleles')
+        mt = mt.key_cols_by('s')
+        ht = get_contig_cov(mt, ReferenceGenome.GRCh38, 'chr1')
+        self.assertListEqual(ht.collect(), [
+            hl.Struct(s='sample_1', chr1_mean_dp=0.05),
+            hl.Struct(s='sample_2', chr1_mean_dp=0.3),
+        ])
