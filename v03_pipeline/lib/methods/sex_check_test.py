446e407ad84d74ed0bdf812baf6550e4a06b6354
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-09-07 17:14:34 -0400

    it works!

--- v03_pipeline/lib/methods/sex_check_test.py
+++ v03_pipeline/lib/methods/sex_check_test.py
@@ -101,7 +101,7 @@ class SexCheckTest(unittest.TestCase):
 
     def test_call_sex(self):
         mt = hl.read_matrix_table(TEST_SEX_AND_RELATEDNESS_CALLSET_MT)
-        ht = call_sex(mt, ReferenceGenome.GRCh38)
+        ht = call_sex(mt, ReferenceGenome.GRCh38, af_field = 'AF')
         self.assertCountEqual(
             ht.collect(),
             [
@@ -170,7 +170,7 @@ class SexCheckTest(unittest.TestCase):
 
     def test_call_sex_w_chrY_coverage(self):  # noqa: N802
         mt = hl.read_matrix_table(TEST_SEX_AND_RELATEDNESS_CALLSET_MT)
-        ht = call_sex(mt, ReferenceGenome.GRCh38, use_chrY_cov=True)
+        ht = call_sex(mt, ReferenceGenome.GRCh38, use_chrY_cov=True, af_field='AF')
         self.assertCountEqual(
             ht.collect(),
             [
