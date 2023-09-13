b01f12f3d2e3feeef5228f48452a82ec986bd8b3
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-09-13 10:30:08 -0400

    remove unused

--- v03_pipeline/lib/model/definitions.py
+++ v03_pipeline/lib/model/definitions.py
@@ -24,25 +24,13 @@ class ReferenceGenome(Enum):
         return self.value[-2:]
 
     @property
-    def autosomes(self) -> set[str]:
-        return {
-            ReferenceGenome.GRCh37: {str(x) for x in range(23)},
-            ReferenceGenome.GRCh38: {f'chr{x}' for x in range(23)},
-        }[self]
-
-    @property
-    def sex_chromosomes(self) -> set[str]:
-        hl_reference = hl.get_reference(self.value)
-        return {
-            *hl_reference.x_contigs,
-            *hl_reference.y_contigs,
-        }
+    def hl_reference(self) -> hl.ReferenceGenome:
+        return hl.get_reference(self.value)
 
     @property
     def standard_contigs(self) -> set[str]:
-        hl_reference = hl.get_reference(self.value)
         return {
-            *hl_reference.contigs[:25],
+            *self.hl_reference.contigs[:25],
         }
 
 
