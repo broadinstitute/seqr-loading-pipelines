78f24da15e7a1517d62f3825488fa7ffedd72865
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-09-18 14:41:36 -0400

    another

--- v03_pipeline/lib/tasks/write_remapped_and_subsetted_callset_test.py
+++ v03_pipeline/lib/tasks/write_remapped_and_subsetted_callset_test.py
@@ -21,11 +21,11 @@ class WriteRemappedAndSubsettedCallsetTaskTest(MockedDatarootTestCase):
         super().setUp()
         shutil.copytree(
             TEST_SEX_CHECK_1,
-            f'{self.mock_dataroot.LOADING_DATASETS}/v03/GRCh38/sex_checks/78d7998164bbe170d4f5282a66873df2e3b18099175069a32565fb0dc08dc3d4.ht',
+            f'{self.mock_env.LOADING_DATASETS}/v03/GRCh38/sex_checks/78d7998164bbe170d4f5282a66873df2e3b18099175069a32565fb0dc08dc3d4.ht',
         )
         shutil.copytree(
             TEST_GNOMAD_QC,
-            f'{self.mock_dataroot.PRIVATE_REFERENCE_DATASETS}/v03/GRCh38/cached_reference_dataset_queries/gnomad_qc.ht',
+            f'{self.mock_env.PRIVATE_REFERENCE_DATASETS}/v03/GRCh38/cached_reference_dataset_queries/gnomad_qc.ht',
         )
 
     def test_write_remapped_and_subsetted_callset_task(
