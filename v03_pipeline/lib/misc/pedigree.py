83748a5f1e26cc35c3c52a30e9c17900b9459a47
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-09-18 11:33:04 -0400

    reformat

--- v03_pipeline/lib/misc/pedigree.py
+++ v03_pipeline/lib/misc/pedigree.py
@@ -11,7 +11,8 @@ if TYPE_CHECKING:
 
 
 @dataclass
-class Lineage:
+class SampleMeta:
+    sex: Ploidy
     mother: str = None
     father: str = None
     maternal_grandmother: str = None
@@ -26,85 +27,70 @@ class Lineage:
 @dataclass
 class Family:
     family_guid: str
-    sample_lineage: dict[str, Lineage]
-    sample_sex: dict[str, Ploidy]
+    samples: dict[str, SampleMeta]
 
     def __hash__(self):
         return hash(self.family_guid)
 
     @staticmethod
-    def parse_sample_sex(rows: list[hl.Struct]) -> dict[str, Ploidy]:
-        sample_sex = {}
+    def parse_direct_lineage(rows: list[hl.Struct]) -> dict[str, SampleMeta]:
+        samples = {}
         for row in rows:
-            sample_sex[row.s] = Ploidy(row.sex)
-        return sample_sex
-
-    @staticmethod
-    def parse_direct_lineage(rows: list[hl.Struct]) -> dict[str, Lineage]:
-        direct_lineage = {}
-        for row in rows:
-            direct_lineage[row.s] = Lineage(
+            samples[row.s] = SampleMeta(
+                sex=Ploidy(row.sex),
                 mother=row.maternal_s,
                 father=row.paternal_s,
             )
 
         for row in rows:
-            # Maternal Parents
-            maternal_s = direct_lineage[row.s].mother
-            if maternal_s and direct_lineage[maternal_s].mother:
-                direct_lineage[row.s].maternal_grandmother = direct_lineage[
-                    maternal_s
-                ].mother
-            if maternal_s and direct_lineage[maternal_s].father:
-                direct_lineage[row.s].maternal_grandfather = direct_lineage[
-                    maternal_s
-                ].father
+            # Maternal GrandParents
+            maternal_s = samples[row.s].mother
+            if maternal_s and samples[maternal_s].mother:
+                samples[row.s].maternal_grandmother = samples[maternal_s].mother
+            if maternal_s and samples[maternal_s].father:
+                samples[row.s].maternal_grandfather = samples[maternal_s].father
 
-            # Paternal Parents
-            paternal_s = direct_lineage[row.s].father
-            if paternal_s and direct_lineage[paternal_s].mother:
-                direct_lineage[row.s].paternal_grandmother = direct_lineage[
-                    paternal_s
-                ].mother
-            if paternal_s and direct_lineage[paternal_s].father:
-                direct_lineage[row.s].paternal_grandfather = direct_lineage[
-                    paternal_s
-                ].father
-        return direct_lineage
+            # Paternal GrandParents
+            paternal_s = samples[row.s].father
+            if paternal_s and samples[paternal_s].mother:
+                samples[row.s].paternal_grandmother = samples[paternal_s].mother
+            if paternal_s and samples[paternal_s].father:
+                samples[row.s].paternal_grandfather = samples[paternal_s].father
+        return samples
 
     @staticmethod
     def parse_collateral_lineage(
-        sample_lineage: dict[str, Lineage],
-    ) -> dict[str, Lineage]:
-        for sample_i, sample_j in itertools.combinations(sample_lineage.keys(), 2):
+        samples: dict[str, SampleMeta],
+    ) -> dict[str, SampleMeta]:
+        for sample_i, sample_j in itertools.combinations(samples.keys(), 2):
             # If other sample is mother or father, continue
             if (
-                sample_j == sample_lineage[sample_i].mother
-                or sample_j == sample_lineage[sample_i].father
+                sample_j == samples[sample_i].mother
+                or sample_j == samples[sample_i].father
             ):
                 continue
 
             # If both parents are not None and the same, samples are siblings.
             if (
-                sample_lineage[sample_i].mother
-                and sample_lineage[sample_i].father
-                and (sample_lineage[sample_i].mother == sample_lineage[sample_j].mother)
-                and (sample_lineage[sample_i].father == sample_lineage[sample_j].father)
+                samples[sample_i].mother
+                and samples[sample_i].father
+                and (samples[sample_i].mother == samples[sample_j].mother)
+                and (samples[sample_i].father == samples[sample_j].father)
             ):
-                sample_lineage[sample_i].siblings.append(
+                samples[sample_i].siblings.append(
                     sample_j,
                 )
                 continue
 
             # If only a single parent is non-null and the same, samples are half siblings
             if (
-                sample_lineage[sample_i].mother
-                and sample_lineage[sample_i].mother == sample_lineage[sample_j].mother
+                samples[sample_i].mother
+                and samples[sample_i].mother == samples[sample_j].mother
             ) or (
-                sample_lineage[sample_i].father
-                and sample_lineage[sample_i].father == sample_lineage[sample_j].father
+                samples[sample_i].father
+                and samples[sample_i].father == samples[sample_j].father
             ):
-                sample_lineage[sample_i].half_siblings.append(
+                samples[sample_i].half_siblings.append(
                     sample_j,
                 )
                 continue
@@ -112,42 +98,28 @@ class Family:
             # If either set of one sample's grandparents is equal the other's parents,
             # they're aunt/uncle
             if (
-                sample_lineage[sample_i].maternal_grandmother
-                and sample_lineage[sample_i].maternal_grandfather
-                and (
-                    sample_lineage[sample_i].maternal_grandmother
-                    == sample_lineage[sample_j].mother
-                )
-                and (
-                    sample_lineage[sample_i].maternal_grandfather
-                    == sample_lineage[sample_j].father
-                )
+                samples[sample_i].maternal_grandmother
+                and samples[sample_i].maternal_grandfather
+                and (samples[sample_i].maternal_grandmother == samples[sample_j].mother)
+                and (samples[sample_i].maternal_grandfather == samples[sample_j].father)
             ) or (
-                sample_lineage[sample_i].paternal_grandmother
-                and sample_lineage[sample_i].paternal_grandfather
-                and (
-                    sample_lineage[sample_i].paternal_grandmother
-                    == sample_lineage[sample_j].mother
-                )
-                and (
-                    sample_lineage[sample_i].paternal_grandfather
-                    == sample_lineage[sample_j].father
-                )
+                samples[sample_i].paternal_grandmother
+                and samples[sample_i].paternal_grandfather
+                and (samples[sample_i].paternal_grandmother == samples[sample_j].mother)
+                and (samples[sample_i].paternal_grandfather == samples[sample_j].father)
             ):
-                sample_lineage[sample_i].aunt_uncles.append(
+                samples[sample_i].aunt_uncles.append(
                     sample_j,
                 )
-        return sample_lineage
+        return samples
 
     @classmethod
     def parse(cls, family_guid: str, rows: list[hl.Struct]) -> Family:
-        sample_lineage = cls.parse_direct_lineage(rows)
-        sample_lineage = cls.parse_collateral_lineage(sample_lineage)
-        sample_sex = cls.parse_sample_sex(rows)
+        samples = cls.parse_direct_lineage(rows)
+        samples = cls.parse_collateral_lineage(samples)
         return cls(
             family_guid=family_guid,
-            sample_lineage=sample_lineage,
-            sample_sex=sample_sex,
+            samples=samples,
         )
 
 
--- v03_pipeline/lib/misc/pedigree_test.py
+++ v03_pipeline/lib/misc/pedigree_test.py
@@ -6,7 +6,7 @@ from v03_pipeline.lib.methods.sex_check import Ploidy
 from v03_pipeline.lib.misc.io import import_pedigree
 from v03_pipeline.lib.misc.pedigree import (
     Family,
-    Lineage,
+    SampleMeta,
     parse_pedigree_ht_to_families,
 )
 
@@ -29,24 +29,55 @@ class PedigreesTest(unittest.TestCase):
         #                   sample_4, sample_5          sample_2        |    sample_1
         #
         #
-        direct_lineage = Family.parse_direct_lineage(
+        samples = Family.parse_direct_lineage(
             [
-                hl.Struct(s='sample_1', maternal_s=None, paternal_s=None),
-                hl.Struct(s='sample_2', maternal_s='sample_3', paternal_s=None),
-                hl.Struct(s='sample_3', maternal_s=None, paternal_s='sample_7'),
-                hl.Struct(s='sample_4', maternal_s='sample_3', paternal_s='sample_8'),
-                hl.Struct(s='sample_5', maternal_s='sample_3', paternal_s='sample_8'),
-                hl.Struct(s='sample_6', maternal_s='sample_9', paternal_s='sample_10'),
-                hl.Struct(s='sample_7', maternal_s=None, paternal_s=None),
-                hl.Struct(s='sample_8', maternal_s='sample_9', paternal_s='sample_10'),
-                hl.Struct(s='sample_9', maternal_s=None, paternal_s=None),
-                hl.Struct(s='sample_10', maternal_s=None, paternal_s=None),
+                hl.Struct(s='sample_1', maternal_s=None, paternal_s=None, sex='F'),
+                hl.Struct(
+                    s='sample_2',
+                    maternal_s='sample_3',
+                    paternal_s=None,
+                    sex='M',
+                ),
+                hl.Struct(
+                    s='sample_3',
+                    maternal_s=None,
+                    paternal_s='sample_7',
+                    sex='F',
+                ),
+                hl.Struct(
+                    s='sample_4',
+                    maternal_s='sample_3',
+                    paternal_s='sample_8',
+                    sex='M',
+                ),
+                hl.Struct(
+                    s='sample_5',
+                    maternal_s='sample_3',
+                    paternal_s='sample_8',
+                    sex='M',
+                ),
+                hl.Struct(
+                    s='sample_6',
+                    maternal_s='sample_9',
+                    paternal_s='sample_10',
+                    sex='M',
+                ),
+                hl.Struct(s='sample_7', maternal_s=None, paternal_s=None, sex='M'),
+                hl.Struct(
+                    s='sample_8',
+                    maternal_s='sample_9',
+                    paternal_s='sample_10',
+                    sex='F',
+                ),
+                hl.Struct(s='sample_9', maternal_s=None, paternal_s=None, sex='F'),
+                hl.Struct(s='sample_10', maternal_s=None, paternal_s=None, sex='M'),
             ],
         )
         self.assertEqual(
-            Family.parse_collateral_lineage(direct_lineage),
+            Family.parse_collateral_lineage(samples),
             {
-                'sample_1': Lineage(
+                'sample_1': SampleMeta(
+                    sex=Ploidy.FEMALE,
                     mother=None,
                     father=None,
                     maternal_grandmother=None,
@@ -57,7 +88,8 @@ class PedigreesTest(unittest.TestCase):
                     half_siblings=[],
                     aunt_uncles=[],
                 ),
-                'sample_2': Lineage(
+                'sample_2': SampleMeta(
+                    sex=Ploidy.MALE,
                     mother='sample_3',
                     father=None,
                     maternal_grandmother=None,
@@ -68,7 +100,8 @@ class PedigreesTest(unittest.TestCase):
                     half_siblings=['sample_4', 'sample_5'],
                     aunt_uncles=[],
                 ),
-                'sample_3': Lineage(
+                'sample_3': SampleMeta(
+                    sex=Ploidy.FEMALE,
                     mother=None,
                     father='sample_7',
                     maternal_grandmother=None,
@@ -79,7 +112,8 @@ class PedigreesTest(unittest.TestCase):
                     half_siblings=[],
                     aunt_uncles=[],
                 ),
-                'sample_4': Lineage(
+                'sample_4': SampleMeta(
+                    sex=Ploidy.MALE,
                     mother='sample_3',
                     father='sample_8',
                     maternal_grandmother=None,
@@ -90,7 +124,8 @@ class PedigreesTest(unittest.TestCase):
                     half_siblings=[],
                     aunt_uncles=['sample_6'],
                 ),
-                'sample_5': Lineage(
+                'sample_5': SampleMeta(
+                    sex=Ploidy.MALE,
                     mother='sample_3',
                     father='sample_8',
                     maternal_grandmother=None,
@@ -101,7 +136,8 @@ class PedigreesTest(unittest.TestCase):
                     half_siblings=[],
                     aunt_uncles=['sample_6'],
                 ),
-                'sample_6': Lineage(
+                'sample_6': SampleMeta(
+                    sex=Ploidy.MALE,
                     mother='sample_9',
                     father='sample_10',
                     maternal_grandmother=None,
@@ -112,7 +148,8 @@ class PedigreesTest(unittest.TestCase):
                     half_siblings=[],
                     aunt_uncles=[],
                 ),
-                'sample_7': Lineage(
+                'sample_7': SampleMeta(
+                    sex=Ploidy.MALE,
                     mother=None,
                     father=None,
                     maternal_grandmother=None,
@@ -123,7 +160,8 @@ class PedigreesTest(unittest.TestCase):
                     half_siblings=[],
                     aunt_uncles=[],
                 ),
-                'sample_8': Lineage(
+                'sample_8': SampleMeta(
+                    sex=Ploidy.FEMALE,
                     mother='sample_9',
                     father='sample_10',
                     maternal_grandmother=None,
@@ -134,7 +172,8 @@ class PedigreesTest(unittest.TestCase):
                     half_siblings=[],
                     aunt_uncles=[],
                 ),
-                'sample_9': Lineage(
+                'sample_9': SampleMeta(
+                    sex=Ploidy.FEMALE,
                     mother=None,
                     father=None,
                     maternal_grandmother=None,
@@ -145,7 +184,8 @@ class PedigreesTest(unittest.TestCase):
                     half_siblings=[],
                     aunt_uncles=[],
                 ),
-                'sample_10': Lineage(
+                'sample_10': SampleMeta(
+                    sex=Ploidy.MALE,
                     mother=None,
                     father=None,
                     maternal_grandmother=None,
@@ -167,7 +207,8 @@ class PedigreesTest(unittest.TestCase):
                 Family(
                     family_guid='BBL_BC1-000345_1',
                     sample_lineage={
-                        'BBL_BC1-000345_01_D1': Lineage(
+                        'BBL_BC1-000345_01_D1': SampleMeta(
+                            sex=Ploidy.FEMALE,
                             mother='BBL_BC1-000345_03_D1',
                             father='BBL_BC1-000345_02_D1',
                             maternal_grandmother=None,
@@ -178,7 +219,8 @@ class PedigreesTest(unittest.TestCase):
                             half_siblings=[],
                             aunt_uncles=[],
                         ),
-                        'BBL_BC1-000345_02_D1': Lineage(
+                        'BBL_BC1-000345_02_D1': SampleMeta(
+                            sex=Ploidy.MALE,
                             mother=None,
                             father=None,
                             maternal_grandmother=None,
@@ -189,7 +231,8 @@ class PedigreesTest(unittest.TestCase):
                             half_siblings=[],
                             aunt_uncles=[],
                         ),
-                        'BBL_BC1-000345_03_D1': Lineage(
+                        'BBL_BC1-000345_03_D1': SampleMeta(
+                            sex=Ploidy.FEMALE,
                             mother=None,
                             father=None,
                             maternal_grandmother=None,
@@ -200,17 +243,13 @@ class PedigreesTest(unittest.TestCase):
                             half_siblings=[],
                             aunt_uncles=[],
                         ),
-                    },
-                    sample_sex={
-                        'BBL_BC1-000345_01_D1': Ploidy.FEMALE,
-                        'BBL_BC1-000345_02_D1': Ploidy.MALE,
-                        'BBL_BC1-000345_03_D1': Ploidy.FEMALE,
                     },
                 ),
                 Family(
                     family_guid='BBL_HT-007-5195_1',
                     sample_lineage={
-                        'BBL_HT-007-5195_01_D1': Lineage(
+                        'BBL_HT-007-5195_01_D1': SampleMeta(
+                            sex=Ploidy.FEMALE,
                             mother='BBL_HT-007-5195_03_D1',
                             father='BBL_HT-007-5195_02_D1',
                             maternal_grandmother=None,
@@ -225,7 +264,8 @@ class PedigreesTest(unittest.TestCase):
                             half_siblings=[],
                             aunt_uncles=[],
                         ),
-                        'BBL_HT-007-5195_02_D1': Lineage(
+                        'BBL_HT-007-5195_02_D1': SampleMeta(
+                            sex=Ploidy.MALE,
                             mother=None,
                             father=None,
                             maternal_grandmother=None,
@@ -236,7 +276,8 @@ class PedigreesTest(unittest.TestCase):
                             half_siblings=[],
                             aunt_uncles=[],
                         ),
-                        'BBL_HT-007-5195_03_D1': Lineage(
+                        'BBL_HT-007-5195_03_D1': SampleMeta(
+                            sex=Ploidy.FEMALE,
                             mother=None,
                             father=None,
                             maternal_grandmother=None,
@@ -247,7 +288,8 @@ class PedigreesTest(unittest.TestCase):
                             half_siblings=[],
                             aunt_uncles=[],
                         ),
-                        'BBL_HT-007-5195_04_D1': Lineage(
+                        'BBL_HT-007-5195_04_D1': SampleMeta(
+                            sex=Ploidy.MALE,
                             mother='BBL_HT-007-5195_03_D1',
                             father='BBL_HT-007-5195_02_D1',
                             maternal_grandmother=None,
@@ -258,7 +300,8 @@ class PedigreesTest(unittest.TestCase):
                             half_siblings=[],
                             aunt_uncles=[],
                         ),
-                        'BBL_HT-007-5195_05_D1': Lineage(
+                        'BBL_HT-007-5195_05_D1': SampleMeta(
+                            sex=Ploidy.FEMALE,
                             mother='BBL_HT-007-5195_03_D1',
                             father='BBL_HT-007-5195_02_D1',
                             maternal_grandmother=None,
@@ -269,7 +312,8 @@ class PedigreesTest(unittest.TestCase):
                             half_siblings=[],
                             aunt_uncles=[],
                         ),
-                        'BBL_HT-007-5195_06_D1': Lineage(
+                        'BBL_HT-007-5195_06_D1': SampleMeta(
+                            sex=Ploidy.MALE,
                             mother='BBL_HT-007-5195_03_D1',
                             father='BBL_HT-007-5195_02_D1',
                             maternal_grandmother=None,
@@ -281,19 +325,12 @@ class PedigreesTest(unittest.TestCase):
                             aunt_uncles=[],
                         ),
                     },
-                    sample_sex={
-                        'BBL_HT-007-5195_01_D1': Ploidy.FEMALE,
-                        'BBL_HT-007-5195_02_D1': Ploidy.MALE,
-                        'BBL_HT-007-5195_03_D1': Ploidy.FEMALE,
-                        'BBL_HT-007-5195_04_D1': Ploidy.MALE,
-                        'BBL_HT-007-5195_05_D1': Ploidy.FEMALE,
-                        'BBL_HT-007-5195_06_D1': Ploidy.MALE,
-                    },
                 ),
                 Family(
                     family_guid='BBL_SDS1-000178_1',
                     sample_lineage={
-                        'BBL_SDS1-000178_01_D1': Lineage(
+                        'BBL_SDS1-000178_01_D1': SampleMeta(
+                            sex=Ploidy.FEMALE,
                             mother=None,
                             father=None,
                             maternal_grandmother=None,
@@ -304,9 +341,6 @@ class PedigreesTest(unittest.TestCase):
                             half_siblings=[],
                             aunt_uncles=[],
                         ),
-                    },
-                    sample_sex={
-                        'BBL_SDS1-000178_01_D1': Ploidy.FEMALE,
                     },
                 ),
             ],
