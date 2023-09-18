7960a4e21742c629c82d06d6ee82093400214687
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-09-18 14:33:04 -0400

    whoops that was the wrong direction

--- v03_pipeline/lib/test/mocked_dataroot_testcase.py
+++ v03_pipeline/lib/test/mocked_dataroot_testcase.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import tempfile
 import unittest
 from unittest.mock import patch
@@ -11,12 +12,12 @@ class MockedDatarootTestCase(unittest.TestCase):
         self.patcher = patch('v03_pipeline.lib.paths.Env')
         self.mock_env = self.patcher.start()
         for field_name in Env.__dataclass_fields__:
-            if 'DATASETS' in field_name:
+            if 'DATASETS' not in field_name:
                 continue
             setattr(self.mock_env, field_name, tempfile.TemporaryDirectory().name)
 
     def tearDown(self) -> None:
         for field_name in Env.__dataclass_fields__:
             if os.path.isdir(getattr(self.mock_env, field_name)):
-                os.path.rmtree(getattr(self.mock_env, field_name))
+                shutil.rmtree(getattr(self.mock_env, field_name))
         self.patcher.stop()
