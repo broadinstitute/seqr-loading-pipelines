c9ae11f9fb8d4004281eda6ed12e275d78266416
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-09-18 13:24:25 -0400

    lint

--- v03_pipeline/lib/tasks/write_relatedness_check_table.py
+++ v03_pipeline/lib/tasks/write_relatedness_check_table.py
@@ -4,7 +4,10 @@ import hail as hl
 import luigi
 
 from v03_pipeline.lib.methods.relatedness import call_relatedness
-from v03_pipeline.lib.paths import relatedness_check_table_path, valid_cached_reference_dataset_query_path
+from v03_pipeline.lib.paths import (
+    relatedness_check_table_path,
+    valid_cached_reference_dataset_query_path,
+)
 from v03_pipeline.lib.tasks.base.base_write_task import BaseWriteTask
 from v03_pipeline.lib.tasks.files import GCSorLocalTarget
 from v03_pipeline.lib.tasks.write_imported_callset import WriteImportedCallsetTask
@@ -23,7 +26,7 @@ class WriteRelatednessCheckTableTask(BaseWriteTask):
         )
 
     def requires(self) -> luigi.Task:
-        requirements =  [
+        requirements = [
             WriteImportedCallsetTask(
                 self.reference_genome,
                 self.dataset_type,
