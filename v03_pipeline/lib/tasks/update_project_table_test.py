4b070800a8c93eae982cb8ec0c34fd9c79a8c235
Benjamin Blankenmeister <b.p.blankenmeister@gmail.com>
2023-09-20 09:37:10 -0400

    Update update_project_table_test.py

--- v03_pipeline/lib/tasks/update_project_table_test.py
+++ v03_pipeline/lib/tasks/update_project_table_test.py
@@ -11,9 +11,6 @@ TEST_PEDIGREE_3 = 'v03_pipeline/var/test/pedigrees/test_pedigree_3.tsv'
 
 
 class UpdateProjectTableTaskTest(MockedDatarootTestCase):
-    def setUp(self) -> None:
-        super().setUp()
-
     def test_update_project_table_task(self) -> None:
         worker = luigi.worker.Worker()
         upt_task = UpdateProjectTableTask(
