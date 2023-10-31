6096610dea77f6ba65153a9e8822fad7f85939e3
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-10-31 17:18:28 -0400

    add prod hail_scripts utils

---
+++ hail_scripts/utils/download_utils.py
@@ -0,0 +1,104 @@
+from contextlib import contextmanager
+import logging
+import os
+import requests
+import tempfile
+
+import hail as hl
+from google.cloud import storage
+
+logger = logging.getLogger(__name__)
+
+def parse_gs_path_to_bucket(gs_path):
+    bucket_name = gs_path.replace('gs://', '').split('/')[0]
+    file_name = gs_path.split(bucket_name)[-1].lstrip('/')
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    return bucket, file_name
+
+
+def stream_gs_file(gs_path, raw_download=False):
+    logger.info(f'Stream from GCS: {gs_path}')
+    bucket, file_name = parse_gs_path_to_bucket(gs_path)
+
+    blob = bucket.get_blob(file_name)
+
+    return blob and blob.download_as_string(raw_download=raw_download)
+
+@contextmanager
+def file_writer(file_path, get_existing_size=False):
+    bucket = None
+    size = None
+    if is_gs_path(file_path):
+        local_file_path = os.path.join(tempfile.gettempdir(), os.path.basename(file_path))
+        bucket, file_name = parse_gs_path_to_bucket(file_path)
+        if get_existing_size:
+            blob = bucket.get_blob(file_name)
+            size = blob and blob.size
+    else:
+        local_file_path = file_path
+        if get_existing_size:
+            size = os.path.isfile(local_file_path) and os.path.getsize(local_file_path)
+
+    local_file = open(local_file_path, 'wb')
+
+    yield local_file, size
+
+    local_file.close()
+
+    if bucket:
+        blob = bucket.blob(file_name)
+        blob.upload_from_filename(local_file_path)
+
+
+def is_gs_path(path):
+    return path.startswith('gs://')
+
+
+def path_exists(path):
+    is_gs = is_gs_path(path)
+    return (is_gs and hl.hadoop_exists(path)) or (not is_gs and os.path.exists(path))
+
+
+DEFAULT_TO_DIR = tempfile.gettempdir()
+
+
+def download_file(url, to_dir=None, verbose=True):
+    """Download the given file and returns its local path.
+     Args:
+        url (string): HTTP or FTP url
+        to_dir: optional save to directory
+        verbose: display verbose information
+     Returns:
+        string: local file path
+    """
+    if to_dir is None:
+        to_dir = DEFAULT_TO_DIR
+
+    if not (url and url.startswith(("http://", "https://"))):
+        raise ValueError("Invalid url: {}".format(url))
+    remote_file_size = _get_remote_file_size(url)
+
+    file_path = os.path.join(to_dir, os.path.basename(url))
+    with file_writer(file_path, get_existing_size=True) as fw:
+        f, file_size = fw
+        if file_size and file_size == remote_file_size:
+            logger.info("Re-using {} previously downloaded from {}".format(file_path, url))
+            return file_path
+
+        is_gz = url.endswith(".gz")
+        response = requests.get(url, stream=is_gz)
+        input_iter = response if is_gz else response.iter_content()
+        if verbose:
+            logger.info("Downloading {} to {}".format(url, file_path))
+
+        f.writelines(input_iter)
+        input_iter.close()
+
+    return file_path
+
+
+def _get_remote_file_size(url):
+    return int(requests.head(url).headers.get('Content-Length', '0'))
---
+++ hail_scripts/utils/mapping_gene_ids.py
@@ -0,0 +1,99 @@
+import gzip
+import logging
+import os
+import pickle
+
+from hail_scripts.utils.download_utils import download_file, path_exists, is_gs_path, file_writer, stream_gs_file
+
+GENOME_VERSION_GRCh37 = "37"
+GENOME_VERSION_GRCh38 = "38"
+
+logger = logging.getLogger(__name__)
+
+GENCODE_GTF_URL = "http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_{gencode_release}/gencode.v{gencode_release}.annotation.gtf.gz"
+
+# expected GTF file header
+GENCODE_FILE_HEADER = [
+    'chrom', 'source', 'feature_type', 'start', 'end', 'score', 'strand', 'phase', 'info'
+]
+
+
+def _get_pickle_file(path):
+    root, ext = os.path.splitext(path)
+    return root + '.pickle'
+
+
+def _load_parsed_data_or_download(gencode_release, download_path):
+    gene_id_mapping = {}
+    url = GENCODE_GTF_URL.format(gencode_release=gencode_release)
+    gencode_gtf_path = os.path.join(download_path, os.path.basename(url))
+    pickle_file = _get_pickle_file(gencode_gtf_path)
+    if path_exists(pickle_file):
+        logger.info('Use the existing pickle file {}.\nIf you want to reload the data, please delete it and re-run the data loading.'.format(pickle_file))
+        if is_gs_path(pickle_file):
+            p = pickle.loads(stream_gs_file(pickle_file))
+        else:
+            with open(pickle_file, 'rb') as handle:
+                p = pickle.load(handle)
+        gene_id_mapping.update(p)
+    elif not path_exists(gencode_gtf_path):
+        gencode_gtf_path = download_file(url, to_dir=download_path)
+        logger.info('Downloaded to {}'.format(gencode_gtf_path))
+    else:
+        logger.info('Use the existing downloaded file {}.\nIf you want to re-download it, please delete the file and re-run the pipeline.'.format(gencode_gtf_path))
+
+    return gene_id_mapping, gencode_gtf_path
+
+
+def _parse_gtf_data(gencode_gtf_path):
+    gene_id_mapping = {}
+    logger.info("Loading {}".format(gencode_gtf_path))
+    is_gs = is_gs_path(gencode_gtf_path)
+    gencode_file = gzip.decompress(stream_gs_file(gencode_gtf_path, raw_download=True)).decode().split('\n') \
+        if is_gs else gzip.open(gencode_gtf_path, 'rt')
+    for i, line in enumerate(gencode_file):
+        line = line.rstrip('\r\n')
+        if not line or line.startswith('#'):
+            continue
+        fields = line.split('\t')
+
+        if len(fields) != len(GENCODE_FILE_HEADER):
+            raise ValueError("Unexpected number of fields on line #%s: %s" % (i, fields))
+
+        record = dict(zip(GENCODE_FILE_HEADER, fields))
+
+        if record['feature_type'] != 'gene':
+            continue
+
+        # parse info field
+        info_fields = [x.strip().split() for x in record['info'].split(';') if x != '']
+        info_fields = {k: v.strip('"') for k, v in info_fields}
+
+        gene_id_mapping[info_fields['gene_name']] = info_fields['gene_id'].split('.')[0]
+
+    if not is_gs:
+        gencode_file.close()
+
+    pickle_file = _get_pickle_file(gencode_gtf_path)
+    logger.info(f'Saving to pickle {pickle_file}')
+    with file_writer(pickle_file) as fw:
+        f, _ = fw
+        pickle.dump(gene_id_mapping, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return gene_id_mapping
+
+
+def load_gencode(gencode_release, download_path=''):
+    """Load Gencode to create a gene symbols to gene ids mapping table.
+
+    Args:
+        gencode_release (int): the gencode release to load (eg. 25)
+        download_path (str): The path for downloaded data
+    """
+    gene_id_mapping, gencode_gtf_path = _load_parsed_data_or_download(gencode_release, download_path)
+
+    if not gene_id_mapping:
+        gene_id_mapping = _parse_gtf_data(gencode_gtf_path)
+
+    logger.info('Got {} gene id mapping records'.format(len(gene_id_mapping)))
+    return gene_id_mapping
