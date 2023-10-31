827874fbd4112d82d5ab18ddf773f13ebb765afd
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-10-30 21:49:04 -0400

    another one

--- v03_pipeline/lib/reference_data/hgmd.py
+++ v03_pipeline/lib/reference_data/hgmd.py
@@ -9,7 +9,7 @@ def download_and_import_hgmd_vcf(
         raise ValueError('Invalid genome_version: ' + str(genome_version))
     mt = hl.import_vcf(
         hgmd_url,
-        genome_version=genome_version,
+        genome_version=f'GRCh{genome_version}',
         force=True,
         min_partitions=100,
         skip_invalid_loci=True,
