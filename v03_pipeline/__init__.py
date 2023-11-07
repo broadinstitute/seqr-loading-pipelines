11facaa1992ba996aa3e01eccbaebec23f8bbd82
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-11-07 15:50:05 -0500

    Fix version in build

--- v03_pipeline/__init__.py
+++ v03_pipeline/__init__.py
@@ -2,4 +2,4 @@ import datetime
 
 MAJOR_VERSION = 3
 
-__version__ = f'{MAJOR_VERSION}.{datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d.%H-%M-%S")}'
+__version__ = f'{MAJOR_VERSION}.{datetime.datetime.now(datetime.timezone.utc).strftime("%Y%m%d.%H%M%S")}'
