c8c4bd415290eb9dfc61b1c8313e2af602c846eb
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-01-04 11:13:17 -0500

    misc pipeline tweaks

---
+++ v03_pipeline/lib/misc/math.py
@@ -0,0 +1,4 @@
+def constrain(number: int | float, lower_bound: int | float, upper_bound: int | float) -> int:
+    if lower_bound > upper_bound:
+        raise ValueError("Lower bound should be less than or equal to upper bound")
+    return max(lower_bound, min(number, upper_bound))
