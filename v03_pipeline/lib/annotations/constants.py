5f90d87189ab468d4527707c08008590c3646fe5
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-01-25 17:56:03 -0500

    Correct training projects

--- v03_pipeline/lib/annotations/constants.py
+++ v03_pipeline/lib/annotations/constants.py
@@ -1,5 +1,5 @@
 PROJECTS_EXCLUDED_FROM_GT_STATS = {
-    # TODO get final list of projects
+    'R0555_seqr_demo',
     'R0607_gregor_training_project_',
-    'R0610_gregor_training_project_',
+    'R0608_gregor_training_project_',
 }
