429db5e6626696d6525e415b0e487a776fa463dc
Julia Klugherz <juliaklugherz@gmail.com>
2024-01-30 11:47:37 -0500

    fix 1 test

--- v03_pipeline/lib/tasks/reference_data/update_variant_annotations_table_with_updated_reference_dataset_test.py
+++ v03_pipeline/lib/tasks/reference_data/update_variant_annotations_table_with_updated_reference_dataset_test.py
@@ -140,6 +140,7 @@ class UpdateVATWithUpdatedRDC(MockedDatarootTestCase):
                             ],
                         ),
                     ),
+                    updates=set(),
                 ),
             ],
         )
