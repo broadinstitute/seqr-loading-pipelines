6803c9b5fef99c4cf69cd3af455c95c6a0f68faa
Julia Klugherz <juliaklugherz@gmail.com>
2024-02-23 16:09:08 -0500

    ruff format'

--- v03_pipeline/lib/tasks/reference_data/update_variant_annotations_table_with_updated_reference_dataset_test.py
+++ v03_pipeline/lib/tasks/reference_data/update_variant_annotations_table_with_updated_reference_dataset_test.py
@@ -388,7 +388,7 @@ class UpdateVATWithUpdatedRDC(MockedDatarootTestCase):
                     clinvar_mito=None,
                     dbnsfp_mito=hl.Struct(
                         SIFT_score=None,
-                        MutationTaster_pred="N",
+                        MutationTaster_pred='N',
                     ),
                     gnomad_mito=None,
                     helix_mito=hl.Struct(
