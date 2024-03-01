1ff8e920ea338e232b6cc1825b764200014fce17
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-02-29 19:19:54 -0500

    env vars

--- v03_pipeline/lib/vep.py
+++ v03_pipeline/lib/vep.py
@@ -1,12 +1,10 @@
-import os
-
 import hail as hl
 
-from v03_pipeline.lib.model import DatasetType, ReferenceGenome
+from v03_pipeline.lib.model import DatasetType, Env, ReferenceGenome
 
 
 def validate_vep_config_reference_genome(reference_genome) -> None:
-    with open(os.environ['VEP_CONFIG_PATH']) as f:
+    with open(Env.VEP_CONFIG_PATH) as f:
         if reference_genome.value not in f.read():
             msg = f'Vep config does not match supplied reference genome {reference_genome.value}'
             raise ValueError(msg)
@@ -22,7 +20,7 @@ def run_vep(
     validate_vep_config_reference_genome(reference_genome)
     return hl.vep(
         ht,
-        # We set no config option here to instead use the `VEP_CONFIG_URI` environment variable.
+        config=Env.VEP_CONFIG_URI,
         name='vep',
         block_size=1000,
         tolerate_parse_error=True,
