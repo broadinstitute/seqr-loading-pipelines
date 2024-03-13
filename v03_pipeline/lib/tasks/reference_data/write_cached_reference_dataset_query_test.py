d23c99b1b2e9ff2ad8b707cc6780a5130feb7570
Julia Klugherz <juliaklugherz@gmail.com>
2024-03-13 13:35:35 -0400

    crdq wrapper task

---
+++ v03_pipeline/lib/tasks/reference_data/write_cached_reference_dataset_query_test.py
@@ -0,0 +1,119 @@
+import unittest
+from unittest import mock
+
+import luigi
+
+from v03_pipeline.lib.model import (
+    CachedReferenceDatasetQuery,
+    DatasetType,
+    ReferenceGenome,
+    SampleType,
+)
+from v03_pipeline.lib.tasks.reference_data.write_cached_reference_dataset_query import (
+    WriteCachedReferenceDatasetQuery,
+)
+from v03_pipeline.lib.test.mock_complete_task import MockCompleteTask
+
+
+@mock.patch(
+    'v03_pipeline.lib.tasks.reference_data.write_cached_reference_dataset_query.UpdatedCachedReferenceDatasetQuery',
+)
+class WriteCachedReferenceDatasetQueryTest(unittest.TestCase):
+    def test_37_snv_indel(self, mock_crdq_task):
+        mock_crdq_task.return_value = MockCompleteTask()
+        worker = luigi.worker.Worker()
+        task = WriteCachedReferenceDatasetQuery(
+            reference_genome=ReferenceGenome.GRCh37,
+            dataset_type=DatasetType.SNV_INDEL,
+        )
+        worker.add(task)
+        worker.run()
+        self.assertTrue(task.complete())
+        mock_crdq_task.assert_has_calls(
+            [
+                mock.call(
+                    reference_genome=ReferenceGenome.GRCh37,
+                    dataset_type=DatasetType.SNV_INDEL,
+                    sample_type=SampleType.WGS,
+                    crdq=CachedReferenceDatasetQuery.CLINVAR_PATH_VARIANTS,
+                ),
+                mock.call(
+                    reference_genome=ReferenceGenome.GRCh37,
+                    dataset_type=DatasetType.SNV_INDEL,
+                    sample_type=SampleType.WGS,
+                    crdq=CachedReferenceDatasetQuery.GNOMAD_CODING_AND_NONCODING_VARIANTS,
+                ),
+                mock.call(
+                    reference_genome=ReferenceGenome.GRCh37,
+                    dataset_type=DatasetType.SNV_INDEL,
+                    sample_type=SampleType.WGS,
+                    crdq=CachedReferenceDatasetQuery.GNOMAD_QC,
+                ),
+                mock.call(
+                    reference_genome=ReferenceGenome.GRCh37,
+                    dataset_type=DatasetType.SNV_INDEL,
+                    sample_type=SampleType.WGS,
+                    crdq=CachedReferenceDatasetQuery.HIGH_AF_VARIANTS,
+                ),
+            ],
+        )
+
+    def test_38_snv_indel(self, mock_crdq_task):
+        mock_crdq_task.return_value = MockCompleteTask()
+        worker = luigi.worker.Worker()
+        task = WriteCachedReferenceDatasetQuery(
+            reference_genome=ReferenceGenome.GRCh38,
+            dataset_type=DatasetType.SNV_INDEL,
+        )
+        worker.add(task)
+        worker.run()
+        self.assertTrue(task.complete())
+        mock_crdq_task.assert_has_calls(
+            [
+                mock.call(
+                    reference_genome=ReferenceGenome.GRCh38,
+                    dataset_type=DatasetType.SNV_INDEL,
+                    sample_type=SampleType.WGS,
+                    crdq=CachedReferenceDatasetQuery.CLINVAR_PATH_VARIANTS,
+                ),
+                mock.call(
+                    reference_genome=ReferenceGenome.GRCh38,
+                    dataset_type=DatasetType.SNV_INDEL,
+                    sample_type=SampleType.WGS,
+                    crdq=CachedReferenceDatasetQuery.GNOMAD_CODING_AND_NONCODING_VARIANTS,
+                ),
+                mock.call(
+                    reference_genome=ReferenceGenome.GRCh38,
+                    dataset_type=DatasetType.SNV_INDEL,
+                    sample_type=SampleType.WGS,
+                    crdq=CachedReferenceDatasetQuery.GNOMAD_QC,
+                ),
+                mock.call(
+                    reference_genome=ReferenceGenome.GRCh38,
+                    dataset_type=DatasetType.SNV_INDEL,
+                    sample_type=SampleType.WGS,
+                    crdq=CachedReferenceDatasetQuery.HIGH_AF_VARIANTS,
+                ),
+            ],
+        )
+
+    def test_38_mito(self, mock_crdq_task):
+        mock_crdq_task.return_value = MockCompleteTask()
+        worker = luigi.worker.Worker()
+        task = WriteCachedReferenceDatasetQuery(
+            reference_genome=ReferenceGenome.GRCh38,
+            dataset_type=DatasetType.MITO,
+        )
+        worker.add(task)
+        worker.run()
+        self.assertTrue(task.complete())
+        mock_crdq_task.assert_has_calls(
+            [
+                mock.call(
+                    reference_genome=ReferenceGenome.GRCh38,
+                    dataset_type=DatasetType.MITO,
+                    sample_type=SampleType.WGS,
+                    crdq=CachedReferenceDatasetQuery.CLINVAR_PATH_VARIANTS,
+                ),
+            ],
+        )
