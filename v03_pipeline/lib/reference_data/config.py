6a60a1541bcc0cff8a63eafbe0954f021a22cccc
Julia Klugherz <juliaklugherz@gmail.com>
2024-04-01 17:58:36 -0400

    change source path of gnomad_qc buckets

--- v03_pipeline/lib/reference_data/config.py
+++ v03_pipeline/lib/reference_data/config.py
@@ -381,12 +381,14 @@ CONFIG = {
         '37': {
             'version': 'v2',
             'custom_import': import_matrix_table,
-            'source_path': 'gs://gnomad/sample_qc/mt/gnomad.joint.high_callrate_common_biallelic_snps.pruned.mt',
+            # Note: copied from 'gs://gnomad/sample_qc/mt/gnomad.joint.high_callrate_common_biallelic_snps.pruned.mt'
+            'source_path': 'gs://seqr-reference-data/gnomad_qc/GRCh37/gnomad.joint.high_callrate_common_biallelic_snps.pruned.mt',
         },
         '38': {
             'version': 'v3.1',
             'custom_import': import_matrix_table,
-            'source_path': 'gs://gnomad/sample_qc/mt/genomes_v3.1/gnomad_v3.1_qc_mt_v2_sites_dense.mt',
+            # Note: copied from 'gs://gnomad/sample_qc/mt/genomes_v3.1/gnomad_v3.1_qc_mt_v2_sites_dense.mt'
+            'source_path': 'gs://seqr-reference-data/gnomad_qc/GRCh38/gnomad_v3.1_qc_mt_v2_sites_dense.mt',
         },
     },
     'exac': {
