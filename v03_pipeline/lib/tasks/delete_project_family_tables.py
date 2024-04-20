4d8730a1cec4d6b8c683a5ba66b6db3b05ca8f54
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-04-20 12:58:07 -0400

    Add delete project families

---
+++ v03_pipeline/lib/tasks/delete_project_family_tables.py
@@ -0,0 +1,45 @@
+import hail as hl
+import luigi
+
+from v03_pipeline.lib.model import DatasetType, ReferenceGenome, SampleType
+from v03_pipeline.lib.paths import project_table_path
+from v03_pipeline.lib.tasks.delete_family_table import DeleteFamilyTableTask
+from v03_pipeline.lib.tasks.files import HailTableTask
+
+
+class DeleteProjectFamilyTablesTask(luigi.Task):
+    reference_genome = luigi.EnumParameter(enum=ReferenceGenome)
+    dataset_type = luigi.EnumParameter(enum=DatasetType)
+    sample_type = luigi.EnumParameter(enum=SampleType)
+    project_guid = luigi.Parameter()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dynamic_delete_family_table_tasks = set()
+
+    def complete(self) -> bool:
+        return len(self.dynamic_delete_family_table_tasks) >= 1 and all(
+            delete_family_table_task.complete()
+            for delete_family_table_task in self.dynamic_delete_family_table_tasks
+        )
+
+    def run(self):
+        project_table_task: luigi.Target = yield HailTableTask(
+            project_table_path(
+                self.reference_genome,
+                self.dataset_type,
+                self.project_guid,
+            ),
+        )
+        project_ht = hl.read_table(project_table_task.path)
+        family_guids = hl.eval(project_ht.globals.family_guids)
+        for family_guid in family_guids:
+            self.dynamic_delete_family_table_tasks.add(
+                DeleteFamilyTableTask(
+                    reference_genome=self.reference_genome,
+                    dataset_type=self.dataset_type,
+                    sample_type=self.sample_type,
+                    family_guid=family_guid,
+                ),
+            )
+        yield self.dynamic_delete_family_table_tasks
