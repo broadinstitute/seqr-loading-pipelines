d8d339c8ded9d53dab9290cc8544454ceafbca5f
Benjamin Blankenmeister <b.p.blankenmeister@gmail.com>
2024-05-09 10:36:42 -0400

    Update v03_pipeline/lib/misc/validation_test.py
    Co-authored-by: Daniel Marten <78616802+matren395@users.noreply.github.com>

--- v03_pipeline/lib/misc/validation_test.py
+++ v03_pipeline/lib/misc/validation_test.py
@@ -38,7 +38,7 @@ class ValidationTest(unittest.TestCase):
             rows={
                 'locus': [
                     hl.Locus(
-                        contig='chr1',
+                        contig='chrX',
                         position=1,
                         reference_genome='GRCh38',
                     ),
