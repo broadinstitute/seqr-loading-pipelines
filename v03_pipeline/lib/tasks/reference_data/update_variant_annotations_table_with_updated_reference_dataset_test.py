d04325ece5722b1dac73fe053f6fe3d659c5fd5e
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-05-21 08:50:35 -0400

    fix floats

--- v03_pipeline/lib/tasks/reference_data/update_variant_annotations_table_with_updated_reference_dataset_test.py
+++ v03_pipeline/lib/tasks/reference_data/update_variant_annotations_table_with_updated_reference_dataset_test.py
@@ -760,7 +760,7 @@ class UpdateVATWithUpdatedRDC(MockedDatarootTestCase):
                         conditions=None,
                     ),
                     dbnsfp=hl.Struct(
-                        REVEL_score=hl.eval(hl.float32(0.043)),
+                        REVEL_score=0.0430000014603138,
                         SIFT_score=None,
                         Polyphen2_HVAR_score=None,
                         MutationTaster_pred_id=0,
@@ -1168,7 +1168,7 @@ class UpdateVATWithUpdatedRDC(MockedDatarootTestCase):
                         conditions=None,
                     ),
                     dbnsfp=hl.Struct(
-                        REVEL_score=0.043,
+                        REVEL_score=0.0430000014603138,
                         SIFT_score=None,
                         Polyphen2_HVAR_score=None,
                         MutationTaster_pred_id=0,
