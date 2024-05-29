d3cbdaadc311a89b6c2bda9dae265f64d146769d
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-05-29 14:26:42 -0400

    add more new fields to mock data

--- v03_pipeline/var/test/vep/mock_vep_data.py
+++ v03_pipeline/var/test/vep/mock_vep_data.py
@@ -578,6 +578,21 @@ MOCK_37_VEP_DATA = hl.struct(
 MOCK_38_VEP_DATA = hl.struct(
     most_severe_consequence='missense_variant',
     check_ref=hl.missing(hl.tstr),
+    motif_feature_consequences=hl.missing(hl.tarray(hl.tstruct(consequence_terms=hl.tarray(hl.tstr), motif_feature_id=hl.tstr))),
+    regulatory_feature_consequences=hl.array(
+        [
+            hl.struct(
+                biotype='retrotransposed',
+                consequence_terms=['regulatory_region_variant'],
+                regulatory_feature_id='regulatory_1',
+            ),
+            hl.struct(
+                biotype='retrotransposed',
+                consequence_terms=['regulatory_region_ablation'],
+                regulatory_feature_id='regulatory_2',
+            ),
+        ],
+    ),
     transcript_consequences=hl.array(
         [
             hl.struct(
