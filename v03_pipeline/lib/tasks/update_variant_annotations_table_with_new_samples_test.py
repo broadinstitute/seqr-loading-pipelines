f4ab34d555d37f130a479f4fbfe11fc034493600
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-07-10 17:41:25 -0400

    missed one

--- v03_pipeline/lib/tasks/update_variant_annotations_table_with_new_samples_test.py
+++ v03_pipeline/lib/tasks/update_variant_annotations_table_with_new_samples_test.py
@@ -588,6 +588,7 @@ class UpdateVariantAnnotationsTableWithNewSamplesTaskTest(MockedDatarootTestCase
                         screen=None,
                         hgmd='HGMD_Pro_2023',
                     ),
+                    mutations=[],
                     enums=hl.Struct(
                         cadd=hl.Struct(),
                         clinvar=hl.Struct(
