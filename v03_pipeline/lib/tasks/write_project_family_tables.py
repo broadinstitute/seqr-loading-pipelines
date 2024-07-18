2786bc056a12d3e34d982d1fb7c7673f0fca74e8
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-07-18 14:15:33 -0400

    Intersect family guids when determining which to load

--- v03_pipeline/lib/tasks/write_project_family_tables.py
+++ v03_pipeline/lib/tasks/write_project_family_tables.py
@@ -2,8 +2,11 @@ import hail as hl
 import luigi
 import luigi.util
 
+from v03_pipeline.lib.misc.io import import_pedigree
+from v03_pipeline.lib.misc.pedigree import parse_pedigree_ht_to_families
 from v03_pipeline.lib.tasks.base.base_hail_table import BaseHailTableTask
 from v03_pipeline.lib.tasks.base.base_loading_run_params import BaseLoadingRunParams
+from v03_pipeline.lib.tasks.files import RawFileTask
 from v03_pipeline.lib.tasks.update_project_table import UpdateProjectTableTask
 from v03_pipeline.lib.tasks.write_family_table import WriteFamilyTableTask
 
@@ -30,13 +33,26 @@ class WriteProjectFamilyTablesTask(BaseHailTableTask):
 
     def run(self):
         # https://luigi.readthedocs.io/en/stable/tasks.html#dynamic-dependencies
+        # Fetch family guids from project table
         update_project_table_task: luigi.Target = yield self.clone(
             UpdateProjectTableTask,
             force=False,
         )
         project_ht = hl.read_table(update_project_table_task.path)
-        family_guids = hl.eval(project_ht.globals.family_guids)
-        for family_guid in family_guids:
+        family_guids_in_project_table = set(hl.eval(project_ht.globals.family_guids))
+
+        # Fetch family guids from pedigree
+        pedigree_ht_task: luigi.Target = yield RawFileTask(self.project_pedigree_path)
+        pedigree_ht = import_pedigree(pedigree_ht_task.path)
+        families_guids_in_pedigree = {
+            f.family_guid for f in parse_pedigree_ht_to_families(pedigree_ht)
+        }
+
+        # Intersect them
+        family_guids_to_load = (
+            family_guids_in_project_table & families_guids_in_pedigree
+        )
+        for family_guid in family_guids_to_load:
             self.dynamic_write_family_table_tasks.add(
                 self.clone(WriteFamilyTableTask, family_guid=family_guid),
             )
