962a8790f184a688966a840d88b6c14373a2d5ca
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-07-24 13:00:20 -0400

    Add mito local constraint

---
+++ v03_pipeline/lib/reference_data/mito.py
@@ -0,0 +1,16 @@
+import hail as hl
+
+from v03_pipeline.lib.model.definitions import ReferenceGenome
+
+
+def download_and_import_local_constraint_tsv(
+    url: str,
+    reference_genome: ReferenceGenome,
+) -> hl.Table:
+    ht = hl.import_table(url, types={'Position': hl.tint32, 'MLC_score': hl.tfloat32})
+    ht = ht.select(
+        locus=hl.locus('chrM', ht.Position, reference_genome.value),
+        alleles=[ht.Reference, ht.Alternate],
+        MLC_Score=ht.MLC_score,
+    )
+    return ht.key_by('locus', 'alleles')
