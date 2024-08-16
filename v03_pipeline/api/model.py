3f133dc63e823c853fe4ff902a87d318a4553414
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-08-16 15:23:40 -0400

    First commit

---
+++ v03_pipeline/api/model.py
@@ -0,0 +1,27 @@
+import hailtop.fs as hfs
+from pydantic import BaseModel, Field, field_validator
+
+VALID_FILE_TYPES = ['vcf', 'vcf.gz', 'vcf.bgz', 'mt']
+
+class LoadingPipelineRequest(BaseModel):
+    callset_path: str
+    projects_to_run: list[str] = Field(min_length=1, frozen=True)
+    sample_source: SampleSource
+    sample_type: SampleType
+    reference_genome: ReferenceGenome
+    dataset_type: DatasetType
+    force: bool = False
+    ignore_missing_samples_when_remapping: bool = False
+    skip_validation: bool = False
+
+    @field_validator('callset_path')
+    @classmethod
+    def check_valid_callset_path(cls, callset_path: str) -> str:
+        if not any(
+        	callset_path.endswith(file_type)
+        	for file_type in VALID_FILE_TYPES
+        ):
+        	raise ValueError('callset_path must be a VCF or a Hail Matrix Table')
+        if not hfs.exists(callset_path):
+        	raise ValueError('callset_path must point to a file that exists')
+        return callset_path
