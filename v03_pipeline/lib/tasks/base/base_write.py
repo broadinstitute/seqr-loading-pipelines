897b9988b40ae49bf0058ad987459c86658b70da
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-08-20 16:09:26 -0400

    move it back

--- v03_pipeline/lib/tasks/base/base_write.py
+++ v03_pipeline/lib/tasks/base/base_write.py
@@ -1,6 +1,5 @@
 import hail as hl
 
-from v03_pipeline.lib.annotations.liftover import remove_liftover
 from v03_pipeline.lib.misc.io import write
 from v03_pipeline.lib.tasks.base.base_hail_table import BaseHailTableTask
 
@@ -13,8 +12,6 @@ class BaseWriteTask(BaseHailTableTask):
         # Set force to false after run, allowing "complete()" to succeeded
         # when dependencies are re-evaluated.
         self.force = False
-        # Ensure any cached liftover files within Hail are cleared
-        remove_liftover()
 
     def create_table(self) -> hl.Table:
         raise NotImplementedError
