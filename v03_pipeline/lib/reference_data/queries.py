b57f0f37af536556f3a86765bc88b536c82ea564
hanars <hsnow@broadinstitute.org>
2024-08-29 12:08:37 -0400

    add stricter af prefilter option

--- v03_pipeline/lib/reference_data/queries.py
+++ v03_pipeline/lib/reference_data/queries.py
@@ -19,6 +19,7 @@ TRANSCRIPT_CONSEQUENCE_TERM_RANK_LOOKUP = hl.dict(
     hl.enumerate(TRANSCRIPT_CONSEQUENCE_TERMS, index_first=False),
 )
 GNOMAD_CODING_NONCODING_HIGH_AF_THRESHOLD = 0.90
+ONE_TENTH_PERCENT = 0.001
 ONE_PERCENT = 0.01
 THREE_PERCENT = 0.03
 FIVE_PERCENT = 0.05
@@ -106,8 +107,9 @@ def high_af_variants(
     **_: Any,
 ) -> hl.Table:
     ht = ht.select_globals()
-    ht = ht.filter(ht.gnomad_genomes.AF_POPMAX_OR_GLOBAL > ONE_PERCENT)
+    ht = ht.filter(ht.gnomad_genomes.AF_POPMAX_OR_GLOBAL > ONE_TENTH_PERCENT)
     return ht.select(
+        is_gt_1_percent=ht.gnomad_genomes.AF_POPMAX_OR_GLOBAL > ONE_PERCENT,
         is_gt_3_percent=ht.gnomad_genomes.AF_POPMAX_OR_GLOBAL > THREE_PERCENT,
         is_gt_5_percent=ht.gnomad_genomes.AF_POPMAX_OR_GLOBAL > FIVE_PERCENT,
         is_gt_10_percent=ht.gnomad_genomes.AF_POPMAX_OR_GLOBAL > TEN_PERCENT,
