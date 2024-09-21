c2e73af6e075627f47481cab94cf451474c49198
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-09-21 10:07:20 -0400

    add a checkpoint

--- v03_pipeline/lib/tasks/update_lookup_table.py
+++ v03_pipeline/lib/tasks/update_lookup_table.py
@@ -2,7 +2,7 @@ import hail as hl
 import luigi
 import luigi.util
 
-from v03_pipeline.lib.misc.io import remap_pedigree_hash
+from v03_pipeline.lib.misc.io import checkpoint, remap_pedigree_hash
 from v03_pipeline.lib.misc.lookup import (
     compute_callset_lookup_ht,
     join_lookup_hts,
@@ -112,4 +112,5 @@ class UpdateLookupTableTask(BaseUpdateLookupTableTask):
                 ),
                 migrations=ht.migrations,
             )
+            ht, _ = checkpoint(ht)
         return ht
