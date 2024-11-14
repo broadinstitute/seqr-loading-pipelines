3ebb2b982cb4e6aa31a1468ee208b71da1c95314
Julia Klugherz <juliaklugherz@gmail.com>
2024-11-14 17:02:37 -0500

    merge

--- v03_pipeline/lib/reference_datasets/local_constraint_mito.py
+++ v03_pipeline/lib/reference_datasets/local_constraint_mito.py
@@ -3,13 +3,13 @@ import hail as hl
 from v03_pipeline.lib.model import ReferenceGenome
 
 
-def get_ht(raw_dataset_path: str, *_) -> hl.Table:
+def get_ht(raw_dataset_path: str, reference_genome: ReferenceGenome) -> hl.Table:
     ht = hl.import_table(
         raw_dataset_path,
         types={'Position': hl.tint32, 'MLC_score': hl.tfloat32},
     )
     ht = ht.select(
-        locus=hl.locus('chrM', ht.Position, ReferenceGenome.GRCh38.value),
+        locus=hl.locus('chrM', ht.Position, reference_genome.value),
         alleles=[ht.Reference, ht.Alternate],
         score=ht.MLC_score,
     )
