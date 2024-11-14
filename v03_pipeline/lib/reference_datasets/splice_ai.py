d7e3bcecdc17b6d4b814c61f8941c353631d20d2
Hana Snow <hsnow@broadinstitute.org>
2024-11-14 16:10:23 -0500

    ruff

--- v03_pipeline/lib/reference_datasets/splice_ai.py
+++ v03_pipeline/lib/reference_datasets/splice_ai.py
@@ -11,10 +11,9 @@ def get_ht(
 ) -> hl.Table:
     ht = vcf_to_ht(raw_dataset_paths, reference_genome)
 
-    # SpliceAI INFO field description from the VCF header:
-    # SpliceAIv1.3 variant annotation. These include delta scores (DS) and delta positions (DP) for acceptor gain (AG),
-    # acceptor loss (AL), donor gain (DG), and donor loss (DL).
-    # Format: ALLELE|SYMBOL|DS_AG|DS_AL|DS_DG|DS_DL|DP_AG|DP_AL|DP_DG|DP_DL
+    # SpliceAI INFO field description from the VCF header: SpliceAIv1.3 variant annotation. These include
+    # delta scores (DS) and delta positions (DP) for acceptor gain (AG), acceptor loss (AL), donor gain (DG), and
+    # donor loss (DL). Format: ALLELE|SYMBOL|DS_AG|DS_AL|DS_DG|DS_DL|DP_AG|DP_AL|DP_DG|DP_DL
     ds_start_index = 2
     ds_end_index = 6
     num_delta_scores = ds_end_index - ds_start_index
@@ -24,9 +23,12 @@ def get_ht(
         .map(hl.float32),
     )
     ht = ht.annotate(delta_score=hl.max(ht.delta_scores))
-    # Splice Consequence enum ID is the index of the max score. If no score, use the last index for "No Consequence"
     return ht.annotate(
         splice_consequence_id=hl.if_else(
-            ht.delta_score > 0, ht.delta_scores.index(ht.delta_score), num_delta_scores
+            ht.delta_score > 0,
+            # Splice Consequence enum ID is the index of the max score
+            ht.delta_scores.index(ht.delta_score),
+            # If no score, use the last index for "No Consequence"
+            num_delta_scores,
         ),
     ).drop('delta_scores')
