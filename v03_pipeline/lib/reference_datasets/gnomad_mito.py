e49e0dc597438c1e57268f5ca5a5a1654e17daf0
Julia Klugherz <juliaklugherz@gmail.com>
2024-11-14 14:58:29 -0500

    gnomad_mito selects

--- v03_pipeline/lib/reference_datasets/gnomad_mito.py
+++ v03_pipeline/lib/reference_datasets/gnomad_mito.py
@@ -1,18 +1,14 @@
 import hail as hl
 
 
-def select_fields(ht: hl.Table) -> hl.Table:
-    selects = {}
-    selects['AN'] = hl.int32(ht.AN)
-    selects['AC_hom'] = hl.int32(ht.AC_hom)
-    selects['AC_het'] = hl.int32(ht.AC_het)
-    selects['AF_hom'] = ht.AF_hom
-    selects['AF_het'] = ht.AF_het
-    selects['max_hl'] = ht.max_hl
-    return ht.select(**selects)
-
-
 def get_ht(raw_dataset_path: str, *_) -> hl.Table:
     ht = hl.read_table(raw_dataset_path)
-    ht = select_fields(ht)
+    ht = ht.select(
+        AN=hl.int32(ht.AN),
+        AC_hom=hl.int32(ht.AC_hom),
+        AC_het=hl.int32(ht.AC_het),
+        AF_hom=ht.AF_hom,
+        AF_het=ht.AF_het,
+        max_hl=ht.max_hl,
+    )
     return ht.select_globals()
