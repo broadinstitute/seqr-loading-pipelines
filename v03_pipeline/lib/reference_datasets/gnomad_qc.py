7392d43f1cc13feedbed4b8a74e03389a3c184b4
Julia Klugherz <juliaklugherz@gmail.com>
2024-11-14 12:05:12 -0500

    linter for now

--- v03_pipeline/lib/reference_datasets/gnomad_qc.py
+++ v03_pipeline/lib/reference_datasets/gnomad_qc.py
@@ -1,7 +1,7 @@
 import hail as hl
 
 
-def get_ht(raw_dataset_path: str, *args) -> hl.Table:
+def get_ht(raw_dataset_path: str, *_) -> hl.Table:
     if raw_dataset_path.endswith('mt'):
         return hl.read_matrix_table(raw_dataset_path).rows()
     return hl.read_table(raw_dataset_path)
