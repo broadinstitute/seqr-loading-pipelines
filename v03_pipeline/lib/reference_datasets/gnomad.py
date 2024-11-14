d6824e57fbc6a6cf09ed7f6191357c716f578fc7
Julia Klugherz <juliaklugherz@gmail.com>
2024-11-14 14:46:51 -0500

    maybe clearer gnomad

--- v03_pipeline/lib/reference_datasets/gnomad.py
+++ v03_pipeline/lib/reference_datasets/gnomad.py
@@ -3,54 +3,56 @@ import hail as hl
 from v03_pipeline.lib.model import ReferenceGenome
 
 
-def select_fields_v2(ht: hl.Table) -> hl.Table:
-    selects = {}
-    global_idx = hl.eval(ht.globals.freq_index_dict['gnomad'])
-    selects['AF'] = hl.float32(ht.freq[global_idx].AF)
-    selects['AN'] = ht.freq[global_idx].AN
-    selects['AC'] = ht.freq[global_idx].AC
-    selects['Hom'] = ht.freq[global_idx].homozygote_count
-
-    selects['AF_POPMAX_OR_GLOBAL'] = hl.float32(
-        hl.or_else(
-            ht.popmax[ht.globals.popmax_index_dict['gnomad']].AF,
-            ht.freq[global_idx].AF,
-        ),
-    )
-    selects['FAF_AF'] = hl.float32(ht.faf[ht.globals.popmax_index_dict['gnomad']].faf95)
-    selects['Hemi'] = hl.if_else(
-        ht.locus.in_autosome_or_par(),
-        0,
-        ht.freq[ht.globals.freq_index_dict['gnomad_male']].AC,
-    )
-    return ht.select(**selects)
+def global_idx_field(reference_genome: ReferenceGenome) -> str:
+    return 'gnomad' if reference_genome == ReferenceGenome.GRCh37 else 'adj'
 
 
-def select_fields_v4(ht: hl.Table) -> hl.Table:
-    selects = {}
-    global_idx = hl.eval(ht.globals.freq_index_dict['adj'])
-    selects['AF'] = hl.float32(ht.freq[global_idx].AF)
-    selects['AN'] = ht.freq[global_idx].AN
-    selects['AC'] = ht.freq[global_idx].AC
-    selects['Hom'] = ht.freq[global_idx].homozygote_count
+def af_popmax_expression(
+    ht: hl.Table,
+    reference_genome: ReferenceGenome,
+) -> hl.Expression:
+    if reference_genome == ReferenceGenome.GRCh37:
+        return ht.popmax[ht.globals.popmax_index_dict['gnomad']].AF
+    return ht.grpmax['gnomad'].AF if hasattr(ht.grpmax, 'gnomad') else ht.grpmax.AF
 
-    grpmax_af = ht.grpmax['gnomad'].AF if hasattr(ht.grpmax, 'gnomad') else ht.grpmax.AF
-    selects['AF_POPMAX_OR_GLOBAL'] = hl.float32(
-        hl.or_else(grpmax_af, ht.freq[global_idx].AF),
+
+def faf_globals_field(reference_genome: ReferenceGenome) -> str:
+    return (
+        'popmax_index_dict'
+        if reference_genome == ReferenceGenome.GRCh37
+        else 'faf_index_dict'
     )
-    selects['FAF_AF'] = hl.float32(ht.faf[ht.globals.faf_index_dict['adj']].faf95)
-    selects['Hemi'] = hl.if_else(
-        ht.locus.in_autosome_or_par(),
-        0,
-        ht.freq[ht.globals.freq_index_dict['XY_adj']].AC,
-    )
-    return ht.select(**selects)
+
+
+def hemi_field(reference_genome: ReferenceGenome) -> str:
+    return 'gnomad_male' if reference_genome == ReferenceGenome.GRCh37 else 'XY_adj'
 
 
 def get_ht(raw_dataset_path: str, reference_genome: ReferenceGenome) -> hl.Table:
     ht = hl.read_table(raw_dataset_path)
-    if reference_genome == ReferenceGenome.GRCh37:
-        ht = select_fields_v2(ht)
-    else:
-        ht = select_fields_v4(ht)
+    global_idx = hl.eval(ht.globals.freq_index_dict[global_idx_field(reference_genome)])
+    ht = ht.select(
+        AF=hl.float32(ht.freq[global_idx].AF),
+        AN=ht.freq[global_idx].AN,
+        AC=ht.freq[global_idx].AC,
+        Hom=ht.freq[global_idx].homozygote_count,
+        AF_POPMAX_OR_GLOBAL=hl.float32(
+            hl.or_else(
+                af_popmax_expression(ht, reference_genome),
+                ht.freq[global_idx].AF,
+            ),
+        ),
+        FAF_AF=hl.float32(
+            ht.faf[
+                ht.globals[faf_globals_field(reference_genome)][
+                    global_idx_field(reference_genome)
+                ]
+            ].faf95,
+        ),
+        Hemi=hl.if_else(
+            ht.locus.in_autosome_or_par(),
+            0,
+            ht.freq[ht.globals.freq_index_dict[hemi_field(reference_genome)]].AC,
+        ),
+    )
     return ht.select_globals()
