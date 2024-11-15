e0888d0a04ddbc8e94b66e0c53909f49ace43305
hanars <hsnow@broadinstitute.org>
2024-11-15 15:27:14 -0500

    remove comment

--- v03_pipeline/lib/reference_datasets/exac.py
+++ v03_pipeline/lib/reference_datasets/exac.py
@@ -14,7 +14,6 @@ RENAME = {
 }
 
 
-# adapted from download_and_create_reference_datasets/v02/create_ht__topmed.py
 def get_ht(raw_dataset_path: str, reference_genome: ReferenceGenome) -> hl.Table:
     ht = vcf_to_ht(raw_dataset_path, reference_genome)
     return ht.rename(**RENAME).select(*RENAME.values())
