65cf7238e181c47fe9a86eee8dd2d937b6b82c77
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-11-20 16:49:38 -0500

    Change validation table reference

--- v03_pipeline/lib/tasks/validate_callset_test.py
+++ v03_pipeline/lib/tasks/validate_callset_test.py
@@ -1,29 +1,27 @@
 import json
 import shutil
-from unittest.mock import Mock, patch
 
 import luigi.worker
 
 from v03_pipeline.lib.model import (
-    CachedReferenceDatasetQuery,
     DatasetType,
     ReferenceGenome,
     SampleType,
 )
 from v03_pipeline.lib.paths import (
-    cached_reference_dataset_query_path,
+    valid_reference_dataset_path,
 )
+from v03_pipeline.lib.reference_datasets.reference_dataset import ReferenceDataset
 from v03_pipeline.lib.tasks.validate_callset import (
     ValidateCallsetTask,
 )
 from v03_pipeline.lib.tasks.write_validation_errors_for_run import (
     WriteValidationErrorsForRunTask,
 )
-from v03_pipeline.lib.test.mock_complete_task import MockCompleteTask
 from v03_pipeline.lib.test.mocked_dataroot_testcase import MockedDatarootTestCase
 
-TEST_CODING_NONCODING_CRDQ_1 = (
-    'v03_pipeline/var/test/reference_data/test_gnomad_coding_noncoding_crdq_1.ht'
+TEST_CODING_AND_NONCODING_HT = (
+    'v03_pipeline/var/test/reference_data/gnomad_coding_and_noncoding_38.ht'
 )
 MULTIPLE_VALIDATION_EXCEPTIONS_VCF = (
     'v03_pipeline/var/test/callsets/multiple_validation_exceptions.vcf'
@@ -36,22 +34,16 @@ class ValidateCallsetTest(MockedDatarootTestCase):
     def setUp(self) -> None:
         super().setUp()
         shutil.copytree(
-            TEST_CODING_NONCODING_CRDQ_1,
-            cached_reference_dataset_query_path(
+            TEST_CODING_AND_NONCODING_HT,
+            valid_reference_dataset_path(
                 ReferenceGenome.GRCh38,
-                DatasetType.SNV_INDEL,
-                CachedReferenceDatasetQuery.GNOMAD_CODING_AND_NONCODING_VARIANTS,
+                ReferenceDataset.gnomad_coding_and_noncoding,
             ),
         )
 
-    @patch(
-        'v03_pipeline.lib.tasks.validate_callset.UpdatedCachedReferenceDatasetQuery',
-    )
     def test_validate_callset_multiple_exceptions(
         self,
-        mock_updated_cached_reference_dataset_query: Mock,
     ) -> None:
-        mock_updated_cached_reference_dataset_query.return_value = MockCompleteTask()
         worker = luigi.worker.Worker()
         validate_callset_task = ValidateCallsetTask(
             reference_genome=ReferenceGenome.GRCh38,
