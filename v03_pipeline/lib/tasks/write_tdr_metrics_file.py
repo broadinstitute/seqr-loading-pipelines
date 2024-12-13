adb00136509d1afdb142e12f244ba7f976b320f9
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-12-13 13:31:25 -0500

    Catch exception

--- v03_pipeline/lib/tasks/write_tdr_metrics_file.py
+++ v03_pipeline/lib/tasks/write_tdr_metrics_file.py
@@ -1,8 +1,10 @@
 import csv
 
+import google.api_core.exceptions
 import luigi
 import luigi.util
 
+from v03_pipeline.lib.logger import get_logger
 from v03_pipeline.lib.misc.terra_data_repository import (
     BIGQUERY_METRICS,
     bq_metrics_query,
@@ -12,6 +14,8 @@ from v03_pipeline.lib.tasks.base.base_loading_pipeline_params import (
     BaseLoadingPipelineParams,
 )
 from v03_pipeline.lib.tasks.files import GCSorLocalTarget
+
+logger = get_logger(__name__)
 
 
 @luigi.util.inherits(BaseLoadingPipelineParams)
@@ -31,5 +35,8 @@ class WriteTDRMetricsFileTask(luigi.Task):
         with self.output().open('w') as f:
             writer = csv.DictWriter(f, fieldnames=BIGQUERY_METRICS, delimiter='\t')
             writer.writeheader()
-            for row in bq_metrics_query(self.bq_table_name):
-                writer.writerow(row)
+            try:
+                for row in bq_metrics_query(self.bq_table_name):
+                    writer.writerow(row)
+            except google.api_core.exceptions.BadRequest:
+                logger.exception('Query Failed')
