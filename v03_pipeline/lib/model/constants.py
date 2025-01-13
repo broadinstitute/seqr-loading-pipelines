e858142b082127d8e4794c10a2414f924acb21b1
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-01-13 14:17:21 -0500

    ruff

--- v03_pipeline/lib/model/constants.py
+++ v03_pipeline/lib/model/constants.py
@@ -13,8 +13,10 @@ PROJECTS_EXCLUDED_FROM_LOOKUP = {
     'R0816_gregor_training_project_',
 }
 GRCH37_TO_GRCH38_LIFTOVER_REF_PATH = importlib.resources.path(
-    'v03_pipeline.var.liftover', 'grch37_to_grch38.over.chain.gz'
+    'v03_pipeline.var.liftover',
+    'grch37_to_grch38.over.chain.gz',
 )
 GRCH38_TO_GRCH37_LIFTOVER_REF_PATH = importlib.resources.path(
-    'v03_pipeline.var.liftover', 'grch38_to_grch37.over.chain.gz'
+    'v03_pipeline.var.liftover',
+    'grch38_to_grch37.over.chain.gz',
 )
