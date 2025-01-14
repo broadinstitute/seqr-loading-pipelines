4f77eaaad1a567d1183532929c2aa6af1034eace
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-01-14 09:46:28 -0500

    default to hail

--- v03_pipeline/lib/model/constants.py
+++ v03_pipeline/lib/model/constants.py
@@ -12,11 +12,11 @@ PROJECTS_EXCLUDED_FROM_LOOKUP = {
     'R0815_gregor_training_project_',
     'R0816_gregor_training_project_',
 }
-GRCH37_TO_GRCH38_LIFTOVER_REF_PATH = importlib.resources.path(
-    'v03_pipeline.var.liftover',
-    'grch37_to_grch38.over.chain.gz',
-).as_posix()
-GRCH38_TO_GRCH37_LIFTOVER_REF_PATH = importlib.resources.path(
-    'v03_pipeline.var.liftover',
-    'grch38_to_grch37.over.chain.gz',
-).as_posix()
+GRCH37_TO_GRCH38_LIFTOVER_REF_PATH = (
+  'gs://hail-common/references/grch37_to_grch38.over.chain.gz'
+  if FeatureFlag.RUN_PIPELINE_ON_DATAPROC else  'v03_pipeline/var/liftover/grch37_to_grch38.over.chain.gz'
+)
+GRCH38_TO_GRCH37_LIFTOVER_REF_PATH = (
+  'gs://hail-common/references/grch38_to_grch37.over.chain.gz'
+  if FeatureFlag.RUN_PIPELINE_ON_DATAPROC else  'v03_pipeline/var/liftover/grch38_to_grch37.over.chain.gz'
+)
