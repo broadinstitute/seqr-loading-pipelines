d48a23a1e6d543c803234d5c33d069f075bbd963
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-02-12 15:58:31 -0500

    First pass

--- v03_pipeline/lib/tasks/delete_project_family_tables.py
+++ v03_pipeline/lib/tasks/delete_project_family_tables.py
@@ -1,8 +1,12 @@
+import os
+
 import hail as hl
+import hailtop.fs as hfs
 import luigi
 import luigi.util
 
-from v03_pipeline.lib.misc.project_tables import get_valid_project_tables
+from v03_pipeline.lib.model import SampleType
+from v03_pipeline.lib.paths import project_table_path
 from v03_pipeline.lib.tasks.base.base_loading_pipeline_params import (
     BaseLoadingPipelineParams,
 )
@@ -11,6 +15,7 @@ from v03_pipeline.lib.tasks.delete_family_table import DeleteFamilyTableTask
 
 @luigi.util.inherits(BaseLoadingPipelineParams)
 class DeleteProjectFamilyTablesTask(luigi.Task):
+    sample_type = luigi.EnumParameter(enum=SampleType)
     project_guid = luigi.Parameter()
 
     def __init__(self, *args, **kwargs):
@@ -18,26 +23,41 @@ class DeleteProjectFamilyTablesTask(luigi.Task):
         self.dynamic_delete_family_table_tasks = set()
 
     def complete(self) -> bool:
+        project_ht_path = project_table_path(
+            self.reference_genome,
+            self.dataset_type,
+            self.sample_type,
+            self.project_guid,
+        )
+        if not (
+            hfs.exists(project_ht_path)
+            and hfs.exists(
+                os.path.join(project_ht_path, '_SUCCESS'),
+            )
+        ):
+            return True
         return len(self.dynamic_delete_family_table_tasks) >= 1 and all(
             delete_family_table_task.complete()
             for delete_family_table_task in self.dynamic_delete_family_table_tasks
         )
 
     def run(self):
-        project_tables = get_valid_project_tables(
-            self.reference_genome,
-            self.dataset_type,
-            self.project_guid,
+        project_ht = hl.read_table(
+            project_table_path(
+                self.reference_genome,
+                self.dataset_type,
+                self.sample_type,
+                self.project_guid,
+            ),
         )
-        for project_ht, sample_type in project_tables:
-            family_guids = hl.eval(project_ht.globals.family_guids)
-            for family_guid in family_guids:
-                self.dynamic_delete_family_table_tasks.add(
-                    DeleteFamilyTableTask(
-                        reference_genome=self.reference_genome,
-                        dataset_type=self.dataset_type,
-                        sample_type=sample_type,
-                        family_guid=family_guid,
-                    ),
-                )
+        family_guids = hl.eval(project_ht.globals.family_guids)
+        for family_guid in family_guids:
+            self.dynamic_delete_family_table_tasks.add(
+                DeleteFamilyTableTask(
+                    reference_genome=self.reference_genome,
+                    dataset_type=self.dataset_type,
+                    sample_type=self.sample_type,
+                    family_guid=family_guid,
+                ),
+            )
         yield self.dynamic_delete_family_table_tasks
