bbf4ad61b5627961a249e741d5b3f50c30b48462
Julia Klugherz <juliaklugherz@gmail.com>
2025-02-14 17:01:00 -0500

    mock something else

--- v03_pipeline/lib/tasks/write_sample_qc_json_test.py
+++ v03_pipeline/lib/tasks/write_sample_qc_json_test.py
@@ -8,85 +8,27 @@ import luigi.worker
 
 from v03_pipeline.lib.model import DatasetType, ReferenceGenome, SampleType
 from v03_pipeline.lib.tasks.write_sample_qc_json import WriteSampleQCJsonTask
+from v03_pipeline.lib.test.mock_complete_task import MockCompleteTask
 from v03_pipeline.lib.test.mocked_dataroot_testcase import MockedDatarootTestCase
 
 TEST_VCF = 'v03_pipeline/var/test/callsets/1kg_30variants.vcf'
+TEST_TDR_METRICS_FILE = 'v03_pipeline/var/test/tdr_metrics.tsv'
 TEST_RUN_ID = 'manual__2024-04-03'
 
 
 class WriteSampleQCJsonTaskTest(MockedDatarootTestCase):
-    @patch('v03_pipeline.lib.tasks.write_tdr_metrics_files.gen_bq_table_names')
-    @patch('v03_pipeline.lib.tasks.write_tdr_metrics_file.bq_metrics_query')
+    @patch('v03_pipeline.lib.tasks.write_sample_qc_json.WriteTDRMetricsFilesTask')
+    @patch('v03_pipeline.lib.tasks.write_sample_qc_json.hfs.ls')
     def test_call_sample_qc(
         self,
-        mock_bq_metrics_query: Mock,
-        mock_gen_bq_table_names: Mock,
+        mock_ls_tdr_dir: Mock,
+        mock_tdr_task: Mock,
     ) -> None:
-        mock_gen_bq_table_names.return_value = ['datarepo-7242affb.datarepo_RP_0113']
-        mock_bq_metrics_query.side_effect = [
-            iter(
-                [
-                    google.cloud.bigquery.table.Row(
-                        (
-                            'HG00731',
-                            'Unknown',
-                            Decimal('5.1'),
-                            Decimal('93.69'),
-                            Decimal('29.31'),
-                        ),
-                        {
-                            'collaborator_sample_id': 0,
-                            'predicted_sex': 1,
-                            'contamination_rate': 2,
-                            'percent_bases_at_20x': 3,
-                            'mean_coverage': 4,
-                        },
-                    ),
-                    google.cloud.bigquery.table.Row(
-                        (
-                            'HG00732',
-                            'Female',
-                            Decimal('5'),
-                            Decimal('90'),
-                            Decimal('28'),
-                        ),
-                        {
-                            'collaborator_sample_id': 0,
-                            'predicted_sex': 1,
-                            'contamination_rate': 2,
-                            'percent_bases_at_20x': 3,
-                            'mean_coverage': 4,
-                        },
-                    ),
-                    google.cloud.bigquery.table.Row(
-                        (
-                            'HG00733',
-                            'Male',
-                            Decimal('6'),
-                            Decimal('85'),
-                            Decimal('36.4'),
-                        ),
-                        {
-                            'collaborator_sample_id': 0,
-                            'predicted_sex': 1,
-                            'contamination_rate': 2,
-                            'percent_bases_at_20x': 3,
-                            'mean_coverage': 4,
-                        },
-                    ),
-                    google.cloud.bigquery.table.Row(
-                        ('NA19675', 'Male', Decimal('0'), Decimal('80'), Decimal('30')),
-                        {
-                            'collaborator_sample_id': 0,
-                            'predicted_sex': 1,
-                            'contamination_rate': 2,
-                            'percent_bases_at_20x': 3,
-                            'mean_coverage': 4,
-                        },
-                    ),
-                ],
-            ),
-        ]
+        mock_tdr_task.return_value = MockCompleteTask()
+        mock_tdr_table = Mock()
+        mock_tdr_table.path = TEST_TDR_METRICS_FILE
+        mock_ls_tdr_dir.return_value = [mock_tdr_table]
+
         worker = luigi.worker.Worker()
         task = WriteSampleQCJsonTask(
             reference_genome=ReferenceGenome.GRCh38,
