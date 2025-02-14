c34b0a66a3a8b0725f5de84a27f3673eb462de3a
Julia Klugherz <juliaklugherz@gmail.com>
2025-02-14 17:14:59 -0500

    add hail sample_qc metrics

--- v03_pipeline/lib/methods/sample_qc.py
+++ v03_pipeline/lib/methods/sample_qc.py
@@ -2,7 +2,9 @@ import pickle
 
 import hail as hl
 from gnomad.sample_qc.ancestry import assign_population_pcs, pc_project
+from gnomad.sample_qc.filtering import compute_stratified_metrics_filter
 from gnomad.sample_qc.pipeline import filter_rows_for_qc
+from gnomad.utils.filtering import filter_to_autosomes
 
 from v03_pipeline.lib.model import SampleType
 
@@ -16,6 +18,15 @@ POP_PCA_LOADINGS_PATH = (
 )
 ANCESTRY_RF_MODEL_PATH = 'v03_pipeline/var/ancestry_imputation_model.pickle'
 NUM_PCS = 20
+
+HAIL_QC_METRICS = [
+    'n_snp',
+    'r_ti_tv',
+    'r_insertion_deletion',
+    'n_insertion',
+    'n_deletion',
+    'r_het_hom_var',
+]
 
 
 def call_sample_qc(
@@ -32,7 +43,8 @@ def call_sample_qc(
     )
     mt = annotate_filtered_callrate(mt)
     mt = annotate_filter_flags(mt, tdr_metrics_ht, sample_type)
-    return annotate_qc_pop(mt)
+    mt = annotate_qc_pop(mt)
+    return run_hail_sample_qc(mt, sample_type)
 
 
 def annotate_filtered_callrate(mt: hl.MatrixTable) -> hl.MatrixTable:
@@ -103,3 +115,28 @@ def _get_pop_pca_scores(mt: hl.MatrixTable) -> hl.Table:
     loadings = hl.read_table(POP_PCA_LOADINGS_PATH)
     scores = pc_project(mt, loadings)
     return scores.annotate(scores=scores.scores[:NUM_PCS], known_pop='Unknown')
+
+
+def run_hail_sample_qc(mt: hl.MatrixTable, sample_type: SampleType) -> hl.MatrixTable:
+    mt = filter_to_autosomes(mt)
+    mt = hl.split_multi_hts(mt)
+    mt = hl.sample_qc(mt)
+    mt = mt.annotate_cols(
+        sample_qc=mt.sample_qc.annotate(
+            f_inbreeding=hl.agg.inbreeding(mt.GT, mt['info.AF'][0]),
+        ),
+    )
+    sample_qc_metrics = HAIL_QC_METRICS
+    if sample_type == SampleType.WGS:
+        sample_qc_metrics = [*sample_qc_metrics, 'call_rate']
+
+    strat_ht = mt.cols()
+    qc_metrics = {metric: strat_ht.sample_qc[metric] for metric in sample_qc_metrics}
+    strata = {'qc_pop': strat_ht.qc_pop}
+
+    metric_ht = compute_stratified_metrics_filter(strat_ht, qc_metrics, strata)
+    metric_ht = metric_ht.annotate(
+        sample_qc=mt.cols()[metric_ht.key].sample_qc,
+        qc_metrics_filters=hl.array(metric_ht.qc_metrics_filters),
+    )
+    return mt.annotate_cols(**metric_ht[mt.col_key])
