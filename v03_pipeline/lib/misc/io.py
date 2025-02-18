061eee4473a6bef17de416eecfe1a6ac92d1315a
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-02-18 13:47:42 -0500

    account for case when field is entirely missing

--- v03_pipeline/lib/misc/io.py
+++ v03_pipeline/lib/misc/io.py
@@ -185,7 +185,11 @@ def import_callset(
             # Note, SEQR_INTERNAL_TRUTH_VID is an entirely optional
             # and unvalidated field.  It will not be properly imported.
             # It simply overrides the existing variant_id if present.
-            variant_id=hl.or_else(mt.info.SEQR_INTERNAL_TRUTH_VID, mt.rsid),
+            variant_id=(
+                hl.or_else(mt.info.SEQR_INTERNAL_TRUTH_VID, mt.rsid)
+                if hasattr(mt.info, 'SEQR_INTERNAL_TRUTH_VID')
+                else mt.rsid
+            ),
         )
     return mt.key_rows_by(*dataset_type.table_key_type(reference_genome).fields)
 
