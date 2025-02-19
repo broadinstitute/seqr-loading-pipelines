f164733a3c2d2f8540a3163512a38da3530f309c
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-02-19 15:51:31 -0500

    gnomad v4 sv migration

---
+++ v03_pipeline/migrations/annotations/0004_add_gnomad_svs.py
@@ -0,0 +1,37 @@
+import hail as hl
+
+from v03_pipeline.lib.annotations import sv
+from v03_pipeline.lib.migration.base_migration import BaseMigration
+from v03_pipeline.lib.model import DatasetType, ReferenceGenome
+from v03_pipeline.lib.reference_datasets.reference_dataset import ReferenceDataset
+
+# This vcf was generated with the gatk command
+PHASE_4_CALLSET_WITH_GNOMAD_V4 = 'gs://seqr-loading-temp/phase4.seqr.gnomad_v4.vcf.gz'
+
+
+class AddGnomadSVs(BaseMigration):
+    reference_genome_dataset_types: frozenset[
+        tuple[ReferenceGenome, DatasetType]
+    ] = frozenset(
+        ((ReferenceGenome.GRCh38, DatasetType.SV),),
+    )
+
+    @staticmethod
+    def migrate(ht: hl.Table, **_) -> hl.Table:
+        mapping_ht = hl.import_vcf(
+            PHASE_4_CALLSET_WITH_GNOMAD_V4, ReferenceGenome.GRCh38.value,
+        ).rows()
+        ht = ht.annotate(
+            **{
+                'info.GNOMAD_V4.1_TRUTH_VID': mapping_ht[ht.key][
+                    'info.GNOMAD_V4.1_TRUTH_VID'
+                ],
+            },
+        )
+        gnomad_svs_ht = ReferenceDataset.gnomad_svs.get_ht(ReferenceGenome.GRCh38)
+        ht = ht.annotate(gnomad_svs=sv.gnomad_svs(ht, gnomad_svs_ht))
+        ht = ht.drop('info.GNOMAD_V4.1_TRUTH_VID')
+        return ht.annotate_globals(
+            versions=ht.globals.versions.annotate(gnomad_svs='1.0'),
+            enums=ht.globals.enums.annotate(gnomad_svs=hl.Struct()),
+        )
