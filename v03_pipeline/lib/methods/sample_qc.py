ec44d61e16d3aa638e84ad5b3936aa46725c1f3b
Julia Klugherz <juliaklugherz@gmail.com>
2025-03-17 14:11:31 -0400

    typo

--- v03_pipeline/lib/methods/sample_qc.py
+++ v03_pipeline/lib/methods/sample_qc.py
@@ -52,7 +52,7 @@ def call_sample_qc(
     )
     mt = annotate_filtered_callrate(mt)
     mt = annotate_filter_flags(mt, tdr_metrics_ht, sample_type)
-    return annotate_qc_gen_anc(mt)
+    return annotate_gq_gen_anc(mt)
 
 
 def annotate_filtered_callrate(mt: hl.MatrixTable) -> hl.MatrixTable:
@@ -94,7 +94,7 @@ def annotate_filter_flags(
     )
 
 
-def annotate_qc_gen_anc(mt: hl.MatrixTable) -> hl.MatrixTable:
+def annotate_gq_gen_anc(mt: hl.MatrixTable) -> hl.MatrixTable:
     mt = mt.select_entries('GT')
     scores = _get_pop_pca_scores(mt)
     with hl.hadoop_open(ANCESTRY_RF_MODEL_PATH, 'rb') as f:
