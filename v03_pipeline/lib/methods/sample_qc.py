7e9eab66e7a532422387ef4d82f7e2bf704d68b8
Benjamin Blankenmeister <b.p.blankenmeister@gmail.com>
2025-04-03 16:19:29 -0400

    bugfix: missing sample qc code on main (#1070)
    * add ancestry imputation - qc_pop
    
    * reduce duplication
    
    * add hail sample_qc metrics
    
    * merge
    
    * use gs
    
    * mock mock whos there
    
    * delete model file
    
    * try adding gnomad qc import
    
    * upgrade gnomad methods package
    
    * use onnx model
    
    * test onnx
    
    * rename
    
    * rename 2
    
    * apply model func
    
    * use validated func
    
    * comments
    
    * typo
    
    * typo 2
    
    * typo 2
    
    ---------
    
    Co-authored-by: Julia Klugherz <juliaklugherz@gmail.com>

--- v03_pipeline/lib/methods/sample_qc.py
+++ v03_pipeline/lib/methods/sample_qc.py
@@ -1,6 +1,16 @@
 import hail as hl
+import onnx
+from gnomad.sample_qc.ancestry import (
+    apply_onnx_classification_model,
+    assign_population_pcs,
+    pc_project,
+)
+from gnomad.sample_qc.filtering import compute_stratified_metrics_filter
 from gnomad.sample_qc.pipeline import filter_rows_for_qc
+from gnomad.utils.filtering import filter_to_autosomes
+from gnomad_qc.v4.sample_qc.assign_ancestry import assign_pop_with_per_pop_probs
 
+from v03_pipeline.lib.misc.io import split_multi_hts
 from v03_pipeline.lib.model import SampleType
 
 GNOMAD_FILTER_MIN_AF = 0.001
@@ -10,6 +20,35 @@ CALLRATE_LOW_THRESHOLD = 0.85
 CONTAMINATION_UPPER_THRESHOLD = 5
 WES_COVERAGE_LOW_THRESHOLD = 85
 WGS_CALLRATE_LOW_THRESHOLD = 30
+
+POP_PCA_LOADINGS_PATH = (
+    'gs://gcp-public-data--gnomad/release/4.0/pca/gnomad.v4.0.pca_loadings.ht'
+)
+ANCESTRY_RF_MODEL_PATH = (
+    'gs://seqr-reference-data/v3.1/GRCh38/SNV_INDEL/ancestry_imputation_model.onnx'
+)
+NUM_PCS = 20
+GNOMAD_POP_PROBABILITY_CUTOFFS = {
+    'afr': 0.93,
+    'ami': 0.98,
+    'amr': 0.89,
+    'asj': 0.94,
+    'eas': 0.95,
+    'fin': 0.92,
+    'mid': 0.55,
+    'nfe': 0.75,
+    'sas': 0.92,
+}
+POPULATION_MISSING_LABEL = 'oth'
+
+HAIL_QC_METRICS = [
+    'n_snp',
+    'r_ti_tv',
+    'r_insertion_deletion',
+    'n_insertion',
+    'n_deletion',
+    'r_het_hom_var',
+]
 
 
 def call_sample_qc(
@@ -24,7 +63,9 @@ def call_sample_qc(
         .default(hl.missing(hl.tcall)),
     )
     mt = annotate_filtered_callrate(mt)
-    return annotate_filter_flags(mt, tdr_metrics_ht, sample_type)
+    mt = annotate_filter_flags(mt, tdr_metrics_ht, sample_type)
+    mt = annotate_qc_gen_anc(mt)
+    return run_hail_sample_qc(mt, sample_type)
 
 
 def annotate_filtered_callrate(mt: hl.MatrixTable) -> hl.MatrixTable:
@@ -64,3 +105,61 @@ def annotate_filter_flags(
             [hl.or_missing(filter_cond, name) for name, filter_cond in flags.items()],
         ).filter(hl.is_defined),
     )
+
+
+def annotate_qc_gen_anc(mt: hl.MatrixTable) -> hl.MatrixTable:
+    mt = mt.select_entries('GT')
+    scores = _get_pop_pca_scores(mt)
+    with hl.hadoop_open(ANCESTRY_RF_MODEL_PATH, 'rb') as f:
+        fit = onnx.load(f)
+
+    pop_pca_ht, _ = assign_population_pcs(
+        scores,
+        pc_cols=scores.scores,
+        output_col='qc_pop',
+        fit=fit,
+        apply_model_func=apply_onnx_classification_model,
+    )
+    pop_pca_ht = pop_pca_ht.key_by('s')
+    scores = scores.annotate(
+        **{f'pop_PC{i + 1}': scores.scores[i] for i in range(NUM_PCS)},
+    ).drop('scores', 'known_pop')
+    pop_pca_ht = pop_pca_ht.annotate(**scores[pop_pca_ht.key])
+    pop_pca_ht = assign_pop_with_per_pop_probs(
+        pop_pca_ht,
+        min_prob_cutoffs=GNOMAD_POP_PROBABILITY_CUTOFFS,
+        missing_label=POPULATION_MISSING_LABEL,
+    )
+    pop_pca_ht = pop_pca_ht.transmute(qc_gen_anc=pop_pca_ht.pop).drop('qc_pop')
+    return mt.annotate_cols(**pop_pca_ht[mt.col_key])
+
+
+def _get_pop_pca_scores(mt: hl.MatrixTable) -> hl.Table:
+    loadings = hl.read_table(POP_PCA_LOADINGS_PATH)
+    scores = pc_project(mt, loadings)
+    return scores.annotate(scores=scores.scores[:NUM_PCS], known_pop='Unknown')
+
+
+def run_hail_sample_qc(mt: hl.MatrixTable, sample_type: SampleType) -> hl.MatrixTable:
+    mt = filter_to_autosomes(mt)
+    mt = split_multi_hts(mt, True)
+    mt = hl.sample_qc(mt)
+    mt = mt.annotate_cols(
+        sample_qc=mt.sample_qc.annotate(
+            f_inbreeding=hl.agg.inbreeding(mt.GT, mt['info.AF'][0]),
+        ),
+    )
+    sample_qc_metrics = HAIL_QC_METRICS
+    if sample_type == SampleType.WGS:
+        sample_qc_metrics = [*sample_qc_metrics, 'call_rate']
+
+    strat_ht = mt.cols()
+    qc_metrics = {metric: strat_ht.sample_qc[metric] for metric in sample_qc_metrics}
+    strata = {'qc_gen_anc': strat_ht.qc_gen_anc}
+
+    metric_ht = compute_stratified_metrics_filter(strat_ht, qc_metrics, strata)
+    metric_ht = metric_ht.annotate(
+        sample_qc=mt.cols()[metric_ht.key].sample_qc,
+        qc_metrics_filters=hl.array(metric_ht.qc_metrics_filters),
+    )
+    return mt.annotate_cols(**metric_ht[mt.col_key])
--- v03_pipeline/lib/tasks/write_sample_qc_json_test.py
+++ v03_pipeline/lib/tasks/write_sample_qc_json_test.py
@@ -1,6 +1,7 @@
 import json
 from unittest.mock import Mock, patch
 
+import hail as hl
 import hailtop.fs as hfs
 import luigi.worker
 
@@ -13,19 +14,88 @@ TEST_VCF = 'v03_pipeline/var/test/callsets/1kg_30variants.vcf'
 TEST_TDR_METRICS_FILE = 'v03_pipeline/var/test/tdr_metrics.tsv'
 TEST_RUN_ID = 'manual__2024-04-03'
 
+PCA_SCORES = [0.00212, 0.011, 0.0105, 0.161, 0.026] + [0.1 for _ in range(15)]
+EXPECTED_ANC_PROBABILITIES = {
+    'prob_afr': 0.02,
+    'prob_ami': 0.0,
+    'prob_amr': 0.02,
+    'prob_asj': 0.9,
+    'prob_eas': 0.0,
+    'prob_fin': 0.0,
+    'prob_mid': 0.0,
+    'prob_nfe': 0.05,
+    'prob_sas': 0.01,
+}
+TEST_ANCESTRY_IMPUTATION_MODEL_PATH = (
+    'v03_pipeline/var/test/ancestry_imputation_model.onnx'
+)
+
 
 class WriteSampleQCJsonTaskTest(MockedDatarootTestCase):
+    @patch(
+        'v03_pipeline.lib.methods.sample_qc.ANCESTRY_RF_MODEL_PATH',
+        TEST_ANCESTRY_IMPUTATION_MODEL_PATH,
+    )
+    @patch('v03_pipeline.lib.methods.sample_qc.assign_population_pcs')
+    @patch('v03_pipeline.lib.methods.sample_qc._get_pop_pca_scores')
     @patch('v03_pipeline.lib.tasks.write_sample_qc_json.WriteTDRMetricsFilesTask')
     @patch('v03_pipeline.lib.tasks.write_sample_qc_json.hfs.ls')
     def test_call_sample_qc(
         self,
         mock_ls_tdr_dir: Mock,
         mock_tdr_task: Mock,
+        mock_get_pop_pca_scores: Mock,
+        mock_assign_population_pcs: Mock,
     ) -> None:
         mock_tdr_task.return_value = MockCompleteTask()
         mock_tdr_table = Mock()
         mock_tdr_table.path = TEST_TDR_METRICS_FILE
         mock_ls_tdr_dir.return_value = [mock_tdr_table]
+        mock_get_pop_pca_scores.return_value = hl.Table.parallelize(
+            [
+                {
+                    's': sample_id,
+                    'scores': PCA_SCORES,
+                    'known_pop': 'Unknown',
+                }
+                for sample_id in ('HG00731', 'HG00732', 'HG00733', 'NA19675')
+            ],
+            hl.tstruct(
+                s=hl.tstr,
+                scores=hl.tarray(hl.tfloat64),
+                known_pop=hl.tstr,
+            ),
+            key='s',
+        )
+        mock_assign_population_pcs.return_value = (
+            hl.Table.parallelize(
+                [
+                    {
+                        's': sample_id,
+                        'pca_scores': PCA_SCORES,
+                        'qc_pop': 'asj',
+                        **EXPECTED_ANC_PROBABILITIES,
+                    }
+                    for sample_id in ('HG00731', 'HG00732', 'HG00733', 'NA19675')
+                ],
+                hl.tstruct(
+                    s=hl.tstr,
+                    pca_scores=hl.tarray(hl.tfloat64),
+                    qc_pop=hl.tstr,
+                    prob_afr=hl.tfloat64,
+                    prob_ami=hl.tfloat64,
+                    prob_amr=hl.tfloat64,
+                    prob_asj=hl.tfloat64,
+                    prob_eas=hl.tfloat64,
+                    prob_fin=hl.tfloat64,
+                    prob_mid=hl.tfloat64,
+                    prob_nfe=hl.tfloat64,
+                    prob_sas=hl.tfloat64,
+                ),
+                key='s',
+            ),
+            None,
+        )
 
         worker = luigi.worker.Worker()
         task = WriteSampleQCJsonTask(
@@ -42,6 +112,12 @@ class WriteSampleQCJsonTaskTest(MockedDatarootTestCase):
         self.assertTrue(task)
         self.assertTrue(hfs.exists(task.output().path))
 
+        expected_qc_gen_anc_results = {
+            'pca_scores': PCA_SCORES,
+            **EXPECTED_ANC_PROBABILITIES,
+            **{f'pop_PC{i + 1}': PCA_SCORES[i] for i in range(20)},
+            'qc_gen_anc': 'oth',
+        }
         with task.output().open('r') as f:
             res = json.load(f)
 
@@ -53,6 +129,37 @@ class WriteSampleQCJsonTaskTest(MockedDatarootTestCase):
                 'percent_bases_at_20x': 93.69000244140625,
                 'mean_coverage': 29.309999465942383,
                 'filter_flags': ['contamination', 'coverage'],
+                **expected_qc_gen_anc_results,
+                'sample_qc.call_rate': 0.9666666666666667,
+                'sample_qc.n_called': 29,
+                'sample_qc.n_not_called': 1,
+                'sample_qc.n_filtered': 0,
+                'sample_qc.n_hom_ref': 16,
+                'sample_qc.n_het': 2,
+                'sample_qc.n_hom_var': 11,
+                'sample_qc.n_non_ref': 13,
+                'sample_qc.n_singleton': 1,
+                'sample_qc.n_snp': 24,
+                'sample_qc.n_insertion': 0,
+                'sample_qc.n_deletion': 0,
+                'sample_qc.n_transition': 15,
+                'sample_qc.n_transversion': 9,
+                'sample_qc.n_star': 0,
+                'sample_qc.r_ti_tv': 1.6666666666666667,
+                'sample_qc.r_het_hom_var': 0.18181818181818182,
+                'sample_qc.r_insertion_deletion': None,
+                'sample_qc.f_inbreeding.f_stat': 0.284195245767986,
+                'sample_qc.f_inbreeding.n_called': 29,
+                'sample_qc.f_inbreeding.expected_homs': 26.20594199999999,
+                'sample_qc.f_inbreeding.observed_homs': 27,
+                'fail_n_snp': True,
+                'fail_r_ti_tv': False,
+                'fail_r_insertion_deletion': None,
+                'fail_n_insertion': True,
+                'fail_n_deletion': True,
+                'fail_r_het_hom_var': False,
+                'fail_call_rate': False,
+                'qc_metrics_filters': ['n_deletion', 'n_insertion', 'n_snp'],
             },
         )
         self.assertCountEqual(
@@ -63,6 +170,37 @@ class WriteSampleQCJsonTaskTest(MockedDatarootTestCase):
                 'percent_bases_at_20x': 90.0,
                 'mean_coverage': 28.0,
                 'filter_flags': ['coverage'],
+                **expected_qc_gen_anc_results,
+                'sample_qc.call_rate': 0.9666666666666667,
+                'sample_qc.n_called': 29,
+                'sample_qc.n_not_called': 1,
+                'sample_qc.n_filtered': 0,
+                'sample_qc.n_hom_ref': 16,
+                'sample_qc.n_het': 4,
+                'sample_qc.n_hom_var': 9,
+                'sample_qc.n_non_ref': 13,
+                'sample_qc.n_singleton': 2,
+                'sample_qc.n_snp': 22,
+                'sample_qc.n_insertion': 0,
+                'sample_qc.n_deletion': 0,
+                'sample_qc.n_transition': 13,
+                'sample_qc.n_transversion': 9,
+                'sample_qc.n_star': 0,
+                'sample_qc.r_ti_tv': 1.4444444444444444,
+                'sample_qc.r_het_hom_var': 0.4444444444444444,
+                'sample_qc.r_insertion_deletion': None,
+                'sample_qc.f_inbreeding.f_stat': -0.431609508464028,
+                'sample_qc.f_inbreeding.n_called': 29,
+                'sample_qc.f_inbreeding.expected_homs': 26.20594199999999,
+                'sample_qc.f_inbreeding.observed_homs': 25,
+                'fail_n_snp': True,
+                'fail_r_ti_tv': False,
+                'fail_r_insertion_deletion': None,
+                'fail_n_insertion': True,
+                'fail_n_deletion': True,
+                'fail_r_het_hom_var': False,
+                'fail_call_rate': False,
+                'qc_metrics_filters': ['n_deletion', 'n_insertion', 'n_snp'],
             },
         )
         self.assertCountEqual(
@@ -73,6 +211,37 @@ class WriteSampleQCJsonTaskTest(MockedDatarootTestCase):
                 'percent_bases_at_20x': 85.0,
                 'mean_coverage': 36.400001525878906,
                 'filter_flags': ['contamination'],
+                **expected_qc_gen_anc_results,
+                'sample_qc.call_rate': 1.0,
+                'sample_qc.n_called': 30,
+                'sample_qc.n_not_called': 0,
+                'sample_qc.n_filtered': 0,
+                'sample_qc.n_hom_ref': 17,
+                'sample_qc.n_het': 3,
+                'sample_qc.n_hom_var': 10,
+                'sample_qc.n_non_ref': 13,
+                'sample_qc.n_singleton': 0,
+                'sample_qc.n_snp': 23,
+                'sample_qc.n_insertion': 0,
+                'sample_qc.n_deletion': 0,
+                'sample_qc.n_transition': 13,
+                'sample_qc.n_transversion': 10,
+                'sample_qc.n_star': 0,
+                'sample_qc.r_ti_tv': 1.3,
+                'sample_qc.r_het_hom_var': 0.3,
+                'sample_qc.r_insertion_deletion': None,
+                'sample_qc.f_inbreeding.f_stat': -0.038400752079048056,
+                'sample_qc.f_inbreeding.n_called': 30,
+                'sample_qc.f_inbreeding.expected_homs': 27.11094199999999,
+                'sample_qc.f_inbreeding.observed_homs': 27,
+                'fail_n_snp': True,
+                'fail_r_ti_tv': False,
+                'fail_r_insertion_deletion': None,
+                'fail_n_insertion': True,
+                'fail_n_deletion': True,
+                'fail_r_het_hom_var': False,
+                'fail_call_rate': False,
+                'qc_metrics_filters': ['n_deletion', 'n_insertion', 'n_snp'],
             },
         )
         self.assertCountEqual(
@@ -83,5 +252,36 @@ class WriteSampleQCJsonTaskTest(MockedDatarootTestCase):
                 'percent_bases_at_20x': 80.0,
                 'mean_coverage': 30.0,
                 'filter_flags': [],
+                **expected_qc_gen_anc_results,
+                'sample_qc.call_rate': 0.9666666666666667,
+                'sample_qc.n_called': 29,
+                'sample_qc.n_not_called': 1,
+                'sample_qc.n_filtered': 0,
+                'sample_qc.n_hom_ref': 18,
+                'sample_qc.n_het': 1,
+                'sample_qc.n_hom_var': 10,
+                'sample_qc.n_non_ref': 11,
+                'sample_qc.n_singleton': 1,
+                'sample_qc.n_snp': 21,
+                'sample_qc.n_insertion': 0,
+                'sample_qc.n_deletion': 0,
+                'sample_qc.n_transition': 12,
+                'sample_qc.n_transversion': 9,
+                'sample_qc.n_star': 0,
+                'sample_qc.r_ti_tv': 1.3333333333333333,
+                'sample_qc.r_het_hom_var': 0.1,
+                'sample_qc.r_insertion_deletion': None,
+                'sample_qc.f_inbreeding.f_stat': 0.6538664159736507,
+                'sample_qc.f_inbreeding.n_called': 29,
+                'sample_qc.f_inbreeding.expected_homs': 26.11094199999999,
+                'sample_qc.f_inbreeding.observed_homs': 28,
+                'fail_n_snp': False,
+                'fail_r_ti_tv': False,
+                'fail_r_insertion_deletion': None,
+                'fail_n_insertion': True,
+                'fail_n_deletion': True,
+                'fail_r_het_hom_var': False,
+                'fail_call_rate': True,
+                'qc_metrics_filters': ['call_rate', 'n_deletion', 'n_insertion'],
             },
         )
