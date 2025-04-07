c1a7f1e575ab45d2202753776820c75e70684899
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-04-07 14:09:54 -0400

    Remove code that supports expecting filters

--- v03_pipeline/lib/tasks/base/base_loading_run_params.py
+++ v03_pipeline/lib/tasks/base/base_loading_run_params.py
@@ -26,10 +26,6 @@ class BaseLoadingRunParams(luigi.Task):
         default=False,
         parsing=luigi.BoolParameter.EXPLICIT_PARSING,
     )
-    skip_expect_filters = luigi.BoolParameter(
-        default=False,
-        parsing=luigi.BoolParameter.EXPLICIT_PARSING,
-    )
     skip_expect_tdr_metrics = luigi.BoolParameter(
         default=False,
         parsing=luigi.BoolParameter.EXPLICIT_PARSING,
