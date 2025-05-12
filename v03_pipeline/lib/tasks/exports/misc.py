e5a8f73f29f6698d15bda40da2f5e327a2bfc3b6
Benjamin Blankenmeister <b.p.blankenmeister@gmail.com>
2025-05-12 10:20:46 -0400

    feat: add tasks for writing parquet tables.   (#1076)
    * Formatting function enums
    
    * improve function name
    
    * handle lookups that propagate missing
    
    * format
    
    * ruff
    
    * rename func
    
    * second func
    
    * start test
    
    * finish tests
    
    * ruff
    
    * Add camelcase
    
    * add camelcase
    
    * ruff
    
    * missing init py
    
    * move functions to export
    
    * mostly functioning entries task
    
    * import
    
    * improve private reference datasets logic
    
    * first pass
    
    * progress
    
    * tests passing
    
    * key
    
    * annotations table
    
    * test transcripts
    
    * closer
    
    * missed some tests
    
    * add a parquet reader requirement
    
    * use project tables
    
    * finish off first pass at test!
    
    * reformat test
    
    * another batch
    
    * add filters
    
    * ruff
    
    * handle hgmd edge case
    
    * add hgmd
    
    * sort it
    
    * ruff
    
    * bugfixes
    
    * update test to new format
    
    * ruff
    
    * v03
    
    * merge
    
    * no longer used
    
    * lint
    
    * formatting
    
    * special case the export
    
    * remove gene/map
    
    * ruff
    
    * add new annotations
    
    * print
    
    * ruff
    
    * canonical is not a float
    
    * add new clinvar variants parquet export (#1092)
    
    * feat: grch37 SNV_INDEL export (#1095)
    
    * support grch37
    
    * enable grch37
    
    * feat: alphabetize nested field (#1096)
    
    * Support alphabetization of nested field
    
    * better sorting
    
    * Update misc_test.py
    
    * ruff

---
+++ v03_pipeline/lib/tasks/exports/misc.py
@@ -0,0 +1,305 @@
+import hail as hl
+
+from v03_pipeline.lib.annotations.enums import (
+    BIOTYPES,
+    FIVEUTR_CONSEQUENCES,
+    LOF_FILTERS,
+    MITOTIP_PATHOGENICITIES,
+    MOTIF_CONSEQUENCE_TERMS,
+    REGULATORY_BIOTYPES,
+    REGULATORY_CONSEQUENCE_TERMS,
+    SV_CONSEQUENCE_RANKS,
+    SV_TYPE_DETAILS,
+    SV_TYPES,
+    TRANSCRIPT_CONSEQUENCE_TERMS,
+)
+from v03_pipeline.lib.misc.nested_field import parse_nested_field
+from v03_pipeline.lib.model import DatasetType, ReferenceGenome
+from v03_pipeline.lib.reference_datasets.reference_dataset import ReferenceDataset
+
+
+def snake_to_camelcase(snake_string: str):
+    components = snake_string.split('_')
+    return components[0] + ''.join(x.title() for x in components[1:])
+
+
+def camelcase_hl_struct(s: hl.StructExpression) -> hl.StructExpression:
+    return s.rename({f: snake_to_camelcase(f) for f in s})
+
+
+def sorted_hl_struct(s: hl.StructExpression) -> hl.StructExpression:
+    if not isinstance(s, hl.StructExpression):
+        return s
+    return s.select(**{k: sorted_hl_struct(s[k]) for k in sorted(s)})
+
+
+def array_structexpression_fields(ht: hl.Table):
+    return [
+        field
+        for field in ht.row
+        if isinstance(
+            ht[field],
+            hl.expr.expressions.typed_expressions.ArrayStructExpression,
+        )
+    ]
+
+
+def transcripts_field_name(
+    reference_genome: ReferenceGenome,
+    dataset_type: DatasetType,
+) -> str:
+    formatting_annotation_names = {
+        fa.__name__ for fa in dataset_type.formatting_annotation_fns(reference_genome)
+    }
+    if 'sorted_gene_consequences' in formatting_annotation_names:
+        return snake_to_camelcase('sorted_gene_consequences')
+    return snake_to_camelcase('sorted_transcript_consequences')
+
+
+def subset_filterable_transcripts_fields(
+    ht: hl.Table,
+    reference_genome: ReferenceGenome,
+    dataset_type: DatasetType,
+) -> hl.Table:
+    field_name = transcripts_field_name(reference_genome, dataset_type)
+    return ht.annotate(
+        **{
+            field_name: hl.enumerate(ht[field_name]).starmap(
+                lambda idx, c: c.select(
+                    **{
+                        new_nested_field_name: parse_nested_field(
+                            ht[field_name],
+                            existing_nested_field_name,
+                        )[idx]
+                        for new_nested_field_name, existing_nested_field_name in dataset_type.export_parquet_filterable_transcripts_fields(
+                            reference_genome,
+                        ).items()
+                    },
+                ),
+            ),
+        },
+    )
+
+
+def camelcase_array_structexpression_fields(
+    ht: hl.Table,
+    reference_genome: ReferenceGenome,
+):
+    for field in array_structexpression_fields(ht):
+        ht = ht.transmute(
+            **{
+                snake_to_camelcase(field): ht[field].map(
+                    lambda c: camelcase_hl_struct(c),
+                ),
+            },
+        )
+
+    # Custom handling of nested sorted_transcript_consequences fields for GRCh38
+    # Note that spliceregion (extended_intronic_splice_region_variant) prevents
+    # a more procedural approach here.
+    if (
+        reference_genome == ReferenceGenome.GRCh38
+        and 'sortedTranscriptConsequences' in ht.row
+    ):
+        ht = ht.annotate(
+            sortedTranscriptConsequences=ht.sortedTranscriptConsequences.map(
+                lambda s: s.annotate(
+                    loftee=camelcase_hl_struct(s.loftee),
+                    utrannotator=camelcase_hl_struct(s.utrannotator),
+                ),
+            ),
+        )
+    return ht
+
+
+def unmap_reference_dataset_annotation_enums(
+    ht: hl.Table,
+    reference_genome: ReferenceGenome,
+    dataset_type: DatasetType,
+) -> hl.Table:
+    reference_datasets = ReferenceDataset.for_reference_genome_dataset_type_annotations(
+        reference_genome,
+        dataset_type,
+    )
+    unmapped_annotation_name = []
+    for annotation_name in ht.enums:
+        if annotation_name not in reference_datasets:
+            continue
+        for enum_name in ht.enums[annotation_name]:
+            if hasattr(ht[annotation_name], f'{enum_name}_ids'):
+                ht = ht.annotate(
+                    **{
+                        annotation_name: ht[annotation_name].annotate(
+                            **{
+                                f'{enum_name}s': ht[annotation_name][
+                                    f'{enum_name}_ids'
+                                ].map(
+                                    lambda idx: ht.enums[annotation_name][enum_name][  # noqa: B023
+                                        idx
+                                    ],
+                                ),
+                            },
+                        ),
+                    },
+                )
+                ht = ht.annotate(
+                    **{annotation_name: ht[annotation_name].drop(f'{enum_name}_ids')},
+                )
+            else:
+                ht = ht.annotate(
+                    **{
+                        annotation_name: ht[annotation_name].annotate(
+                            **{
+                                enum_name: ht.enums[annotation_name][enum_name][
+                                    ht[annotation_name][f'{enum_name}_id']
+                                ],
+                            },
+                        ),
+                    },
+                )
+                ht = ht.annotate(
+                    **{annotation_name: ht[annotation_name].drop(f'{enum_name}_id')},
+                )
+        unmapped_annotation_name.append(annotation_name)
+
+    # Explicit clinvar edge case:
+    if hasattr(ht, ReferenceDataset.clinvar.value):
+        ht = ht.annotate(
+            **{
+                ReferenceDataset.clinvar.value: ht[
+                    ReferenceDataset.clinvar.value
+                ].annotate(
+                    conflictingPathogenicities=ht[
+                        ReferenceDataset.clinvar.value
+                    ].conflictingPathogenicities.map(
+                        lambda s: s.annotate(
+                            pathogenicity=ht.enums.clinvar.pathogenicity[
+                                s.pathogenicity_id
+                            ],
+                        ).drop('pathogenicity_id'),
+                    ),
+                ),
+            },
+        )
+
+    # Explicit hgmd edge case:
+    if hasattr(
+        ht,
+        ReferenceDataset.hgmd.value,
+    ):
+        ht = ht.annotate(
+            **{
+                ReferenceDataset.hgmd.value: ht[ReferenceDataset.hgmd.value]
+                .annotate(class_=ht[ReferenceDataset.hgmd.value]['class'])
+                .drop('class'),
+            },
+        )
+    return ht.annotate_globals(enums=ht.globals.enums.drop(*unmapped_annotation_name))
+
+
+def unmap_formatting_annotation_enums(
+    ht: hl.Table,
+    reference_genome: ReferenceGenome,
+    dataset_type: DatasetType,
+) -> hl.Table:
+    formatting_annotation_names = {
+        fa.__name__ for fa in dataset_type.formatting_annotation_fns(reference_genome)
+    }
+    if 'sorted_motif_feature_consequences' in formatting_annotation_names:
+        ht = ht.annotate(
+            sorted_motif_feature_consequences=ht.sorted_motif_feature_consequences.map(
+                lambda c: c.annotate(
+                    consequence_terms=c.consequence_term_ids.map(
+                        lambda tid: hl.array(MOTIF_CONSEQUENCE_TERMS)[tid],
+                    ),
+                ).drop('consequence_term_ids'),
+            ),
+        )
+        ht = ht.annotate_globals(
+            enums=ht.enums.drop('sorted_motif_feature_consequences'),
+        )
+    if 'sorted_regulatory_feature_consequences' in formatting_annotation_names:
+        ht = ht.annotate(
+            sorted_regulatory_feature_consequences=ht.sorted_regulatory_feature_consequences.map(
+                lambda c: c.annotate(
+                    biotype=hl.array(REGULATORY_BIOTYPES)[c.biotype_id],
+                    consequence_terms=c.consequence_term_ids.map(
+                        lambda tid: hl.array(REGULATORY_CONSEQUENCE_TERMS)[tid],
+                    ),
+                ).drop('biotype_id', 'consequence_term_ids'),
+            ),
+        )
+        ht = ht.annotate_globals(
+            enums=ht.enums.drop('sorted_regulatory_feature_consequences'),
+        )
+    if 'sorted_transcript_consequences' in formatting_annotation_names:
+        ht = ht.annotate(
+            sorted_transcript_consequences=ht.sorted_transcript_consequences.map(
+                lambda c: c.annotate(
+                    biotype=hl.array(BIOTYPES)[c.biotype_id],
+                    consequence_terms=c.consequence_term_ids.map(
+                        lambda tid: hl.array(TRANSCRIPT_CONSEQUENCE_TERMS)[tid],
+                    ),
+                    **{
+                        'loftee': c.loftee.annotate(
+                            lof_filters=c.loftee.lof_filter_ids.map(
+                                lambda fid: hl.array(LOF_FILTERS)[fid],
+                            ),
+                        ).drop('lof_filter_ids'),
+                        'utrannotator': c.utrannotator.annotate(
+                            fiveutr_consequence=hl.array(FIVEUTR_CONSEQUENCES)[
+                                c.utrannotator.fiveutr_consequence_id
+                            ],
+                        ).drop('fiveutr_consequence_id'),
+                    }
+                    if reference_genome == ReferenceGenome.GRCh38
+                    and dataset_type == DatasetType.SNV_INDEL
+                    else {
+                        'lof_filters': c.lof_filter_ids.map(
+                            lambda fid: hl.array(LOF_FILTERS)[fid],
+                        ),
+                    },
+                ).drop(
+                    'biotype_id',
+                    'consequence_term_ids',
+                    *(
+                        []
+                        if reference_genome == ReferenceGenome.GRCh38
+                        and dataset_type == DatasetType.SNV_INDEL
+                        else [
+                            'lof_filter_ids',
+                        ]
+                    ),
+                ),
+            ),
+        )
+        ht = ht.annotate_globals(enums=ht.enums.drop('sorted_transcript_consequences'))
+    if 'mitotip' in formatting_annotation_names:
+        ht = ht.annotate(
+            mitotip=hl.Struct(
+                trna_prediction=hl.array(MITOTIP_PATHOGENICITIES)[
+                    ht.mitotip.trna_prediction_id
+                ],
+            ),
+        )
+        ht = ht.annotate_globals(enums=ht.enums.drop('mitotip'))
+    if 'sv_type_id' in formatting_annotation_names:
+        ht = ht.annotate(sv_type=hl.array(SV_TYPES)[ht.sv_type_id]).drop('sv_type_id')
+        ht = ht.annotate_globals(enums=ht.enums.drop('sv_type'))
+    if 'sv_type_detail_id' in formatting_annotation_names:
+        ht = ht.annotate(
+            sv_type_detail=hl.array(SV_TYPE_DETAILS)[ht.sv_type_detail_id],
+        ).drop('sv_type_detail_id')
+        ht = ht.annotate_globals(enums=ht.enums.drop('sv_type_detail'))
+    if 'sorted_gene_consequences' in formatting_annotation_names:
+        ht = ht.annotate(
+            sorted_gene_consequences=ht.sorted_gene_consequences.map(
+                lambda c: c.annotate(
+                    major_consequence=hl.array(SV_CONSEQUENCE_RANKS)[
+                        c.major_consequence_id
+                    ],
+                ).drop('major_consequence_id'),
+            ),
+        )
+        ht = ht.annotate_globals(enums=ht.enums.drop('sorted_gene_consequences'))
+    return ht
---
+++ v03_pipeline/lib/tasks/exports/write_new_variants_parquet_test.py
@@ -0,0 +1,287 @@
+import os
+
+import hail as hl
+import luigi.worker
+import pandas as pd
+
+from v03_pipeline.lib.model import (
+    DatasetType,
+    ReferenceGenome,
+    SampleType,
+)
+from v03_pipeline.lib.paths import new_variants_parquet_path, new_variants_table_path
+from v03_pipeline.lib.tasks.exports.write_new_variants_parquet import (
+    WriteNewVariantsParquetTask,
+)
+from v03_pipeline.lib.test.misc import convert_ndarray_to_list
+from v03_pipeline.lib.test.mocked_dataroot_testcase import MockedDatarootTestCase
+
+TEST_SNV_INDEL_ANNOTATIONS = (
+    'v03_pipeline/var/test/exports/GRCh38/SNV_INDEL/annotations.ht'
+)
+TEST_GRCH37_SNV_INDEL_ANNOTATIONS = (
+    'v03_pipeline/var/test/exports/GRCh37/SNV_INDEL/annotations.ht'
+)
+
+TEST_RUN_ID = 'manual__2024-04-03'
+
+
+class WriteNewVariantsParquetTest(MockedDatarootTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        ht = hl.read_table(
+            TEST_SNV_INDEL_ANNOTATIONS,
+        )
+        ht.write(
+            new_variants_table_path(
+                ReferenceGenome.GRCh38,
+                DatasetType.SNV_INDEL,
+                TEST_RUN_ID,
+            ),
+        )
+        ht = hl.read_table(
+            TEST_GRCH37_SNV_INDEL_ANNOTATIONS,
+        )
+        ht.write(
+            new_variants_table_path(
+                ReferenceGenome.GRCh37,
+                DatasetType.SNV_INDEL,
+                TEST_RUN_ID,
+            ),
+        )
+
+    def test_write_new_variants_parquet_test(
+        self,
+    ) -> None:
+        worker = luigi.worker.Worker()
+        task = WriteNewVariantsParquetTask(
+            reference_genome=ReferenceGenome.GRCh38,
+            dataset_type=DatasetType.SNV_INDEL,
+            sample_type=SampleType.WGS,
+            callset_path='fake_callset',
+            project_guids=[
+                'fake_project',
+            ],
+            project_pedigree_paths=['fake_pedigree'],
+            skip_validation=True,
+            run_id=TEST_RUN_ID,
+        )
+        worker.add(task)
+        worker.run()
+        self.assertTrue(task.output().exists())
+        self.assertTrue(task.complete())
+        df = pd.read_parquet(
+            os.path.join(
+                new_variants_parquet_path(
+                    ReferenceGenome.GRCh38,
+                    DatasetType.SNV_INDEL,
+                    TEST_RUN_ID,
+                ),
+            ),
+        )
+        export_json = convert_ndarray_to_list(df.head(1).to_dict('records'))
+        export_json[0]['sortedTranscriptConsequences'] = [
+            export_json[0]['sortedTranscriptConsequences'][0],
+        ]
+        self.assertEqual(
+            export_json,
+            [
+                {
+                    'key': 0,
+                    'xpos': 1000939121,
+                    'chrom': '1',
+                    'pos': 939121,
+                    'ref': 'C',
+                    'alt': 'T',
+                    'variantId': '1-939121-C-T',
+                    'rsid': None,
+                    'CAID': 'CA502654',
+                    'liftedOverChrom': '1',
+                    'liftedOverPos': 874501,
+                    'hgmd': {'accession': 'abcdefg', 'class_': 'DFP'},
+                    'screenRegionType': None,
+                    'predictions': {
+                        'cadd': 23.5,
+                        'eigen': 2.628000020980835,
+                        'fathmm': 0.7174800038337708,
+                        'gnomad_noncoding': None,
+                        'mpc': 0.01291007362306118,
+                        'mut_pred': None,
+                        'mut_tester': 'D',
+                        'polyphen': 0.164000004529953,
+                        'primate_ai': 0.5918066501617432,
+                        'revel': 0.3109999895095825,
+                        'sift': 0.0010000000474974513,
+                        'splice_ai': 0.0,
+                        'splice_ai_consequence': 'No consequence',
+                        'vest': 0.39500001072883606,
+                    },
+                    'populations': {
+                        'exac': {
+                            'ac': 20,
+                            'af': 0.00019039999460801482,
+                            'an': 47974,
+                            'filter_af': 0.0007150234305299819,
+                            'hemi': None,
+                            'het': 20,
+                            'hom': 0,
+                        },
+                        'gnomad_exomes': {
+                            'ac': 964,
+                            'af': 0.0006690866430290043,
+                            'an': 1440770,
+                            'filter_af': 0.0008023773552849889,
+                            'hemi': 0,
+                            'hom': 0,
+                        },
+                        'gnomad_genomes': {
+                            'ac': 42,
+                            'af': 0.0002759889466688037,
+                            'an': 152180,
+                            'filter_af': 0.0005293028079904616,
+                            'hemi': 0,
+                            'hom': 0,
+                        },
+                        'topmed': {
+                            'ac': 41,
+                            'af': 0.00032651599030941725,
+                            'an': 125568,
+                            'het': 41,
+                            'hom': 0,
+                        },
+                    },
+                    'sortedMotifFeatureConsequences': [
+                        {
+                            'consequenceTerms': ['TF_binding_site_variant'],
+                            'motifFeatureId': 'ENSM00493959715',
+                        },
+                    ],
+                    'sortedRegulatoryFeatureConsequences': [
+                        {
+                            'biotype': 'CTCF_binding_site',
+                            'consequenceTerms': ['regulatory_region_variant'],
+                            'regulatoryFeatureId': 'ENSR00000344437',
+                        },
+                    ],
+                    'sortedTranscriptConsequences': [
+                        {
+                            'alphamissensePathogenicity': None,
+                            'canonical': 1,
+                            'consequenceTerms': ['missense_variant'],
+                            'extendedIntronicSpliceRegionVariant': False,
+                            'fiveutrConsequence': None,
+                            'geneId': 'ENSG00000187634',
+                        },
+                    ],
+                },
+            ],
+        )
+
+    def test_grch37_write_new_variants_parquet_test(
+        self,
+    ) -> None:
+        worker = luigi.worker.Worker()
+        task = WriteNewVariantsParquetTask(
+            reference_genome=ReferenceGenome.GRCh37,
+            dataset_type=DatasetType.SNV_INDEL,
+            sample_type=SampleType.WGS,
+            callset_path='fake_callset',
+            project_guids=[
+                'fake_project',
+            ],
+            project_pedigree_paths=['fake_pedigree'],
+            skip_validation=True,
+            run_id=TEST_RUN_ID,
+        )
+        worker.add(task)
+        worker.run()
+        self.assertTrue(task.output().exists())
+        self.assertTrue(task.complete())
+        df = pd.read_parquet(
+            os.path.join(
+                new_variants_parquet_path(
+                    ReferenceGenome.GRCh37,
+                    DatasetType.SNV_INDEL,
+                    TEST_RUN_ID,
+                ),
+            ),
+        )
+        export_json = convert_ndarray_to_list(df.head(1).to_dict('records'))
+        export_json[0]['sortedTranscriptConsequences'] = [
+            export_json[0]['sortedTranscriptConsequences'][0],
+        ]
+        self.assertEqual(
+            export_json,
+            [
+                {
+                    'key': 1424,
+                    'xpos': 1000069134,
+                    'chrom': '1',
+                    'pos': 69134,
+                    'ref': 'A',
+                    'alt': 'G',
+                    'variantId': '1-69134-A-G',
+                    'rsid': None,
+                    'CAID': 'CA502008',
+                    'liftedOverChrom': '1',
+                    'liftedOverPos': 69134,
+                    'hgmd': None,
+                    'predictions': {
+                        'cadd': 15.880000114440918,
+                        'eigen': 1.0019999742507935,
+                        'fathmm': 0.056940000504255295,
+                        'mpc': 1.8921889066696167,
+                        'mut_pred': 0.3779999911785126,
+                        'mut_tester': 'N',
+                        'polyphen': 0.0010000000474974513,
+                        'primate_ai': 0.37232041358947754,
+                        'revel': 0.07500000298023224,
+                        'sift': 0.1289999932050705,
+                        'splice_ai': 0.019999999552965164,
+                        'splice_ai_consequence': 'Donor gain',
+                        'vest': 0.10700000077486038,
+                    },
+                    'populations': {
+                        'exac': {
+                            'ac': 0,
+                            'af': 0.0016550000291317701,
+                            'an': 66,
+                            'filter_af': None,
+                            'hemi': None,
+                            'het': 0,
+                            'hom': 0,
+                        },
+                        'gnomad_exomes': {
+                            'ac': 505,
+                            'af': 0.026665963232517242,
+                            'an': 18938,
+                            'filter_af': 0.08191808313131332,
+                            'hemi': 0,
+                            'hom': 127,
+                        },
+                        'gnomad_genomes': {
+                            'ac': 1,
+                            'af': 0.0001722949673421681,
+                            'an': 5804,
+                            'filter_af': 0.0005662514013238251,
+                            'hemi': 0,
+                            'hom': 0,
+                        },
+                        'topmed': {
+                            'ac': 95,
+                            'af': 0.0007565619889646769,
+                            'an': 125568,
+                            'het': 95,
+                            'hom': 0,
+                        },
+                    },
+                    'sortedTranscriptConsequences': [
+                        {
+                            'canonical': 1,
+                            'consequenceTerms': ['missense_variant'],
+                            'geneId': 'ENSG00000186092',
+                        },
+                    ],
+                },
+            ],
+        )
