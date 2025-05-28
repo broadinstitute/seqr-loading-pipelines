3c39b1c278ad04a442b1951113e45b6ab98f82ba
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-05-28 17:54:09 -0400

    First mito test

--- v03_pipeline/lib/tasks/exports/write_new_clinvar_variants_parquet_test.py
+++ v03_pipeline/lib/tasks/exports/write_new_clinvar_variants_parquet_test.py
@@ -1,3 +1,6 @@
+from unittest import mock
+from unittest.mock import Mock
+
 import hail as hl
 import luigi.worker
 import pandas as pd
@@ -15,11 +18,13 @@ from v03_pipeline.lib.tasks.exports.write_new_clinvar_variants_parquet import (
     WriteNewClinvarVariantsParquetTask,
 )
 from v03_pipeline.lib.test.misc import convert_ndarray_to_list
+from v03_pipeline.lib.test.mock_complete_task import MockCompleteTask
 from v03_pipeline.lib.test.mocked_dataroot_testcase import MockedDatarootTestCase
 
 TEST_SNV_INDEL_ANNOTATIONS = (
     'v03_pipeline/var/test/exports/GRCh38/SNV_INDEL/annotations.ht'
 )
+TEST_MITO_ANNOTATIONS = 'v03_pipeline/var/test/exports/GRCh38/MITO/annotations.ht'
 
 TEST_RUN_ID = 'manual__2024-04-03'
 
@@ -34,6 +39,16 @@ class WriteNewClinvarVariantsParquetTest(MockedDatarootTestCase):
             new_variants_table_path(
                 ReferenceGenome.GRCh38,
                 DatasetType.SNV_INDEL,
+                TEST_RUN_ID,
+            ),
+        )
+        ht = hl.read_table(
+            TEST_MITO_ANNOTATIONS,
+        )
+        ht.write(
+            new_variants_table_path(
+                ReferenceGenome.GRCh38,
+                DatasetType.MITO,
                 TEST_RUN_ID,
             ),
         )
@@ -78,6 +93,57 @@ class WriteNewClinvarVariantsParquetTest(MockedDatarootTestCase):
                     'conditions': ['not provided'],
                     'assertions': [],
                     'pathogenicity': 'Uncertain_significance',
+                },
+            ],
+        )
+
+    @mock.patch(
+        'v03_pipeline.lib.tasks.exports.write_new_clinvar_variants_parquet.WriteNewVariantsTableTask',
+    )
+    def test_write_new_mito_clinvar_variants_parquet_test(
+        self,
+        write_new_variants_table_task: Mock,
+    ) -> None:
+        write_new_variants_table_task.return_value = MockCompleteTask()
+        worker = luigi.worker.Worker()
+        task = WriteNewClinvarVariantsParquetTask(
+            reference_genome=ReferenceGenome.GRCh38,
+            dataset_type=DatasetType.MITO,
+            sample_type=SampleType.WGS,
+            callset_path='fake_callset',
+            project_guids=[
+                'fake_project',
+            ],
+            project_pedigree_paths=['fake_pedigree'],
+            skip_validation=True,
+            run_id=TEST_RUN_ID,
+        )
+        worker.add(task)
+        worker.run()
+        self.assertTrue(task.output().exists())
+        self.assertTrue(task.complete())
+        df = pd.read_parquet(
+            new_clinvar_variants_parquet_path(
+                ReferenceGenome.GRCh38,
+                DatasetType.MITO,
+                TEST_RUN_ID,
+            ),
+        )
+        export_json = convert_ndarray_to_list(df.head(1).to_dict('records'))
+        self.assertEqual(
+            export_json,
+            [
+                {
+                    'key': 81,
+                    'alleleId': 3226084,
+                    'conflictingPathogenicities': None,
+                    'goldStars': 0,
+                    'submitters': [
+                        'Genomics Division, Defence Institute of Physiology and Allied Sciences',
+                    ],
+                    'conditions': ['Venous thromboembolism'],
+                    'assertions': ['Affects'],
+                    'pathogenicity': 'No_pathogenic_assertion',
                 },
             ],
         )
