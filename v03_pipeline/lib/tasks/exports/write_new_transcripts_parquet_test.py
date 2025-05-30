1b1b3f44c6408bd1d2af0939a3894f642eaa00fd
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-05-30 00:52:35 -0400

    sv transcripts

--- v03_pipeline/lib/tasks/exports/write_new_transcripts_parquet_test.py
+++ v03_pipeline/lib/tasks/exports/write_new_transcripts_parquet_test.py
@@ -26,6 +26,7 @@ TEST_GRCH37_SNV_INDEL_ANNOTATIONS = (
     'v03_pipeline/var/test/exports/GRCh37/SNV_INDEL/annotations.ht'
 )
 TEST_MITO_ANNOTATIONS = 'v03_pipeline/var/test/exports/GRCh38/MITO/annotations.ht'
+TEST_SV_ANNOTATIONS = 'v03_pipeline/var/test/exports/GRCh38/SV/annotations.ht'
 
 TEST_RUN_ID = 'manual__2024-04-03'
 
@@ -60,6 +61,16 @@ class WriteNewTranscriptsParquetTest(MockedDatarootTestCase):
             new_variants_table_path(
                 ReferenceGenome.GRCh38,
                 DatasetType.MITO,
+                TEST_RUN_ID,
+            ),
+        )
+        ht = hl.read_table(
+            TEST_SV_ANNOTATIONS,
+        )
+        ht.write(
+            new_variants_table_path(
+                ReferenceGenome.GRCh38,
+                DatasetType.SV,
                 TEST_RUN_ID,
             ),
         )
@@ -301,6 +312,58 @@ class WriteNewTranscriptsParquetTest(MockedDatarootTestCase):
                 'majorConsequence': 'non_coding_transcript_exon_variant',
                 'transcriptId': 'ENST00000387314',
                 'transcriptRank': 0,
+            },
+        )
+        self.assertEqual(
+            list(export_json[0]['transcripts'][0].keys()),
+            sorted(export_json[0]['transcripts'][0].keys()),
+        )
+
+    @mock.patch(
+        'v03_pipeline.lib.tasks.exports.write_new_transcripts_parquet.WriteNewVariantsTableTask',
+    )
+    def test_sv_write_new_transcripts_parquet_test(
+        self,
+        write_new_variants_table_task: Mock,
+    ) -> None:
+        write_new_variants_table_task.return_value = MockCompleteTask()
+        worker = luigi.worker.Worker()
+        task = WriteNewTranscriptsParquetTask(
+            reference_genome=ReferenceGenome.GRCh38,
+            dataset_type=DatasetType.SV,
+            sample_type=SampleType.WGS,
+            callset_path='fake_callset',
+            project_guids=[
+                'fake_project',
+            ],
+            project_pedigree_paths=['fake_pedigree'],
+            skip_validation=True,
+            run_id=TEST_RUN_ID,
+        )
+        worker.add(task)
+        worker.run()
+        self.assertTrue(task.output().exists())
+        self.assertTrue(task.complete())
+        df = pd.read_parquet(
+            os.path.join(
+                new_transcripts_parquet_path(
+                    ReferenceGenome.GRCh38,
+                    DatasetType.SV,
+                    TEST_RUN_ID,
+                ),
+            ),
+        )
+        export_json = convert_ndarray_to_list(df.head(1).to_dict('records'))
+        self.assertListEqual(list(export_json[0].keys()), ['key', 'transcripts'])
+        self.assertEqual(
+            export_json[0]['key'],
+            0,
+        )
+        self.assertEqual(
+            export_json[0]['transcripts'][0],
+            {
+                'geneId': 'ENSG00000186092',
+                'majorConsequence': 'NEAREST_TSS',
             },
         )
         self.assertEqual(
