29a4ecce238443ed05fe211148cf3b4eef0fef34
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-06-09 17:36:12 -0400

    export more variants

--- v03_pipeline/lib/tasks/exports/write_new_transcripts_parquet_test.py
+++ v03_pipeline/lib/tasks/exports/write_new_transcripts_parquet_test.py
@@ -11,7 +11,7 @@ from v03_pipeline.lib.model import (
     ReferenceGenome,
     SampleType,
 )
-from v03_pipeline.lib.paths import new_transcripts_parquet_path, new_variants_table_path
+from v03_pipeline.lib.paths import new_transcripts_parquet_path, new_variants_table_path, variant_annotations_table_path
 from v03_pipeline.lib.tasks.exports.write_new_transcripts_parquet import (
     WriteNewTranscriptsParquetTask,
 )
@@ -79,10 +79,9 @@ class WriteNewTranscriptsParquetTest(MockedDatarootTestCase):
             TEST_GCNV_ANNOTATIONS,
         )
         ht.write(
-            new_variants_table_path(
+            variant_annotations_table_path(
                 ReferenceGenome.GRCh38,
                 DatasetType.GCNV,
-                TEST_RUN_ID,
             ),
         )
 
@@ -383,13 +382,13 @@ class WriteNewTranscriptsParquetTest(MockedDatarootTestCase):
         )
 
     @mock.patch(
-        'v03_pipeline.lib.tasks.exports.write_new_transcripts_parquet.WriteNewVariantsTableTask',
+        'v03_pipeline.lib.tasks.exports.write_new_transcripts_parquet.UpdateVariantAnnotationsTableWithNewSamplesTask',
     )
     def test_gcnv_write_new_transcripts_parquet_test(
         self,
-        write_new_variants_table_task: Mock,
+        update_variant_annotations_task: Mock,
     ) -> None:
-        write_new_variants_table_task.return_value = MockCompleteTask()
+        update_variant_annotations_task.return_value = MockCompleteTask()
         worker = luigi.worker.Worker()
         task = WriteNewTranscriptsParquetTask(
             reference_genome=ReferenceGenome.GRCh38,
