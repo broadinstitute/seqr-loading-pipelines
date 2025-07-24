a85b9294165bd3045115ea4b75d59449102b20d5
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-07-24 17:39:30 -0400

    fix env var

--- v03_pipeline/lib/misc/clickhouse_test.py
+++ v03_pipeline/lib/misc/clickhouse_test.py
@@ -167,7 +167,7 @@ class ClickhouseTest(MockedDatarootTestCase):
             PRIMARY KEY key
             SOURCE(
                 CLICKHOUSE(
-                    USER {Env.CLICKHOUSE_USER} PASSWORD {Env.CLICKHOUSE_PASSWORD or "''"}
+                    USER {Env.CLICKHOUSE_WRITER_USER} PASSWORD {Env.CLICKHOUSE_PASSWORD or "''"}
                     DB {Env.CLICKHOUSE_DATABASE} TABLE `GRCh38/SNV_INDEL/gt_stats`
                 )
             )
@@ -913,7 +913,7 @@ class ClickhouseTest(MockedDatarootTestCase):
             next(iter([s[0] for s in staging_tables if 'DICTIONARY' in s[0]])).strip(),
             # important test!  Ensuring that the staging dictionary points to the production gt_stats table.
             f"""
-            CREATE DICTIONARY staging.`{table_name_builder.run_id_hash}/GRCh38/SNV_INDEL/gt_stats_dict` (`key` UInt32, `ac_wes` UInt16, `ac_wgs` UInt16) PRIMARY KEY key SOURCE(CLICKHOUSE(USER {Env.CLICKHOUSE_USER} PASSWORD '[HIDDEN]' DB {Env.CLICKHOUSE_DATABASE} TABLE `GRCh38/SNV_INDEL/gt_stats`)) LIFETIME(MIN 0 MAX 0) LAYOUT(FLAT(MAX_ARRAY_SIZE 10000))
+            CREATE DICTIONARY staging.`{table_name_builder.run_id_hash}/GRCh38/SNV_INDEL/gt_stats_dict` (`key` UInt32, `ac_wes` UInt16, `ac_wgs` UInt16) PRIMARY KEY key SOURCE(CLICKHOUSE(USER {Env.CLICKHOUSE_WRITER_USER} PASSWORD '[HIDDEN]' DB {Env.CLICKHOUSE_DATABASE} TABLE `GRCh38/SNV_INDEL/gt_stats`)) LIFETIME(MIN 0 MAX 0) LAYOUT(FLAT(MAX_ARRAY_SIZE 10000))
             """.strip(),
         )
 
