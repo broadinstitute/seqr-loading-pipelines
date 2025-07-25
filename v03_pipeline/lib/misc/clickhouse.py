0e6c345957e15ab6ed3000209a8ce7c309d70b1e
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-07-24 20:49:39 -0400

    ruff

--- v03_pipeline/lib/misc/clickhouse.py
+++ v03_pipeline/lib/misc/clickhouse.py
@@ -162,9 +162,9 @@ class TableNameBuilder:
 def logged_query(query, params=None, timeout: int | None = None):
     client = get_clickhouse_client(timeout)
     sanitized_query = query
-    if Env.CLICKHOUSE_PASSWORD:
+    if Env.CLICKHOUSE_WRITER_PASSWORD:
         sanitized_query = sanitized_query.replace(
-            Env.CLICKHOUSE_PASSWORD,
+            Env.CLICKHOUSE_WRITER_PASSWORD,
             REDACTED,
         )
     logger.info(f'Executing query: {sanitized_query} | Params: {params}')
@@ -215,10 +215,9 @@ def create_staging_non_table_entities(
             },
         )[0][0]
         if isinstance(clickhouse_entity, ClickHouseDictionary):
-            password = Env.CLICKHOUSE_PASSWORD or "''"
             create_entity_statement = create_entity_statement.replace(
                 "PASSWORD '[HIDDEN]'",
-                f'PASSWORD {password}',
+                f'PASSWORD {Env.CLICKHOUSE_WRITER_PASSWORD}',
             )
         create_entity_statement = create_entity_statement.replace(
             table_name_builder.dst_prefix,
