f26c52869195389117018045a3a581b33fbec98a
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-09-08 14:52:07 -0400

    leave out randomness from run id path

--- v03_pipeline/bin/pipeline_worker.py
+++ v03_pipeline/bin/pipeline_worker.py
@@ -24,7 +24,7 @@ def main():
             with open(latest_queue_path) as f:
                 lpr = LoadingPipelineRequest.model_validate_json(f.read())
             run_id = re.search(
-                r'request_(\d{8}-\d{6}_\d+)\.json',
+                r'request_(\d{8}-\d{6})_\d+\.json',
                 os.path.basename(latest_queue_path),
             ).group(1)
             loading_run_task_params = {
