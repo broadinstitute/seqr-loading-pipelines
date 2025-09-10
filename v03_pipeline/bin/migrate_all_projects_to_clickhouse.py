c2daafcf93c9e6005e434744f1ff96742a272dfb
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-09-10 18:52:30 -0400

    lint

--- v03_pipeline/bin/migrate_all_projects_to_clickhouse.py
+++ v03_pipeline/bin/migrate_all_projects_to_clickhouse.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-import sys
 
 import luigi
 
 from v03_pipeline.lib.model import DatasetType, ReferenceGenome
 from v03_pipeline.lib.tasks import MigrateAllProjectsToClickHouseTask
 
-
 if __name__ == '__main__':
     worker = luigi.worker.Worker()
     worker.add(
         MigrateAllProjectsToClickHouseTask(
-            ReferenceGenome.GRCh37, DatasetType.SNV_INDEL
+            ReferenceGenome.GRCh37, DatasetType.SNV_INDEL,
         ),
         MigrateAllProjectsToClickHouseTask(
-            ReferenceGenome.GRCh38, DatasetType.SNV_INDEL
+            ReferenceGenome.GRCh38, DatasetType.SNV_INDEL,
         ),
     )
     worker.run()
