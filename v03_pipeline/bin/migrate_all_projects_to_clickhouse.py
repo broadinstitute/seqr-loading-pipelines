eb7d70ee9074ee3342fe1ec9c218f351065ce472
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-11-17 12:09:38 -0500

    ruff

--- v03_pipeline/bin/migrate_all_projects_to_clickhouse.py
+++ v03_pipeline/bin/migrate_all_projects_to_clickhouse.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 import uuid
 
+import hailtop.fs as hfs
 import luigi
 
-from v03_pipeline.lib.core import DatasetType, FeatureFlag, ReferenceGenome
+from v03_pipeline.lib.core import DatasetType, FeatureFlag, ReferenceGenome, SampleType
 from v03_pipeline.lib.core.constants import MIGRATION_RUN_ID
+from v03_pipeline.lib.paths import project_table_path
 from v03_pipeline.lib.tasks import (
     MigrateAllProjectsToClickHouseOnDataprocTask,
     MigrateAllProjectsToClickHouseTask,
+)
+from v03_pipeline.lib.tasks.write_clickhouse_load_success_file import (
+    WriteClickhouseLoadSuccessFileTask,
 )
 
 if __name__ == '__main__':
@@ -16,13 +21,37 @@ if __name__ == '__main__':
         if FeatureFlag.RUN_PIPELINE_ON_DATAPROC
         else MigrateAllProjectsToClickHouseTask
     )
+    run_id_prefix = MIGRATION_RUN_ID + '-' + str(uuid.uuid1().int)[:4]
     luigi.build(
         [
             task_cls(
                 reference_genome,
                 DatasetType.SNV_INDEL,
-                run_id=MIGRATION_RUN_ID + '-' + str(uuid.uuid1().int)[:4],
+                run_id=run_id_prefix,
             )
             for reference_genome in ReferenceGenome
         ],
     )
+    clickhouse_load_tasks = []
+    for sample_type in SampleType:
+        for reference_genome in ReferenceGenome:
+            for p in hfs.ls(
+                project_table_path(
+                    reference_genome,
+                    DatasetType.SNV_INDEL,
+                    sample_type,
+                    '*',
+                ),
+            ):
+                project_guid = p.path.split('/')[-1].replace('.ht', '')
+                clickhouse_load_tasks.append(
+                    WriteClickhouseLoadSuccessFileTask(
+                        reference_genome=reference_genome,
+                        dataset_type=DatasetType.SNV_INDEL,
+                        run_id=f'{run_id_prefix}_{sample_type.value}_{project_guid}',
+                        sample_type=sample_type,
+                        project_guid=project_guid,
+                        attempt_id=0,
+                    ),
+                )
+    luigi.build(clickhouse_load_tasks)
