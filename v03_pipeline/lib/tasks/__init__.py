7051a6e5e94bf2dd8c94afbfa4300d8691416d38
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-12-01 23:18:53 -0500

    ruff

--- v03_pipeline/lib/tasks/__init__.py
+++ v03_pipeline/lib/tasks/__init__.py
@@ -4,10 +4,10 @@ from v03_pipeline.lib.tasks.clickhouse_migration.migrate_all_projects_to_clickho
 from v03_pipeline.lib.tasks.clickhouse_migration.migrate_all_projects_to_clickhouse_on_dataproc import (
     MigrateAllProjectsToClickHouseOnDataprocTask,
 )
-from v03_pipeline.lib.tasks.run_pipeline import RunPipelineTask
 from v03_pipeline.lib.tasks.reference_data.updated_reference_dataset_parquet import (
     UpdatedReferenceDatasetParquetTask,
 )
+from v03_pipeline.lib.tasks.run_pipeline import RunPipelineTask
 from v03_pipeline.lib.tasks.write_metadata_for_run import WriteMetadataForRunTask
 from v03_pipeline.lib.tasks.write_success_file import WriteSuccessFileTask
 
