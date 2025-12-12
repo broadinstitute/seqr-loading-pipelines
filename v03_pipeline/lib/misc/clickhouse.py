0540e30cfc67d440aaff3010e09f6abade6e131c
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-12-11 20:34:19 -0500

    ruff

--- v03_pipeline/lib/misc/clickhouse.py
+++ v03_pipeline/lib/misc/clickhouse.py
@@ -347,7 +347,7 @@ class ClickhouseReferenceDataset(StrEnum):
         if not self.has_seqr_variants:
             return
         exists_seqr_variants = logged_query(
-            f'EXISTS TABLE {self.seqr_variants_path(table_name_builder)}'
+            f'EXISTS TABLE {self.seqr_variants_path(table_name_builder)}',
         )[0][0]
         if not exists_seqr_variants:
             return
