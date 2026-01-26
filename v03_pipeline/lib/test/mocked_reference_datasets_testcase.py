a50febf38f3f323909b32b5231d00de52efc120f
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-01-25 22:31:15 -0500

    ruff

--- v03_pipeline/lib/test/mocked_reference_datasets_testcase.py
+++ v03_pipeline/lib/test/mocked_reference_datasets_testcase.py
@@ -1,7 +1,4 @@
-
 from v03_pipeline.lib.test.mocked_dataroot_testcase import MockedDatarootTestCase
-
-REFERENCE_DATASETS_PATH = 'v03_pipeline/var/test/reference_datasets'
 
 
 class MockedReferenceDatasetsTestCase(MockedDatarootTestCase):
