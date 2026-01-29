87194fe856980003cc913632e99a20796052becf
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-01-29 09:07:44 -0500

    ruff

--- v03_pipeline/lib/tasks/variants_migration/load_clickhouse_variants_tables.py
+++ v03_pipeline/lib/tasks/variants_migration/load_clickhouse_variants_tables.py
@@ -1,11 +1,16 @@
 import luigi
 import luigi.util
 
+from v03_pipeline.lib.core import FeatureFlag
 from v03_pipeline.lib.tasks.base.base_loading_run_params import (
     BaseLoadingRunParams,
 )
-from v03_pipeline.lib.tasks.variants_migration import (
+from v03_pipeline.lib.tasks.variants_migration.migrate_variant_details_parquet import (
+    MigrateVariantDetailsParquetOnDataprocTask,
     MigrateVariantDetailsParquetTask,
+)
+from v03_pipeline.lib.tasks.variants_migration.migrate_variants_parquet import (
+    MigrateVariantsParquetOnDataprocTask,
     MigrateVariantsParquetTask,
 )
 
@@ -13,10 +18,17 @@ from v03_pipeline.lib.tasks.variants_migration import (
 @luigi.util.inherits(BaseLoadingRunParams)
 class LoadClickhouseVariantsTablesTask(luigi.WrapperTask):
     def requires(self) -> luigi.Task:
-        return [
-            self.clone(MigrateVariantDetailsParquetTask),
-            self.clone(MigrateVariantsParquetTask),
-        ]
+        return (
+            [
+                self.clone(MigrateVariantDetailsParquetOnDataprocTask),
+                self.clone(MigrateVariantsParquetOnDataprocTask),
+            ]
+            if FeatureFlag.RUN_PIPELINE_ON_DATAPROC
+            else [
+                self.clone(MigrateVariantDetailsParquetTask),
+                self.clone(MigrateVariantsParquetTask),
+            ]
+        )
 
     def run(self) -> None:
         pass
