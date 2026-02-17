7ea1d66c637df9255a3ef47016ae854f23cd703a
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-02-17 15:03:49 -0600

    chore: port variants migration from dev

--- v03_pipeline/lib/tasks/__init__.py
+++ v03_pipeline/lib/tasks/__init__.py
@@ -8,12 +8,20 @@ from v03_pipeline.lib.tasks.reference_data.updated_reference_dataset_parquet imp
     UpdatedReferenceDatasetParquetTask,
 )
 from v03_pipeline.lib.tasks.run_pipeline import RunPipelineTask
+from v03_pipeline.lib.tasks.variants_migration.migrate_variant_details_parquet import (
+    MigrateVariantDetailsParquetTask,
+)
+from v03_pipeline.lib.tasks.variants_migration.migrate_variants_parquet import (
+    MigrateVariantsParquetTask,
+)
 from v03_pipeline.lib.tasks.write_metadata_for_run import WriteMetadataForRunTask
 from v03_pipeline.lib.tasks.write_success_file import WriteSuccessFileTask
 
 __all__ = [
     'MigrateAllProjectsToClickHouseOnDataprocTask',
     'MigrateAllProjectsToClickHouseTask',
+    'MigrateVariantDetailsParquetTask',
+    'MigrateVariantsParquetTask',
     'RunPipelineTask',
     'UpdatedReferenceDatasetParquetTask',
     'WriteMetadataForRunTask',
