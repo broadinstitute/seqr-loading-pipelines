1b2d144be3f55cf11d2e44d14600bc3fee5b9195
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-02-23 18:15:10 -0600

    ruff

--- v03_pipeline/lib/tasks/variants_migration/migrate_variant_details_parquet.py
+++ v03_pipeline/lib/tasks/variants_migration/migrate_variant_details_parquet.py
@@ -13,7 +13,6 @@ from v03_pipeline.lib.tasks.base.base_write_parquet import BaseWriteParquetTask
 from v03_pipeline.lib.tasks.dataproc.base_run_job_on_dataproc import (
     BaseRunJobOnDataprocTask,
 )
-from v03_pipeline.lib.tasks.files import GCSorLocalTarget
 from v03_pipeline.lib.tasks.exports.fields import (
     get_variant_details_export_fields,
 )
@@ -21,6 +20,7 @@ from v03_pipeline.lib.tasks.exports.misc import (
     camelcase_array_structexpression_fields,
     unmap_formatting_annotation_enums,
 )
+from v03_pipeline.lib.tasks.files import GCSorLocalTarget
 from v03_pipeline.lib.tasks.variants_migration.update_variant_annotations_table_with_dropped_reference_datasets import (
     UpdateVariantAnnotationsTableWithDroppedReferenceDatasetsTask,
 )
