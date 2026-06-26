eff2355c18c0e0d0a8f0eb4ca4c4bc845a5bc553
Hana Snow <hsnow@broadinstitute.org>
2026-06-26 15:41:43 -0400

    ruff

--- loading_pipeline/lib/tasks/write_remapped_and_subsetted_callset.py
+++ loading_pipeline/lib/tasks/write_remapped_and_subsetted_callset.py
@@ -57,7 +57,7 @@ class WriteRemappedAndSubsettedCallsetTask(BaseWriteTask):
             return False
         mt_globals = hl.read_matrix_table(self.output().path).globals.collect()
         return bool(
-            mt_globals.family_samples
+            mt_globals.family_samples,
         ) and mt_globals.remap_pedigree_hash == remap_pedigree_hash(
             project_pedigree_path(
                 self.reference_genome,
