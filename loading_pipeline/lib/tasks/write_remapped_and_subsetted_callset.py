3251a50b88416e79a82d2d4c200338d60d524eb0
Hana Snow <hsnow@broadinstitute.org>
2026-06-26 15:37:05 -0400

    fix global check

--- loading_pipeline/lib/tasks/write_remapped_and_subsetted_callset.py
+++ loading_pipeline/lib/tasks/write_remapped_and_subsetted_callset.py
@@ -55,9 +55,9 @@ class WriteRemappedAndSubsettedCallsetTask(BaseWriteTask):
     def complete(self) -> luigi.Target:
         if not super().complete():
             return False
-        mt_globals = hl.eval(hl.read_matrix_table(self.output().path).globals)
+        mt_globals = hl.read_matrix_table(self.output().path).globals.collect()
         return (
-            mt_globals.family_samples
+            bool(mt_globals.family_samples)
             and mt_globals.remap_pedigree_hash
             == remap_pedigree_hash(
                 project_pedigree_path(
